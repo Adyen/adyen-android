757362fe15ae91b61a1e717001a3fdbc400b5b7a
josephj <joseph.jreij@adyen.com>
2020-12-04 14:36:19 +0100

    Validate the Blik code input

--- blik-base/src/main/java/com/adyen/checkout/blik/BlikOutputData.java
+++ blik-base/src/main/java/com/adyen/checkout/blik/BlikOutputData.java
@@ -14,14 +14,18 @@ import androidx.annotation.NonNull;
 
 import com.adyen.checkout.base.component.OutputData;
 import com.adyen.checkout.base.validation.ValidatedField;
+import com.adyen.checkout.core.log.LogUtil;
+import com.adyen.checkout.core.log.Logger;
 
 class BlikOutputData implements OutputData {
+    private static final String TAG = LogUtil.getTag();
+
+    private static final int BLIK_CODE_LENGTH = 6;
 
     private final ValidatedField<String> mBlikCodeField;
 
     BlikOutputData(@NonNull String blikCode) {
-        mBlikCodeField = new ValidatedField<>(blikCode,
-                TextUtils.isEmpty(blikCode) ? ValidatedField.Validation.PARTIAL : ValidatedField.Validation.VALID);
+        mBlikCodeField = new ValidatedField<>(blikCode, getBlikCodeValidation(blikCode));
     }
 
     @Override
@@ -32,5 +36,18 @@ class BlikOutputData implements OutputData {
     @NonNull
     public ValidatedField<String> getBlikCodeField() {
         return mBlikCodeField;
+    }
+
+    private ValidatedField.Validation getBlikCodeValidation(@NonNull String blikCode) {
+        if (TextUtils.isEmpty(blikCode)) return ValidatedField.Validation.PARTIAL;
+        try {
+            Integer.parseInt(blikCode);
+        } catch (NumberFormatException e) {
+            Logger.e(TAG, "Failed to parse blik code to Integer", e);
+            return ValidatedField.Validation.INVALID;
+        }
+        if (blikCode.length() == BLIK_CODE_LENGTH) return ValidatedField.Validation.VALID;
+        if (blikCode.length() < BLIK_CODE_LENGTH) return ValidatedField.Validation.PARTIAL;
+        return ValidatedField.Validation.INVALID;
     }
 }
