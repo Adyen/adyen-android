28a570d8379dcb33598bf51b4ec39f18b3d167c2
Caio Faustino <caio.faustino@adyen.com>
2020-12-15 14:56:53 +0100

    Update SepaPaymentMethod keys and enable it again.

--- base-v3/src/main/java/com/adyen/checkout/base/model/payments/request/SepaPaymentMethod.java
+++ base-v3/src/main/java/com/adyen/checkout/base/model/payments/request/SepaPaymentMethod.java
@@ -28,8 +28,8 @@ public class SepaPaymentMethod extends PaymentMethodDetails {
 
     public static final String PAYMENT_METHOD_TYPE = PaymentMethodTypes.SEPA;
 
-    private static final String OWNER_NAME = "sepa.ownerName";
-    private static final String IBAN_NUMBER = "sepa.ibanNumber";
+    private static final String OWNER_NAME = "ownerName";
+    private static final String IBAN = "iban";
 
     @NonNull
     public static final Serializer<SepaPaymentMethod> SERIALIZER = new Serializer<SepaPaymentMethod>() {
@@ -43,7 +43,7 @@ public class SepaPaymentMethod extends PaymentMethodDetails {
                 jsonObject.putOpt(PaymentMethodDetails.TYPE, modelObject.getType());
 
                 jsonObject.putOpt(OWNER_NAME, modelObject.getOwnerName());
-                jsonObject.putOpt(IBAN_NUMBER, modelObject.getIbanNumber());
+                jsonObject.putOpt(IBAN, modelObject.getIban());
             } catch (JSONException e) {
                 throw new ModelSerializationException(SepaPaymentMethod.class, e);
             }
@@ -59,14 +59,14 @@ public class SepaPaymentMethod extends PaymentMethodDetails {
             sepaPaymentMethod.setType(jsonObject.optString(PaymentMethodDetails.TYPE, null));
 
             sepaPaymentMethod.setOwnerName(jsonObject.optString(OWNER_NAME, null));
-            sepaPaymentMethod.setIbanNumber(jsonObject.optString(IBAN_NUMBER, null));
+            sepaPaymentMethod.setIban(jsonObject.optString(IBAN, null));
 
             return sepaPaymentMethod;
         }
     };
 
     private String ownerName;
-    private String ibanNumber;
+    private String iban;
 
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
@@ -83,11 +83,11 @@ public class SepaPaymentMethod extends PaymentMethodDetails {
     }
 
     @Nullable
-    public String getIbanNumber() {
-        return ibanNumber;
+    public String getIban() {
+        return iban;
     }
 
-    public void setIbanNumber(@Nullable String ibanNumber) {
-        this.ibanNumber = ibanNumber;
+    public void setIban(@Nullable String iban) {
+        this.iban = iban;
     }
 }
