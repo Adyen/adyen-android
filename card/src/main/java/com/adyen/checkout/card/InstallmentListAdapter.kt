a04cebed2a600cb9b64160b3d50a2a9749fdfc37
ozgur <6615094+ozgur00@users.noreply.github.com>
2021-10-26 14:25:24 +0200

    Fix copyrights for installment files

--- card/src/main/java/com/adyen/checkout/card/InstallmentListAdapter.kt
+++ card/src/main/java/com/adyen/checkout/card/InstallmentListAdapter.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 26/10/2021.
+ */
+
 package com.adyen.checkout.card
 
 import android.content.Context
