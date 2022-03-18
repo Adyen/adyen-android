fab8b18dcf65be934d19c7750c54c063e357d5e5
Caio Faustino <caio.faustino@adyen.com>
2022-03-18 10:03:51 +0100

    Refactor encoding package to Kotlin.
    Signed-off-by: Caio Faustino <caio.faustino@adyen.com>

--- components-core/src/main/java/com/adyen/checkout/components/encoding/Base64Encoder.kt
+++ components-core/src/main/java/com/adyen/checkout/components/encoding/Base64Encoder.kt
@@ -5,45 +5,25 @@
  *
  * Created by Ran Haveshush on 22/11/2018.
  */
+package com.adyen.checkout.components.encoding
 
-package com.adyen.checkout.components.encoding;
+import android.util.Base64
+import java.nio.charset.Charset
 
-import android.util.Base64;
+object Base64Encoder {
 
-import androidx.annotation.NonNull;
+    private const val UTF_8 = "UTF-8"
+    private val DEFAULT_CHARSET = if (Charset.isSupported(UTF_8)) Charset.forName(UTF_8) else Charset.defaultCharset()
 
-import com.adyen.checkout.core.exception.NoConstructorException;
-
-import java.nio.charset.Charset;
-
-public final class Base64Encoder {
-
-    private static final String UTF_8 = "UTF-8";
-    private static final Charset DEFAULT_CHARSET = Charset.isSupported(UTF_8) ? Charset.forName(UTF_8) : Charset.defaultCharset();
-
-    @NonNull
-    public static String encode(@NonNull String decodedData) {
-        return encode(decodedData, Base64.DEFAULT);
+    @JvmOverloads
+    fun encode(decodedData: String, flags: Int = Base64.DEFAULT): String {
+        val decodedBytes = decodedData.toByteArray(DEFAULT_CHARSET)
+        return Base64.encodeToString(decodedBytes, flags)
     }
 
-    @NonNull
-    public static String encode(@NonNull String decodedData, int flags) {
-        final byte[] decodedBytes = decodedData.getBytes(DEFAULT_CHARSET);
-        return Base64.encodeToString(decodedBytes, flags);
-    }
-
-    @NonNull
-    public static String decode(@NonNull String encodedData) {
-        return decode(encodedData, Base64.DEFAULT);
-    }
-
-    @NonNull
-    public static String decode(@NonNull String encodedData, int flags) {
-        final byte[] decodedBytes = Base64.decode(encodedData, flags);
-        return new String(decodedBytes, DEFAULT_CHARSET);
-    }
-
-    private Base64Encoder() {
-        throw NoConstructorException.INSTANCE;
+    @JvmOverloads
+    fun decode(encodedData: String, flags: Int = Base64.DEFAULT): String {
+        val decodedBytes = Base64.decode(encodedData, flags)
+        return String(decodedBytes, DEFAULT_CHARSET)
     }
 }
