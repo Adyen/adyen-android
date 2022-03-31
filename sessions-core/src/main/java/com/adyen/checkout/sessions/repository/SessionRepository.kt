9d979a83a8c5bcf47c0f93649bb447715b879314
jreij <joseph.jreij@adyen.com>
2022-03-31 12:22:22 +0200

    SessionAPIs: adding session payments API call

--- sessions-core/src/main/java/com/adyen/checkout/sessions/repository/SessionRepository.kt
+++ sessions-core/src/main/java/com/adyen/checkout/sessions/repository/SessionRepository.kt
@@ -11,12 +11,17 @@ package com.adyen.checkout.sessions.repository
 import com.adyen.checkout.components.api.suspendedCall
 import com.adyen.checkout.components.base.Configuration
 import com.adyen.checkout.components.model.payments.request.OrderRequest
+import com.adyen.checkout.components.model.payments.request.PaymentComponentData
+import com.adyen.checkout.components.model.payments.request.PaymentMethodDetails
 import com.adyen.checkout.core.exception.CheckoutException
 import com.adyen.checkout.core.log.LogUtil
 import com.adyen.checkout.core.log.Logger
+import com.adyen.checkout.sessions.api.SessionPaymentsConnection
 import com.adyen.checkout.sessions.api.SessionSetupConnection
 import com.adyen.checkout.sessions.model.Session
+import com.adyen.checkout.sessions.model.request.SessionPaymentsRequest
 import com.adyen.checkout.sessions.model.request.SessionSetupRequest
+import com.adyen.checkout.sessions.model.response.SessionPaymentsResponse
 import com.adyen.checkout.sessions.model.response.SessionSetupResponse
 import java.io.IOException
 import org.json.JSONException
@@ -47,6 +52,29 @@ class SessionRepository {
         } catch (e: JSONException) {
             Logger.e(TAG, "SessionSetupConnection unexpected result", e)
             throw CheckoutException("Unable to setup session")
+        }
+    }
+
+    suspend fun submitPayment(
+        configuration: Configuration,
+        session: Session,
+        paymentComponentData: PaymentComponentData<out PaymentMethodDetails>
+    ): SessionPaymentsResponse {
+        Logger.d(TAG, "Submitting payment")
+        try {
+            val request = SessionPaymentsRequest(session.sessionData.orEmpty(), paymentComponentData)
+            return SessionPaymentsConnection(
+                request = request,
+                environment = configuration.environment,
+                sessionId = session.id,
+                clientKey = configuration.clientKey
+            ).suspendedCall()
+        } catch (e: IOException) {
+            Logger.e(TAG, "SessionPaymentsConnection Failed", e)
+            throw CheckoutException("Unable to submit payment")
+        } catch (e: JSONException) {
+            Logger.e(TAG, "SessionPaymentsConnection unexpected result", e)
+            throw CheckoutException("Unable to submit payment")
         }
     }
 }
