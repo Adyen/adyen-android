0de4bb1182cff6922f157068af423234c8c0a762
Oscar Spruit <oscarspruit@gmail.com>
2022-04-05 16:49:37 +0200

    AbstractInternalNetworking: Create a HttpClient interface to abstract away Connection

--- sessions-core/src/main/java/com/adyen/checkout/sessions/api/SessionDetailsConnection.kt
+++ sessions-core/src/main/java/com/adyen/checkout/sessions/api/SessionDetailsConnection.kt
@@ -15,9 +15,9 @@ import com.adyen.checkout.core.log.Logger
 import com.adyen.checkout.core.model.toStringPretty
 import com.adyen.checkout.sessions.model.payments.SessionDetailsRequest
 import com.adyen.checkout.sessions.model.payments.SessionDetailsResponse
-import java.io.IOException
 import org.json.JSONException
 import org.json.JSONObject
+import java.io.IOException
 
 private val TAG = LogUtil.getTag()
 private const val ENDPOINT = "v1/sessions/"
@@ -35,7 +35,7 @@ class SessionDetailsConnection(
         Logger.v(TAG, "call - $url")
         val requestJson = SessionDetailsRequest.SERIALIZER.serialize(request)
         Logger.v(TAG, "request - ${requestJson.toStringPretty()}")
-        val result = post(CONTENT_TYPE_JSON_HEADER, requestJson.toString().toByteArray(Charsets.UTF_8))
+        val result = post(requestJson.toString(), CONTENT_TYPE_JSON_HEADER)
         val resultJson = JSONObject(String(result, Charsets.UTF_8))
         Logger.v(TAG, "response: ${resultJson.toStringPretty()}")
         return SessionDetailsResponse.SERIALIZER.deserialize(resultJson)
