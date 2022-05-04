65688a6257e2a4533295d9d60a8148e54ec65fa8
ozgur <6615094+ozgur00@users.noreply.github.com>
2022-05-04 15:43:45 +0200

    FullAddressForm: Fix validation issues

--- card/src/main/java/com/adyen/checkout/card/util/AddressValidationUtils.kt
+++ card/src/main/java/com/adyen/checkout/card/util/AddressValidationUtils.kt
@@ -4,6 +4,7 @@ import com.adyen.checkout.card.AddressFormUIState
 import com.adyen.checkout.card.AddressInputModel
 import com.adyen.checkout.card.AddressOutputData
 import com.adyen.checkout.card.R
+import com.adyen.checkout.card.ui.AddressFormInput
 import com.adyen.checkout.components.ui.FieldState
 import com.adyen.checkout.components.ui.Validation
 
@@ -23,7 +24,7 @@ object AddressValidationUtils {
     private fun validatePostalCode(addressInputModel: AddressInputModel): AddressOutputData {
         return with(addressInputModel) {
             AddressOutputData(
-                postalCode = validateAddressField(postalCode),
+                postalCode = validateAddressField(postalCode, true),
                 street = FieldState(street, Validation.Valid),
                 stateOrProvince = FieldState(stateOrProvince, Validation.Valid),
                 houseNumberOrName = FieldState(houseNumberOrName, Validation.Valid),
@@ -35,15 +36,16 @@ object AddressValidationUtils {
     }
 
     private fun validateAddressInput(addressInputModel: AddressInputModel): AddressOutputData {
+        val spec = AddressFormInput.AddressSpecification.fromString(addressInputModel.country)
         return with(addressInputModel) {
             AddressOutputData(
-                postalCode = validateAddressField(postalCode),
-                street = validateAddressField(street),
-                stateOrProvince = validateAddressField(stateOrProvince),
-                houseNumberOrName = validateAddressField(houseNumberOrName),
-                apartmentSuite = FieldState(apartmentSuite, Validation.Valid),
-                city = validateAddressField(city),
-                country = validateAddressField(country)
+                postalCode = validateAddressField(postalCode, spec.postalCode.isRequired),
+                street = validateAddressField(street, spec.street.isRequired),
+                stateOrProvince = validateAddressField(stateOrProvince, spec.stateProvince.isRequired),
+                houseNumberOrName = validateAddressField(houseNumberOrName, spec.houseNumber.isRequired),
+                apartmentSuite = validateAddressField(apartmentSuite, spec.apartmentSuite.isRequired),
+                city = validateAddressField(city, spec.city.isRequired),
+                country = validateAddressField(country, spec.country.isRequired)
             )
         }
     }
@@ -65,8 +67,8 @@ object AddressValidationUtils {
         }
     }
 
-    private fun validateAddressField(input: String): FieldState<String> {
-        return if (input.isNotEmpty()) {
+    private fun validateAddressField(input: String, shouldValidate: Boolean): FieldState<String> {
+        return if (input.isNotEmpty() || !shouldValidate) {
             FieldState(input, Validation.Valid)
         } else {
             FieldState(input, Validation.Invalid(R.string.checkout_address_form_field_not_valid))
