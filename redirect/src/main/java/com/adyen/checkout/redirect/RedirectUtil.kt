d28f468daa418d8f08c98cf9aa05db49712b267c
etman55 <etman850@gmail.com>
2022-06-30 16:33:30 +0200

    fix redirect android 11

--- redirect/src/main/java/com/adyen/checkout/redirect/RedirectUtil.kt
+++ redirect/src/main/java/com/adyen/checkout/redirect/RedirectUtil.kt
@@ -7,15 +7,17 @@
  */
 package com.adyen.checkout.redirect
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import com.adyen.checkout.components.ui.util.ThemeUtil
 import com.adyen.checkout.core.exception.CheckoutException
+import com.adyen.checkout.core.exception.ComponentException
 import com.adyen.checkout.core.log.LogUtil
 import com.adyen.checkout.core.log.Logger
 import org.json.JSONException
@@ -35,30 +37,6 @@ object RedirectUtil {
     private const val PAYMENT_RESULT_PARAMETER = "PaRes"
     private const val MD_PARAMETER = "MD"
     private const val QUERY_STRING_RESULT = "returnUrlQueryString"
-
-    private const val RESOLVER_ACTIVITY_PACKAGE_NAME = "android"
-
-    @SuppressWarnings("TooGenericExceptionCaught")
-    private fun determineResolveResult(context: Context, uri: Uri): ResolveResult {
-        return try {
-            val intent = Intent(Intent.ACTION_VIEW, uri)
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://"))
-            val packageManager = context.packageManager
-            val resolveInfo = packageManager.resolveActivity(intent, 0)
-            val browserInfo = packageManager.resolveActivity(browserIntent, PackageManager.MATCH_DEFAULT_ONLY)
-            val resolvedPackageName = resolveInfo?.activityInfo?.packageName
-            val browserPackageName = browserInfo?.activityInfo?.packageName
-            when (resolvedPackageName) {
-                RESOLVER_ACTIVITY_PACKAGE_NAME -> ResolveResult(ResolveResult.Type.RESOLVER_ACTIVITY, resolveInfo)
-                browserPackageName -> ResolveResult(ResolveResult.Type.DEFAULT_BROWSER, resolveInfo)
-                null -> ResolveResult(ResolveResult.Type.UNKNOWN, null)
-                else -> ResolveResult(ResolveResult.Type.APPLICATION, resolveInfo)
-            }
-        } catch (e: Exception) {
-            Logger.e(TAG, "determineResolveResult exception", e)
-            ResolveResult(ResolveResult.Type.UNKNOWN, null)
-        }
-    }
 
     /**
      * A redirect may return to the application using the ReturnUrl when properly setup in an Intent Filter. Is usually contains result information
@@ -100,35 +78,108 @@ object RedirectUtil {
         }
     }
 
-    /**
-     * Creates the intent that will start the redirect.
-     * @param context Any context.
-     * @param uri The Uri to redirect to.
-     * @return And intent that targets either another app or a Web page.
-     */
-    @JvmStatic
-    fun createRedirectIntent(context: Context, uri: Uri): Intent {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            createCustomTabIntent(context, uri)
-        } else {
-            if (determineResolveResult(context, uri).type == ResolveResult.Type.APPLICATION) {
-                Intent(Intent.ACTION_VIEW, uri)
-            } else {
-                createCustomTabIntent(context, uri)
-            }
+    @Suppress("ReturnCount")
+    fun launchUriRedirect(context: Context, uri: Uri) {
+        if (launchNative(context, uri)) return
+        if (launchWithCustomTabs(context, uri)) return
+        if (launchBrowser(context, uri)) return
+        throw ComponentException("Redirect to app failed.")
+    }
+
+    private fun launchNative(context: Context, uri: Uri): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) launchNativeApi30(context, uri)
+        else launchNativeBeforeApi30(context, uri)
+    }
+
+    private fun launchNativeBeforeApi30(context: Context, uri: Uri): Boolean {
+        val pm = context.packageManager
+
+        // Get all Apps that resolve a generic url
+        val browserActivityIntent = Intent()
+            .setAction(Intent.ACTION_VIEW)
+            .addCategory(Intent.CATEGORY_BROWSABLE)
+            .setData(Uri.fromParts("http", "", null))
+        val genericResolvedSet = pm.queryIntentActivities(browserActivityIntent, 0).map {
+            it.resolvePackageName
+        }.toSet()
+
+        // Get all apps that resolve the specific Url
+        val specializedActivityIntent = Intent(Intent.ACTION_VIEW, uri)
+            .addCategory(Intent.CATEGORY_BROWSABLE)
+        val resolvedSpecializedSet = pm.queryIntentActivities(specializedActivityIntent, 0).map {
+            it.resolvePackageName
+        }.toMutableSet()
+
+        // Keep only the Urls that resolve the specific, but not the generic
+        // urls.
+        resolvedSpecializedSet.removeAll(genericResolvedSet)
+
+        // If the list is empty, no native app handlers were found.
+        if (resolvedSpecializedSet.isEmpty()) {
+            Logger.d(TAG, "launchNativeBeforeApi30 - could not find native app to redirect with")
+            return false
+        }
+
+        // We found native handlers. Launch the Intent.
+        specializedActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(specializedActivityIntent)
+        Logger.d(TAG, "launchNativeBeforeApi30 - redirect successful with native app")
+        return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun launchNativeApi30(context: Context, uri: Uri?): Boolean {
+        val nativeAppIntent = Intent(Intent.ACTION_VIEW, uri)
+            .addCategory(Intent.CATEGORY_BROWSABLE)
+            .addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_REQUIRE_NON_BROWSER
+            )
+        return try {
+            context.startActivity(nativeAppIntent)
+            Logger.d(TAG, "launchNativeApi30 - redirect successful with native app")
+            true
+        } catch (ex: ActivityNotFoundException) {
+            Logger.d(TAG, "launchNativeApi30 - could not find native app to redirect with", ex)
+            false
         }
     }
 
-    private fun createCustomTabIntent(context: Context, uri: Uri): Intent {
+    private fun launchWithCustomTabs(context: Context, uri: Uri): Boolean {
+        // open in custom tabs if there's no native app for the target uri
         val defaultColors = CustomTabColorSchemeParams.Builder()
             .setToolbarColor(ThemeUtil.getPrimaryThemeColor(context))
             .build()
+        return try {
+            CustomTabsIntent.Builder()
+                .setShowTitle(true)
+                .setDefaultColorSchemeParams(defaultColors)
+                .build()
+                .launchUrl(context, uri)
+            Logger.d(TAG, "launchWithCustomTabs - redirect successful with custom tabs")
+            true
+        } catch (e: ActivityNotFoundException) {
+            Logger.d(TAG, "launchWithCustomTabs - device doesn't support custom tabs or chrome is disabled", e)
+            false
+        }
+    }
 
-        val customTabsIntent = CustomTabsIntent.Builder()
-            .setShowTitle(true)
-            .setDefaultColorSchemeParams(defaultColors)
-            .build()
-        customTabsIntent.intent.data = uri
-        return customTabsIntent.intent
+    /**
+     * in case the device doesn't support custom tabs or doesn't support google services (Huawei device).
+     */
+    private fun launchBrowser(context: Context, uri: Uri): Boolean {
+        return try {
+            val browserActivityIntent = Intent()
+                .setAction(Intent.ACTION_VIEW)
+                .addCategory(Intent.CATEGORY_BROWSABLE)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .setData(uri)
+            context.startActivity(browserActivityIntent)
+            Logger.d(TAG, "launchBrowser - redirect successful with browser")
+            true
+        } catch (e: ActivityNotFoundException) {
+            Logger.d(TAG, "launchBrowser - could not do redirect on browser or there's no browser!", e)
+            false
+        }
     }
 }
