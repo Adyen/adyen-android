7bdccb256e5384dad7821439e8121d3891c05096
etman55 <etman850@gmail.com>
2022-08-10 10:47:19 +0200

    OnlineBanking_PL: create online banking module

---
+++ onlinebanking/src/test/java/com/adyen/checkout/onlinebanking/ExampleUnitTest.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 9/8/2022.
+ */
+
+package com.adyen.checkout.onlinebanking
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
