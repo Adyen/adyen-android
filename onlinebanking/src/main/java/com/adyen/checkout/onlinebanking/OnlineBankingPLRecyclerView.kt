0bc28149d830245fd827edbf75da7d8ed755c6bb
etman55 <etman850@gmail.com>
2022-08-12 10:20:29 +0200

    OnlineBanking_PL: create online banking Recycler view

---
+++ onlinebanking/src/main/java/com/adyen/checkout/onlinebanking/OnlineBankingPLRecyclerView.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 10/8/2022.
+ */
+
+package com.adyen.checkout.onlinebanking
+
+import android.content.Context
+import android.util.AttributeSet
+import com.adyen.checkout.components.model.payments.request.OnlineBankingPLPaymentMethods
+import com.adyen.checkout.issuerlist.IssuerListRecyclerView
+
+class OnlineBankingPLRecyclerView : IssuerListRecyclerView<OnlineBankingPLPaymentMethods,OnlineBankingPLComponent> {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}
