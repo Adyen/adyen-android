7fb7d38e7a9d3fd62b9902cbe2d68888722002ac
etman55 <etman850@gmail.com>
2022-08-12 10:20:02 +0200

    OnlineBanking_PL: create online banking component and component provider

---
+++ onlinebanking/src/main/java/com/adyen/checkout/onlinebanking/OnlineBankingPLComponent.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 10/8/2022.
+ */
+
+package com.adyen.checkout.onlinebanking
+
+import androidx.lifecycle.SavedStateHandle
+import com.adyen.checkout.components.PaymentComponentProvider
+import com.adyen.checkout.components.base.GenericPaymentMethodDelegate
+import com.adyen.checkout.components.model.payments.request.OnlineBankingPLPaymentMethods
+import com.adyen.checkout.components.util.PaymentMethodTypes
+import com.adyen.checkout.issuerlist.IssuerListComponent
+import com.adyen.checkout.issuerlist.IssuerListDelegate
+
+class OnlineBankingPLComponent(
+    savedStateHandle: SavedStateHandle,
+    paymentMethodDelegate: GenericPaymentMethodDelegate,
+    issuerListDelegate: IssuerListDelegate<OnlineBankingPLPaymentMethods>,
+    configuration: OnlineBankingPLConfiguration
+) : IssuerListComponent<OnlineBankingPLPaymentMethods>(
+    savedStateHandle,
+    paymentMethodDelegate,
+    issuerListDelegate,
+    configuration
+) {
+    override fun getSupportedPaymentMethodTypes(): Array<String> = PAYMENT_METHOD_TYPES
+
+    companion object {
+        @JvmField
+        val PROVIDER: PaymentComponentProvider<OnlineBankingPLComponent, OnlineBankingPLConfiguration> =
+            OnlineBankingPLComponentProvider()
+        val PAYMENT_METHOD_TYPES = arrayOf(PaymentMethodTypes.ONLINE_BANKING_PL)
+    }
+}
---
+++ onlinebanking/src/main/java/com/adyen/checkout/onlinebanking/OnlineBankingPLComponentProvider.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 10/8/2022.
+ */
+
+package com.adyen.checkout.onlinebanking
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.savedstate.SavedStateRegistryOwner
+import com.adyen.checkout.components.PaymentComponentProvider
+import com.adyen.checkout.components.base.GenericPaymentMethodDelegate
+import com.adyen.checkout.components.base.lifecycle.viewModelFactory
+import com.adyen.checkout.components.model.paymentmethods.PaymentMethod
+import com.adyen.checkout.components.model.payments.request.OnlineBankingPLPaymentMethods
+import com.adyen.checkout.issuerlist.DefaultIssuerListDelegate
+
+class OnlineBankingPLComponentProvider : PaymentComponentProvider<OnlineBankingPLComponent, OnlineBankingPLConfiguration> {
+    override fun get(
+        savedStateRegistryOwner: SavedStateRegistryOwner,
+        viewModelStoreOwner: ViewModelStoreOwner,
+        paymentMethod: PaymentMethod,
+        configuration: OnlineBankingPLConfiguration,
+        defaultArgs: Bundle?
+    ): OnlineBankingPLComponent {
+        val genericFactory: ViewModelProvider.Factory =
+            viewModelFactory(savedStateRegistryOwner, defaultArgs) { savedStateHandle ->
+                val delegate = DefaultIssuerListDelegate(paymentMethod) { OnlineBankingPLPaymentMethods() }
+                OnlineBankingPLComponent(
+                    savedStateHandle,
+                    GenericPaymentMethodDelegate(paymentMethod),
+                    delegate,
+                    configuration
+                )
+            }
+        return ViewModelProvider(viewModelStoreOwner, genericFactory).get(OnlineBankingPLComponent::class.java)
+    }
+}
