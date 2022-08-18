478f44167bcdd6681f9b9f3c41f9af35689fd1b8
jreij <joseph.jreij@adyen.com>
2022-08-18 18:15:22 +0200

    RedirectDelegate: add tests for DefaultRedirectDelegate

---
+++ redirect/src/main/java/com/adyen/checkout/redirect/test/TestRedirectHandler.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by josephj on 18/8/2022.
+ */
+
+package com.adyen.checkout.redirect.test
+
+import android.content.Context
+import android.net.Uri
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.core.exception.ComponentException
+import com.adyen.checkout.redirect.handler.RedirectHandler
+import org.json.JSONObject
+
+/**
+ * Test implementation of [RedirectHandler]. This class should never be used except in test code.
+ */
+// TODO move to test fixtures once it becomes supported on Android
+@RestrictTo(RestrictTo.Scope.TESTS)
+internal class TestRedirectHandler : RedirectHandler {
+
+    var exception: ComponentException? = null
+
+    override fun parseRedirectResult(data: Uri?): JSONObject {
+        exception?.let { throw it }
+        return REDIRECT_RESULT
+    }
+
+    override fun launchUriRedirect(context: Context, url: String?) {
+        exception?.let { throw it }
+    }
+
+    companion object {
+        val REDIRECT_RESULT = JSONObject().apply { put("redirect", "successful") }
+    }
+}
---
+++ redirect/src/test/java/com/adyen/checkout/redirect/DefaultRedirectDelegateTest.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by josephj on 18/8/2022.
+ */
+
+package com.adyen.checkout.redirect
+
+import android.app.Activity
+import android.content.Intent
+import app.cash.turbine.test
+import com.adyen.checkout.components.model.payments.response.RedirectAction
+import com.adyen.checkout.core.exception.ComponentException
+import com.adyen.checkout.redirect.test.TestRedirectHandler
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class DefaultRedirectDelegateTest {
+
+    private lateinit var redirectHandler: TestRedirectHandler
+    private lateinit var delegate: DefaultRedirectDelegate
+
+    @BeforeEach
+    fun beforeEach() {
+        redirectHandler = TestRedirectHandler()
+        delegate = DefaultRedirectDelegate(redirectHandler)
+    }
+
+    @Test
+    fun `when handleAction called and RedirectHandler returns an error, then the error is propagated`() = runTest {
+        val error = ComponentException("Failed to make redirect.")
+        redirectHandler.exception = error
+
+        delegate.exceptionFlow.test {
+            delegate.handleAction(Activity(), RedirectAction())
+
+            assertEquals(error, awaitItem())
+        }
+    }
+
+    @Test
+    fun `when handleAction called with valid data, then no error is propagated`() = runTest {
+        delegate.exceptionFlow.test {
+            delegate.handleAction(Activity(), RedirectAction())
+
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `when handleIntent called and RedirectHandler returns an error, then the error is propagated`() = runTest {
+        val error = ComponentException("Failed to parse redirect result.")
+        redirectHandler.exception = error
+
+        delegate.exceptionFlow.test {
+            delegate.handleIntent(Intent())
+
+            assertEquals(error, awaitItem())
+        }
+    }
+
+    @Test
+    fun `when handleIntent called with valid data, then the details are emitted`() = runTest {
+        delegate.detailsFlow.test {
+            delegate.handleIntent(Intent())
+
+            assertEquals(TestRedirectHandler.REDIRECT_RESULT, awaitItem())
+        }
+    }
+}
