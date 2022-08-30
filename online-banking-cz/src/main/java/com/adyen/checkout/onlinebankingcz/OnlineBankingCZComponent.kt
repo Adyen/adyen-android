707f1ca51f2c1a6b543c4859cb8ed0a9db78882f
etman55 <etman850@gmail.com>
2022-08-30 17:32:50 +0200

    OnlineBankingCZ: add terms and conditions url to OnlineBankingCZComponent.kt

--- online-banking-cz/src/main/java/com/adyen/checkout/onlinebankingcz/OnlineBankingCZComponent.kt
+++ online-banking-cz/src/main/java/com/adyen/checkout/onlinebankingcz/OnlineBankingCZComponent.kt
@@ -28,6 +28,14 @@ class OnlineBankingCZComponent(
     configuration
 ) {
 
+    private var termsAndConditionsUrl: String? = null
+
+    init {
+        termsAndConditionsUrl = (issuerListDelegate as OnlineBankingCZDelegate).getTermsAndConditionsUrl()
+    }
+
+    fun getTermsAndConditionsUrl(): String? = termsAndConditionsUrl
+
     override fun getSupportedPaymentMethodTypes(): Array<String> = PAYMENT_METHOD_TYPES
 
     companion object {
