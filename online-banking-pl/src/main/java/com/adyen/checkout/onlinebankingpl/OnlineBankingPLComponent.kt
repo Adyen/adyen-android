cd3fefe41cd8cf1e7d4e22ca1e7ec4f27e58fc63
etman55 <etman850@gmail.com>
2022-09-21 16:26:53 +0200

    OnlineBankingPL: add OnlineBankingPLComponent

---
+++ online-banking-pl/src/main/java/com/adyen/checkout/onlinebankingpl/OnlineBankingPLComponent.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 21/9/2022.
+ */
+
+package com.adyen.checkout.onlinebankingpl
+
+import androidx.lifecycle.SavedStateHandle
+import com.adyen.checkout.components.PaymentComponentProvider
+import com.adyen.checkout.components.base.GenericPaymentComponentProvider
+import com.adyen.checkout.components.base.GenericPaymentMethodDelegate
+import com.adyen.checkout.components.model.payments.request.OnlineBankingPLPaymentMethod
+import com.adyen.checkout.components.util.PaymentMethodTypes
+import com.adyen.checkout.issuerlist.IssuerListComponent
+
+class OnlineBankingPLComponent(
+        savedStateHandle: SavedStateHandle,
+        paymentMethodDelegate: GenericPaymentMethodDelegate,
+        configuration: OnlineBankingPLConfiguration
+) : IssuerListComponent<OnlineBankingPLPaymentMethod>(
+        savedStateHandle,
+        paymentMethodDelegate,
+        configuration
+) {
+    override fun getSupportedPaymentMethodTypes(): Array<String> = PAYMENT_METHOD_TYPES
+
+    companion object {
+        @JvmField
+        val PROVIDER: PaymentComponentProvider<OnlineBankingPLComponent, OnlineBankingPLConfiguration> =
+                GenericPaymentComponentProvider(OnlineBankingPLComponent::class.java)
+        val PAYMENT_METHOD_TYPES = arrayOf(PaymentMethodTypes.ONLINE_BANKING_PL)
+    }
+
+    override fun instantiateTypedPaymentMethod(): OnlineBankingPLPaymentMethod {
+        return OnlineBankingPLPaymentMethod()
+    }
+
+}
