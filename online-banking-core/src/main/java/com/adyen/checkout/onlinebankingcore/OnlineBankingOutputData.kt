f00c02350dce1b957e1c9bff5c0a086830a7db51
etman55 <etman850@gmail.com>
2022-09-21 14:40:32 +0200

    OnlineBankingCore: remove duplicate copy rights

--- online-banking-core/src/main/java/com/adyen/checkout/onlinebankingcore/OnlineBankingOutputData.kt
+++ online-banking-core/src/main/java/com/adyen/checkout/onlinebankingcore/OnlineBankingOutputData.kt
@@ -6,14 +6,6 @@
  * Created by atef on 20/9/2022.
  */
 
-/*
- * Copyright (c) 2022 Adyen N.V.
- *
- * This file is open source and available under the MIT license. See the LICENSE file for more info.
- *
- * Created by atef on 9/9/2022.
- */
-
 package com.adyen.checkout.onlinebankingcore
 
 import com.adyen.checkout.components.base.OutputData
--- online-banking-core/src/main/java/com/adyen/checkout/onlinebankingcore/OnlineBankingView.kt
+++ online-banking-core/src/main/java/com/adyen/checkout/onlinebankingcore/OnlineBankingView.kt
@@ -5,15 +5,6 @@
  *
  * Created by atef on 20/9/2022.
  */
-
-/*
- * Copyright (c) 2022 Adyen N.V.
- *
- * This file is open source and available under the MIT license. See the LICENSE file for more info.
- *
- * Created by atef on 8/9/2022.
- */
-
 package com.adyen.checkout.onlinebankingcore
 
 import android.content.Context
