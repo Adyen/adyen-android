ec239e1a75f545e4b9f0a992d8bb50622a74514e
etman55 <etman850@gmail.com>
2022-09-22 13:00:19 +0200

    OnlineBankingPL: add missing copy rights

--- online-banking-pl/src/main/java/com/adyen/checkout/onlinebankingpl/OnlineBankingPLSpinnerView.kt
+++ online-banking-pl/src/main/java/com/adyen/checkout/onlinebankingpl/OnlineBankingPLSpinnerView.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 21/9/2022.
+ */
+
 package com.adyen.checkout.onlinebankingpl
 
 import android.content.Context
