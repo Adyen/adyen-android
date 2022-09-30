32ab0a526baa2adb6b0a5839607898482c5ac0cb
jreij <joseph.jreij@adyen.com>
2022-09-30 13:40:38 +0200

    BindGiftCardViewToDelegate: update git card fragment in drop-in

--- drop-in/src/main/java/com/adyen/checkout/dropin/ui/component/GiftCardComponentDialogFragment.kt
+++ drop-in/src/main/java/com/adyen/checkout/dropin/ui/component/GiftCardComponentDialogFragment.kt
@@ -14,25 +14,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import com.adyen.checkout.components.ComponentError
-import com.adyen.checkout.components.ComponentView
 import com.adyen.checkout.components.PaymentComponent
 import com.adyen.checkout.components.PaymentComponentState
 import com.adyen.checkout.components.ViewableComponent
 import com.adyen.checkout.components.base.Configuration
-import com.adyen.checkout.components.base.OutputData
 import com.adyen.checkout.components.model.payments.request.PaymentMethodDetails
 import com.adyen.checkout.core.exception.CheckoutException
 import com.adyen.checkout.core.log.LogUtil
 import com.adyen.checkout.core.log.Logger
 import com.adyen.checkout.dropin.databinding.FragmentGiftcardComponentBinding
-import com.adyen.checkout.dropin.getViewFor
 import com.adyen.checkout.dropin.ui.base.BaseComponentDialogFragment
 import com.adyen.checkout.giftcard.GiftCardComponentState
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class GiftCardComponentDialogFragment : BaseComponentDialogFragment() {
 
-    private lateinit var componentView: ComponentView<in OutputData, ViewableComponent<*, *, *>>
     private var _binding: FragmentGiftcardComponentBinding? = null
     private val binding: FragmentGiftcardComponentBinding get() = requireNotNull(_binding)
 
@@ -46,14 +42,14 @@ class GiftCardComponentDialogFragment : BaseComponentDialogFragment() {
     }
 
     override fun setPaymentPendingInitialization(pending: Boolean) {
-        if (!componentView.isConfirmationRequired) return
+        if (!binding.giftCardView.isConfirmationRequired) return
         binding.redeemButton.isVisible = !pending
         if (pending) binding.progressBar.show()
         else binding.progressBar.hide()
     }
 
     override fun highlightValidationErrors() {
-        componentView.highlightValidationErrors()
+        binding.giftCardView.highlightValidationErrors()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -61,30 +57,28 @@ class GiftCardComponentDialogFragment : BaseComponentDialogFragment() {
         binding.header.text = paymentMethod.name
 
         try {
-            componentView = getViewFor(requireContext(), paymentMethod.type!!)
-            attachComponent(component, componentView)
+            attachComponent(component)
         } catch (e: CheckoutException) {
             handleError(ComponentError(e))
         }
     }
 
     override fun onChanged(paymentComponentState: PaymentComponentState<in PaymentMethodDetails>?) {
-        componentDialogViewModel.componentStateChanged(component.state, componentView.isConfirmationRequired)
+        componentDialogViewModel.componentStateChanged(component.state, binding.giftCardView.isConfirmationRequired)
     }
 
     private fun attachComponent(
         component: PaymentComponent<PaymentComponentState<in PaymentMethodDetails>, Configuration>,
-        componentView: ComponentView<in OutputData, ViewableComponent<*, *, *>>
     ) {
         component.observe(viewLifecycleOwner, this)
         component.observeErrors(viewLifecycleOwner, createErrorHandlerObserver())
-        binding.componentContainer.addView(componentView as View)
-        componentView.attach(component as ViewableComponent<*, *, *>, viewLifecycleOwner)
 
-        if (componentView.isConfirmationRequired) {
+        binding.giftCardView.attach(component as ViewableComponent<*, *, *>, viewLifecycleOwner)
+
+        if (binding.giftCardView.isConfirmationRequired) {
             binding.redeemButton.setOnClickListener { componentDialogViewModel.payButtonClicked() }
             setInitViewState(BottomSheetBehavior.STATE_EXPANDED)
-            (componentView as View).requestFocus()
+            binding.giftCardView.requestFocus()
         } else {
             binding.redeemButton.isVisible = false
         }
