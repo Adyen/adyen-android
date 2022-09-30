b03944fdc49488380c42d3f5e8ed558986b02431
etman55 <etman850@gmail.com>
2022-09-30 15:26:31 +0200

    ViewBinding: add view binding to SEPA

--- sepa/src/main/java/com/adyen/checkout/sepa/SepaView.kt
+++ sepa/src/main/java/com/adyen/checkout/sepa/SepaView.kt
@@ -22,7 +22,7 @@ import com.adyen.checkout.components.ui.view.AdyenTextInputEditText
 import com.adyen.checkout.core.log.LogUtil
 import com.adyen.checkout.core.log.Logger.d
 import com.adyen.checkout.core.log.Logger.v
-import com.google.android.material.textfield.TextInputLayout
+import com.adyen.checkout.sepa.databinding.SepaViewBinding
 
 class SepaView @JvmOverloads constructor(
     context: Context,
@@ -36,48 +36,43 @@ class SepaView @JvmOverloads constructor(
         SepaComponent>(context, attrs, defStyleAttr),
     Observer<SepaOutputData> {
 
-    private lateinit var holderNameInput: TextInputLayout
-    private lateinit var ibanNumberInput: TextInputLayout
+    private val binding: SepaViewBinding = SepaViewBinding.inflate(LayoutInflater.from(getContext()), this)
     private lateinit var holderNameEditText: AdyenTextInputEditText
     private lateinit var ibanNumberEditText: AdyenTextInputEditText
-
     // Regular View constructor
     init {
         orientation = VERTICAL
-        LayoutInflater.from(getContext()).inflate(R.layout.sepa_view, this, true)
         val padding = resources.getDimension(R.dimen.standard_margin).toInt()
         setPadding(padding, padding, padding, 0)
     }
 
     override fun initLocalizedStrings(localizedContext: Context) {
-        holderNameInput.setLocalizedHintFromStyle(R.style.AdyenCheckout_Sepa_HolderNameInput)
-        ibanNumberInput.setLocalizedHintFromStyle(R.style.AdyenCheckout_Sepa_AccountNumberInput)
+        binding.textInputLayoutHolderName.setLocalizedHintFromStyle(R.style.AdyenCheckout_Sepa_HolderNameInput)
+        binding.textInputLayoutIbanNumber.setLocalizedHintFromStyle(R.style.AdyenCheckout_Sepa_AccountNumberInput)
     }
 
     override fun initView() {
-        holderNameInput = findViewById(R.id.textInputLayout_holderName)
-        ibanNumberInput = findViewById(R.id.textInputLayout_ibanNumber)
-        holderNameEditText = holderNameInput.editText as AdyenTextInputEditText
-        ibanNumberEditText = ibanNumberInput.editText as AdyenTextInputEditText
+        holderNameEditText = binding.textInputLayoutHolderName.editText as AdyenTextInputEditText
+        ibanNumberEditText = binding.textInputLayoutIbanNumber.editText as AdyenTextInputEditText
 
         holderNameEditText.setOnChangeListener {
             component.inputData.name = holderNameEditText.rawValue
             notifyInputDataChanged()
-            holderNameInput.error = null
+            binding.textInputLayoutHolderName.error = null
         }
         ibanNumberEditText.setOnChangeListener {
             component.inputData.iban = ibanNumberEditText.rawValue
             notifyInputDataChanged()
-            ibanNumberInput.error = null
+            binding.textInputLayoutIbanNumber.error = null
         }
         ibanNumberEditText.onFocusChangeListener = OnFocusChangeListener { _: View?, hasFocus: Boolean ->
             val outputData = component.outputData
             val ibanNumberValidation = outputData?.ibanNumberField?.validation
             if (hasFocus) {
-                ibanNumberInput.error = null
+                binding.textInputLayoutIbanNumber.error = null
             } else if (ibanNumberValidation != null && !ibanNumberValidation.isValid()) {
                 val errorReasonResId = (ibanNumberValidation as Validation.Invalid).reason
-                ibanNumberInput.error = localizedContext.getString(errorReasonResId)
+                binding.textInputLayoutIbanNumber.error = localizedContext.getString(errorReasonResId)
             }
         }
     }
@@ -104,17 +99,17 @@ class SepaView @JvmOverloads constructor(
         val ownerNameValidation = outputData.ownerNameField.validation
         if (!ownerNameValidation.isValid()) {
             errorFocused = true
-            holderNameInput.requestFocus()
+            binding.textInputLayoutHolderName.requestFocus()
             val errorReasonResId = (ownerNameValidation as Validation.Invalid).reason
-            holderNameInput.error = localizedContext.getString(errorReasonResId)
+            binding.textInputLayoutHolderName.error = localizedContext.getString(errorReasonResId)
         }
         val ibanNumberValidation = outputData.ibanNumberField.validation
         if (!ibanNumberValidation.isValid()) {
             if (!errorFocused) {
-                ibanNumberInput.requestFocus()
+                binding.textInputLayoutIbanNumber.requestFocus()
             }
             val errorReasonResId = (ibanNumberValidation as Validation.Invalid).reason
-            ibanNumberInput.error = localizedContext.getString(errorReasonResId)
+            binding.textInputLayoutIbanNumber.error = localizedContext.getString(errorReasonResId)
         }
     }
 
