f26e02900074f150fd7ca1c5a916158393b9c880
Oscar Spruit <oscarspruit@gmail.com>
2022-12-07 14:36:25 +0100

    Add action configs to GiftCardConfiguration
    COAND-681

--- giftcard/src/main/java/com/adyen/checkout/giftcard/GiftCardConfiguration.kt
+++ giftcard/src/main/java/com/adyen/checkout/giftcard/GiftCardConfiguration.kt
@@ -8,7 +8,8 @@
 package com.adyen.checkout.giftcard
 
 import android.content.Context
-import com.adyen.checkout.components.base.BaseConfigurationBuilder
+import com.adyen.checkout.action.ActionHandlingPaymentMethodConfigurationBuilder
+import com.adyen.checkout.action.GenericActionConfiguration
 import com.adyen.checkout.components.base.Configuration
 import com.adyen.checkout.core.api.Environment
 import kotlinx.parcelize.Parcelize
@@ -20,12 +21,13 @@ class GiftCardConfiguration private constructor(
     override val environment: Environment,
     override val clientKey: String,
     override val isAnalyticsEnabled: Boolean?,
+    internal val genericActionConfiguration: GenericActionConfiguration,
 ) : Configuration {
 
     /**
      * Builder to create a [GiftCardConfiguration].
      */
-    class Builder : BaseConfigurationBuilder<GiftCardConfiguration, Builder> {
+    class Builder : ActionHandlingPaymentMethodConfigurationBuilder<GiftCardConfiguration, Builder> {
 
         /**
          * Constructor for Builder with default values.
@@ -59,6 +61,7 @@ class GiftCardConfiguration private constructor(
                 environment = environment,
                 clientKey = clientKey,
                 isAnalyticsEnabled = isAnalyticsEnabled,
+                genericActionConfiguration = genericActionConfigurationBuilder.build(),
             )
         }
     }
