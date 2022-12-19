a856d02f081afcf88d8a3d35324963662bd98cd9
jreij <joseph.jreij@adyen.com>
2022-12-19 13:41:36 +0100

    Add tests for WeChatPayActionComponent
    COAND-688

---
+++ wechatpay/src/test/java/com/adyen/checkout/wechatpay/WeChatPayActionComponentTest.kt
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2022 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by josephj on 19/12/2022.
+ */
+
+package com.adyen.checkout.wechatpay
+
+import android.app.Activity
+import android.content.Intent
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.viewModelScope
+import app.cash.turbine.test
+import com.adyen.checkout.components.ActionComponentEvent
+import com.adyen.checkout.components.model.payments.response.SdkAction
+import com.adyen.checkout.components.model.payments.response.SdkData
+import com.adyen.checkout.components.test.TestComponentViewType
+import com.adyen.checkout.core.log.Logger
+import com.adyen.checkout.test.TestDispatcherExtension
+import com.adyen.checkout.test.extensions.invokeOnCleared
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@ExtendWith(MockitoExtension::class, TestDispatcherExtension::class)
+internal class WeChatPayActionComponentTest(
+    @Mock private val weChatDelegate: WeChatDelegate,
+) {
+
+    private lateinit var component: WeChatPayActionComponent
+
+    @BeforeEach
+    fun before() {
+        Logger.setLogcatLevel(Logger.NONE)
+
+        whenever(weChatDelegate.viewFlow) doReturn MutableStateFlow(WeChatComponentViewType)
+        component = WeChatPayActionComponent(weChatDelegate)
+    }
+
+    @Test
+    fun `when component is created then delegate is initialized`() {
+        verify(weChatDelegate).initialize(component.viewModelScope)
+    }
+
+    @Test
+    fun `when component is cleared then delegate is cleared`() {
+        component.invokeOnCleared()
+
+        verify(weChatDelegate).onCleared()
+    }
+
+    @Test
+    fun `when observe is called then observe in delegate is called`() {
+        val lifecycleOwner = mock<LifecycleOwner>()
+        val callback: (ActionComponentEvent) -> Unit = {}
+
+        component.observe(lifecycleOwner, callback)
+
+        verify(weChatDelegate).observe(lifecycleOwner, component.viewModelScope, callback)
+    }
+
+    @Test
+    fun `when removeObserver is called then removeObserver in delegate is called`() {
+        component.removeObserver()
+
+        verify(weChatDelegate).removeObserver()
+    }
+
+    @Test
+    fun `when component is initialized then view flow should match delegate view flow`() = runTest {
+        component.viewFlow.test {
+            assertEquals(WeChatComponentViewType, awaitItem())
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `when delegate view flow emits a value then component view flow should match that value`() = runTest {
+        val delegateViewFlow = MutableStateFlow(TestComponentViewType.VIEW_TYPE_1)
+        whenever(weChatDelegate.viewFlow) doReturn delegateViewFlow
+        component = WeChatPayActionComponent(weChatDelegate)
+
+        component.viewFlow.test {
+            assertEquals(TestComponentViewType.VIEW_TYPE_1, awaitItem())
+
+            delegateViewFlow.emit(TestComponentViewType.VIEW_TYPE_2)
+            assertEquals(TestComponentViewType.VIEW_TYPE_2, awaitItem())
+
+            expectNoEvents()
+        }
+    }
+
+    @Test
+    fun `when handleAction is called then handleAction in delegate is called`() {
+        val action = SdkAction<SdkData>()
+        val activity = Activity()
+        component.handleAction(action, activity)
+
+        verify(weChatDelegate).handleAction(action, activity)
+    }
+
+    @Test
+    fun `when handleIntent is called then handleIntent in delegate is called`() {
+        val intent = Intent()
+        component.handleIntent(intent)
+
+        verify(weChatDelegate).handleIntent(intent)
+    }
+}
