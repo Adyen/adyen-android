d8707e53929b0b7f546c6f48ffe9abb66e3da25b
Atef Etman <etman850@gmail.com>
2022-12-21 16:36:30 +0200

    Fix duplicate copy right
    COAND-623

--- online-banking-core/src/test/java/com/adyen/checkout/onlinebankingcore/utils/TestOnlineBankingConfiguration.kt
+++ online-banking-core/src/test/java/com/adyen/checkout/onlinebankingcore/utils/TestOnlineBankingConfiguration.kt
@@ -3,14 +3,6 @@
  *
  * This file is open source and available under the MIT license. See the LICENSE file for more info.
  *
- * Created by josephj on 13/12/2022.
- */
-
-/*
- * Copyright (c) 2022 Adyen N.V.
- *
- * This file is open source and available under the MIT license. See the LICENSE file for more info.
- *
  * Created by josephj on 21/11/2022.
  */
 
@@ -21,8 +13,8 @@ import com.adyen.checkout.components.base.BaseConfigurationBuilder
 import com.adyen.checkout.components.base.Configuration
 import com.adyen.checkout.components.model.payments.Amount
 import com.adyen.checkout.core.api.Environment
-import java.util.Locale
 import kotlinx.parcelize.Parcelize
+import java.util.Locale
 
 @Parcelize
 class TestOnlineBankingConfiguration private constructor(
