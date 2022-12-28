8809bc4e82ef3552d856d1c9f0d2ac8379142a99
jreij <joseph.jreij@adyen.com>
2022-12-28 16:24:28 +0100

    Refactor PreselectedStoredPaymentMethodFragment
    Logic should be moved to the view model, and it should not rely on requiresInput() anymore, but instead we can make use of the component state. When the input is not valid we should show the stored component in a separate screen to allow the shopper to fill the required details (for example CVC). But when the input is valid (for example with setHideCvcStoredCard(true)) the shopper is not required to enter anything so we can skip the next screen step and automatically submit the component from this fragment
    
    COAND-698

--- drop-in/src/main/java/com/adyen/checkout/dropin/ui/viewmodel/PreselectedStoredPaymentViewModel.kt
+++ drop-in/src/main/java/com/adyen/checkout/dropin/ui/viewmodel/PreselectedStoredPaymentViewModel.kt
@@ -8,51 +8,128 @@
 
 package com.adyen.checkout.dropin.ui.viewmodel
 
+import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
+import com.adyen.checkout.components.ComponentError
+import com.adyen.checkout.components.PaymentComponentEvent
+import com.adyen.checkout.components.PaymentComponentState
+import com.adyen.checkout.components.channel.bufferedChannel
 import com.adyen.checkout.components.model.paymentmethods.StoredPaymentMethod
-import com.adyen.checkout.core.log.LogUtil
+import com.adyen.checkout.components.model.payments.Amount
+import com.adyen.checkout.components.util.CurrencyUtils
+import com.adyen.checkout.components.util.isZero
 import com.adyen.checkout.dropin.DropInConfiguration
+import com.adyen.checkout.dropin.R
 import com.adyen.checkout.dropin.ui.paymentmethods.StoredPaymentMethodModel
 import com.adyen.checkout.dropin.ui.stored.mapStoredModel
-import com.adyen.checkout.dropin.ui.viewmodel.PreselectedStoredState.Idle
-import com.adyen.checkout.dropin.ui.viewmodel.PreselectedStoredState.ShowStoredPaymentDialog
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 
 internal class PreselectedStoredPaymentViewModel(
-    storedPaymentMethod: StoredPaymentMethod,
-    private val componentRequiresInput: Boolean,
-    dropInConfiguration: DropInConfiguration,
+    private val storedPaymentMethod: StoredPaymentMethod,
+    private val amount: Amount,
+    private val dropInConfiguration: DropInConfiguration,
 ) : ViewModel() {
 
-    companion object {
-        private val TAG = LogUtil.getTag()
-    }
+    private val _uiStateFlow = MutableStateFlow(getInitialUIState())
+    val uiStateFlow: Flow<PreselectedStoredState> = _uiStateFlow
 
-    private val storedPaymentMethodMutableLiveData = MutableStateFlow(
-        storedPaymentMethod.mapStoredModel(
+    private val eventsChannel: Channel<PreselectedStoredEvent> = bufferedChannel()
+    val eventsFlow: Flow<PreselectedStoredEvent> = eventsChannel.receiveAsFlow()
+
+    private var componentState: PaymentComponentState<*>? = null
+
+    private fun getInitialUIState(): PreselectedStoredState {
+        val storedPaymentMethodModel = storedPaymentMethod.mapStoredModel(
             dropInConfiguration.isRemovingStoredPaymentMethodsEnabled,
             dropInConfiguration.environment,
         )
-    )
-    val storedPaymentLiveData: Flow<StoredPaymentMethodModel> = storedPaymentMethodMutableLiveData
+        return PreselectedStoredState(
+            storedPaymentMethodModel = storedPaymentMethodModel,
+            buttonState = ButtonState.TextOnly(R.string.continue_button)
+        )
+    }
 
-    private val componentFragmentStateMutable = MutableStateFlow<PreselectedStoredState>(Idle)
-    val componentFragmentState: Flow<PreselectedStoredState> = componentFragmentStateMutable
+    fun onPaymentComponentEvent(event: PaymentComponentEvent<*>) {
+        when (event) {
+            is PaymentComponentEvent.StateChanged -> {
+                componentState = event.state
+                val buttonState = getButtonState(event.state)
+                val newState = _uiStateFlow.value.copy(buttonState = buttonState)
+                _uiStateFlow.tryEmit(newState)
+            }
+            is PaymentComponentEvent.Error -> {
+                eventsChannel.trySend(PreselectedStoredEvent.ShowError(event.error))
+            }
+            is PaymentComponentEvent.Submit -> {
+                eventsChannel.trySend(PreselectedStoredEvent.RequestPaymentsCall(event.state))
+            }
+            is PaymentComponentEvent.ActionDetails -> {
+                throw IllegalStateException("This event should not be used in drop-in")
+            }
+        }
+    }
 
-    fun payButtonClicked() {
-        if (componentRequiresInput) {
-            componentFragmentStateMutable.tryEmit(ShowStoredPaymentDialog)
+    private fun getButtonState(state: PaymentComponentState<*>): ButtonState {
+        return when {
+            !state.isInputValid -> {
+                // component requires user input -> use Continue and load the component in a fragment on click
+                ButtonState.TextOnly(R.string.continue_button)
+            }
+            // TODO extract this logic and the one in AdyenComponentView to separate method
+            amount.isZero -> {
+                ButtonState.TextOnly(R.string.confirm_preauthorization)
+            }
+            else -> {
+                val amountString = CurrencyUtils.formatAmount(
+                    amount,
+                    dropInConfiguration.shopperLocale
+                )
+                ButtonState.TextWithAmount(R.string.pay_button_with_value, amountString)
+            }
+        }
+    }
+
+    fun onButtonClicked() {
+        val componentState = componentState ?: return
+
+        if (!componentState.isInputValid) {
+            // component requires user input -> load the stored component in a separate fragment
+            eventsChannel.trySend(PreselectedStoredEvent.ShowStoredPaymentScreen)
             return
         }
-        componentFragmentStateMutable.tryEmit(PreselectedStoredState.Submit)
+
+        // component does not require user input -> we should submit it directly instead of switch to a new screen
+        eventsChannel.trySend(PreselectedStoredEvent.SubmitComponent)
+
+        if (!componentState.isValid) {
+            // component is not yet ready for submitting -> show a loading indicator until the component tells us we can
+            // make the payments call (by returning PaymentComponentEvent.Submit)
+            val newState = _uiStateFlow.value.copy(buttonState = ButtonState.Loading)
+            _uiStateFlow.tryEmit(newState)
+        }
     }
 }
 
-sealed class PreselectedStoredState {
-    object Idle : PreselectedStoredState()
-    object ShowStoredPaymentDialog : PreselectedStoredState()
-    object Submit : PreselectedStoredState()
+internal data class PreselectedStoredState(
+    val storedPaymentMethodModel: StoredPaymentMethodModel,
+    val buttonState: ButtonState,
+)
 
-    override fun toString(): String = this::class.simpleName ?: ""
+internal sealed class ButtonState {
+    object Loading : ButtonState()
+    data class TextOnly(@StringRes val labelResId: Int) : ButtonState()
+    data class TextWithAmount(
+        @StringRes val labelResId: Int,
+        val amountString: String
+    ) : ButtonState()
+}
+
+internal sealed class PreselectedStoredEvent {
+    object ShowStoredPaymentScreen : PreselectedStoredEvent()
+    object SubmitComponent : PreselectedStoredEvent()
+    data class RequestPaymentsCall(val state: PaymentComponentState<*>) : PreselectedStoredEvent()
+    data class ShowError(val componentError: ComponentError) : PreselectedStoredEvent()
 }
