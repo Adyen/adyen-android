aefb35a1c62bd92152488b3fd4d1844c65a754f3
Oscar Spruit <oscarspruit@gmail.com>
2023-01-24 17:17:26 +0100

    Add order to SessionDetailsResponse
    COAND-680

--- sessions-core/src/main/java/com/adyen/checkout/sessions/model/payments/SessionDetailsResponse.kt
+++ sessions-core/src/main/java/com/adyen/checkout/sessions/model/payments/SessionDetailsResponse.kt
@@ -8,6 +8,7 @@
 package com.adyen.checkout.sessions.model.payments
 
 import com.adyen.checkout.components.model.payments.response.Action
+import com.adyen.checkout.components.model.payments.response.OrderResponse
 import com.adyen.checkout.core.exception.ModelSerializationException
 import com.adyen.checkout.core.model.ModelObject
 import com.adyen.checkout.core.model.ModelUtils
@@ -22,6 +23,7 @@ data class SessionDetailsResponse(
     val resultCode: String?,
     val action: Action?,
     val sessionResult: String?,
+    val order: OrderResponse?,
 ) : ModelObject() {
 
     companion object {
@@ -30,6 +32,7 @@ data class SessionDetailsResponse(
         private const val RESULT_CODE = "resultCode"
         private const val ACTION = "action"
         private const val SESSION_RESULT = "sessionResult"
+        private const val ORDER = "order"
 
         @JvmField
         val SERIALIZER: Serializer<SessionDetailsResponse> = object : Serializer<SessionDetailsResponse> {
@@ -41,6 +44,7 @@ data class SessionDetailsResponse(
                         putOpt(RESULT_CODE, modelObject.resultCode)
                         putOpt(ACTION, ModelUtils.serializeOpt(modelObject.action, Action.SERIALIZER))
                         putOpt(SESSION_RESULT, modelObject.sessionResult)
+                        putOpt(ORDER, ModelUtils.serializeOpt(modelObject.order, OrderResponse.SERIALIZER))
                     } catch (e: JSONException) {
                         throw ModelSerializationException(SessionDetailsResponse::class.java, e)
                     }
@@ -54,6 +58,7 @@ data class SessionDetailsResponse(
                     resultCode = jsonObject.optString(RESULT_CODE),
                     action = ModelUtils.deserializeOpt(jsonObject.optJSONObject(ACTION), Action.SERIALIZER),
                     sessionResult = jsonObject.optString(SESSION_RESULT),
+                    order = ModelUtils.deserializeOpt(jsonObject.optJSONObject(ORDER), OrderResponse.SERIALIZER),
                 )
             }
         }
