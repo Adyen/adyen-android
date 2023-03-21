33d5e6823f7dbe259b6abd5ebf0cc6b2e8d11f0f
Oscar Spruit <oscarspruit@gmail.com>
2023-03-21 10:44:32 +0100

    Add missing kdocs for giftcard
    COAND-724
    
    More gift card kdoc

--- giftcard/src/main/java/com/adyen/checkout/giftcard/GiftCardComponent.kt
+++ giftcard/src/main/java/com/adyen/checkout/giftcard/GiftCardComponent.kt
@@ -15,16 +15,15 @@ import com.adyen.checkout.action.internal.DefaultActionHandlingComponent
 import com.adyen.checkout.action.internal.ui.GenericActionDelegate
 import com.adyen.checkout.components.core.BalanceResult
 import com.adyen.checkout.components.core.OrderResponse
+import com.adyen.checkout.components.core.PaymentMethodTypes
 import com.adyen.checkout.components.core.internal.ButtonComponent
 import com.adyen.checkout.components.core.internal.ComponentEventHandler
 import com.adyen.checkout.components.core.internal.PaymentComponent
 import com.adyen.checkout.components.core.internal.PaymentComponentEvent
 import com.adyen.checkout.components.core.internal.toActionCallback
 import com.adyen.checkout.components.core.internal.ui.ComponentDelegate
-import com.adyen.checkout.components.core.PaymentMethodTypes
 import com.adyen.checkout.core.internal.util.LogUtil
 import com.adyen.checkout.core.internal.util.Logger
-import com.adyen.checkout.giftcard.GiftCardComponent.Companion.PROVIDER
 import com.adyen.checkout.giftcard.internal.provider.GiftCardComponentProvider
 import com.adyen.checkout.giftcard.internal.ui.GiftCardDelegate
 import com.adyen.checkout.ui.core.internal.ui.ButtonDelegate
@@ -34,7 +33,7 @@ import com.adyen.checkout.ui.core.internal.util.mergeViewFlows
 import kotlinx.coroutines.flow.Flow
 
 /**
- * Component should not be instantiated directly. Instead use the [PROVIDER] object.
+ * A [PaymentComponent] that supports the 'giftcard' payment method.
  */
 class GiftCardComponent internal constructor(
     private val giftCardDelegate: GiftCardDelegate,
@@ -85,11 +84,25 @@ class GiftCardComponent internal constructor(
             ?: Logger.e(TAG, "Payment component is not interactable, ignoring.")
     }
 
+    /**
+     * Pass the [BalanceResult] you get from the call to the /paymentMethods/balance endpoint of the Checkout API to
+     * continue the gift card flow. You should make this call in the [GiftCardComponentCallback.onBalanceCheck]
+     * callback. Deserialize the response using [BalanceResult.SERIALIZER].
+     *
+     * @param balanceResult The deserialized response from the /paymentMethods/balance endpoint.
+     */
     fun resolveBalanceResult(balanceResult: BalanceResult) {
         (delegate as? GiftCardDelegate)?.resolveBalanceResult(balanceResult)
             ?: Logger.e(TAG, "Payment component is not able to resolve balance result, ignoring.")
     }
 
+    /**
+     * Pass the [OrderResponse] you get from the call to the /orders endpoint of the Checkout API to continue the gift
+     * card flow. You should make this call in the [GiftCardComponentCallback.onRequestOrder] callback. Deserialize the
+     * response using [OrderResponse.SERIALIZER].
+     *
+     * @param orderResponse The deserialized response from the /orders endpoint.
+     */
     fun resolveOrderResponse(orderResponse: OrderResponse) {
         (delegate as? GiftCardDelegate)?.resolveOrderResponse(orderResponse)
             ?: Logger.e(TAG, "Payment component is not able to resolve order response, ignoring.")
