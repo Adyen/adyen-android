77334f886d4b3807cc3d8d0bad1fd68e4d6c2e65
Oscar Spruit <oscarspruit@gmail.com>
2023-03-21 11:03:48 +0100

    Add missing kdocs for mbway
    COAND-724

--- mbway/src/main/java/com/adyen/checkout/mbway/MBWayComponent.kt
+++ mbway/src/main/java/com/adyen/checkout/mbway/MBWayComponent.kt
@@ -13,16 +13,15 @@ import androidx.lifecycle.viewModelScope
 import com.adyen.checkout.action.internal.ActionHandlingComponent
 import com.adyen.checkout.action.internal.DefaultActionHandlingComponent
 import com.adyen.checkout.action.internal.ui.GenericActionDelegate
+import com.adyen.checkout.components.core.PaymentMethodTypes
 import com.adyen.checkout.components.core.internal.ButtonComponent
 import com.adyen.checkout.components.core.internal.ComponentEventHandler
 import com.adyen.checkout.components.core.internal.PaymentComponent
 import com.adyen.checkout.components.core.internal.PaymentComponentEvent
 import com.adyen.checkout.components.core.internal.toActionCallback
 import com.adyen.checkout.components.core.internal.ui.ComponentDelegate
-import com.adyen.checkout.components.core.PaymentMethodTypes
 import com.adyen.checkout.core.internal.util.LogUtil
 import com.adyen.checkout.core.internal.util.Logger
-import com.adyen.checkout.mbway.MBWayComponent.Companion.PROVIDER
 import com.adyen.checkout.mbway.internal.provider.MBWayComponentProvider
 import com.adyen.checkout.mbway.internal.ui.MBWayDelegate
 import com.adyen.checkout.ui.core.internal.ui.ButtonDelegate
@@ -32,7 +31,7 @@ import com.adyen.checkout.ui.core.internal.util.mergeViewFlows
 import kotlinx.coroutines.flow.Flow
 
 /**
- * Component should not be instantiated directly. Instead use the [PROVIDER] object.
+ * A [PaymentComponent] that supports the 'mbway' payment method.
  */
 class MBWayComponent internal constructor(
     private val mbWayDelegate: MBWayDelegate,
