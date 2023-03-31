c7a8a9ade148c0ae96bd12feebc6e8776b5d115a
Atef Etman <etman850@gmail.com>
2023-03-31 14:53:14 +0200

    Add Boleto configuration
    COAND-366

---
+++ boleto/src/main/java/com/adyen/checkout/boleto/BoletoConfiguration.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2023 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 31/3/2023.
+ */
+
+package com.adyen.checkout.boleto
+
+import android.content.Context
+import com.adyen.checkout.action.GenericActionConfiguration
+import com.adyen.checkout.action.internal.ActionHandlingPaymentMethodConfigurationBuilder
+import com.adyen.checkout.components.core.Amount
+import com.adyen.checkout.components.core.internal.ButtonConfiguration
+import com.adyen.checkout.components.core.internal.ButtonConfigurationBuilder
+import com.adyen.checkout.components.core.internal.Configuration
+import com.adyen.checkout.core.Environment
+import kotlinx.parcelize.Parcelize
+import java.util.Locale
+
+/**
+ * Configuration class for the [BoletoComponent].
+ */
+@Parcelize
+@Suppress("LongParameterList")
+class BoletoConfiguration private constructor(
+    override val shopperLocale: Locale,
+    override val environment: Environment,
+    override val clientKey: String,
+    override val isAnalyticsEnabled: Boolean?,
+    override val amount: Amount,
+    override val isSubmitButtonVisible: Boolean?,
+    val genericActionConfiguration: GenericActionConfiguration,
+    val isSendEmailVisible: Boolean?
+) : Configuration, ButtonConfiguration {
+
+    /**
+     * Builder to create a [BoletoConfiguration].
+     */
+    class Builder :
+        ActionHandlingPaymentMethodConfigurationBuilder<BoletoConfiguration, Builder>,
+        ButtonConfigurationBuilder {
+        private var isSubmitButtonVisible: Boolean? = null
+        private var isSendEmailVisible: Boolean? = null
+
+        /**
+         * Constructor for Builder with default values.
+         *
+         * @param context   A context
+         * @param environment   The [Environment] to be used for network calls to Adyen.
+         * @param clientKey Your Client Key used for network calls from the SDK to Adyen.
+         */
+        constructor(context: Context, environment: Environment, clientKey: String) : super(
+            context,
+            environment,
+            clientKey
+        )
+
+        /**
+         * Builder with required parameters.
+         *
+         * @param shopperLocale The Locale of the shopper.
+         * @param environment   The [Environment] to be used for network calls to Adyen.
+         * @param clientKey Your Client Key used for network calls from the SDK to Adyen.
+         */
+        constructor(shopperLocale: Locale, environment: Environment, clientKey: String) : super(
+            shopperLocale,
+            environment,
+            clientKey
+        )
+
+        /**
+         * Sets if submit button will be visible or not.
+         *
+         * Default is True.
+         *
+         * @param isSubmitButtonVisible Is submit button should be visible or not.
+         */
+        override fun setSubmitButtonVisible(isSubmitButtonVisible: Boolean): Builder {
+            this.isSubmitButtonVisible = isSubmitButtonVisible
+            return this
+        }
+
+        /**
+         * Sets the visibility of send email copy filed
+         *
+         * Default value is false
+         * @param isSendEmailVisible
+         */
+        fun setIsSendEmailFieldVisibility(isSendEmailVisible: Boolean): Builder {
+            this.isSendEmailVisible = isSendEmailVisible
+            return this
+        }
+
+        override fun buildInternal() = BoletoConfiguration(
+            shopperLocale = shopperLocale,
+            environment = environment,
+            clientKey = clientKey,
+            isAnalyticsEnabled = isAnalyticsEnabled,
+            amount = amount,
+            isSubmitButtonVisible = isSubmitButtonVisible,
+            genericActionConfiguration = genericActionConfigurationBuilder.build(),
+            isSendEmailVisible = isSendEmailVisible
+        )
+    }
+}
