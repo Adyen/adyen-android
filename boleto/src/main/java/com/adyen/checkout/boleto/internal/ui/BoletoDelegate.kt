b47d4a680a099399ab3c92d8c4b2538ebc14d9a4
Atef Etman <etman850@gmail.com>
2023-03-31 15:15:53 +0200

    Add Boleto delegate interface
    COAND-366

---
+++ boleto/src/main/java/com/adyen/checkout/boleto/internal/ui/BoletoDelegate.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by atef on 31/3/2023.
+ */
+
+package com.adyen.checkout.boleto.internal.ui
+
+import com.adyen.checkout.boleto.BoletoComponentState
+import com.adyen.checkout.boleto.internal.ui.model.BoletoInputData
+import com.adyen.checkout.boleto.internal.ui.model.BoletoOutputData
+import com.adyen.checkout.components.core.internal.ui.PaymentComponentDelegate
+import com.adyen.checkout.ui.core.internal.ui.AddressDelegate
+import com.adyen.checkout.ui.core.internal.ui.ButtonDelegate
+import com.adyen.checkout.ui.core.internal.ui.UIStateDelegate
+import com.adyen.checkout.ui.core.internal.ui.ViewProvidingDelegate
+import kotlinx.coroutines.flow.Flow
+
+internal interface BoletoDelegate :
+    PaymentComponentDelegate<BoletoComponentState>,
+    ViewProvidingDelegate,
+    ButtonDelegate,
+    UIStateDelegate,
+    AddressDelegate {
+
+    val outputData: BoletoOutputData
+
+    val outputDataFlow: Flow<BoletoOutputData>
+
+    val componentStateFlow: Flow<BoletoComponentState>
+
+    fun updateInputData(update: BoletoInputData.() -> Unit)
+
+    fun setInteractionBlocked(isInteractionBlocked: Boolean)
+}
