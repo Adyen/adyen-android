5c5454a59f45efbe8230594a8c85593a54c53254
ozgur <6615094+ozgur00@users.noreply.github.com>
2023-04-06 13:13:07 +0200

    Add docs for SessionsGiftCardComponentCallback
    COAND-719

--- giftcard/src/main/java/com/adyen/checkout/giftcard/SessionsGiftCardComponentCallback.kt
+++ giftcard/src/main/java/com/adyen/checkout/giftcard/SessionsGiftCardComponentCallback.kt
@@ -15,10 +15,37 @@ import com.adyen.checkout.sessions.core.SessionComponentCallback
 import com.adyen.checkout.sessions.core.SessionModel
 import com.adyen.checkout.sessions.core.SessionPaymentResult
 
-// TODO SESSIONS: docs
+/**
+ * Implement this callback to interact with a [GiftCardComponent] initialized with a session.
+ */
 interface SessionsGiftCardComponentCallback : SessionComponentCallback<GiftCardComponentState> {
 
+    /**
+     * Indicates that an order has been created. You can implement this optional callback to access the response
+     * returned from order creation.
+     *
+     * @param orderResponse The order that has been created to make the payment using gift cards.
+     */
     fun onOrder(orderResponse: OrderResponse) = Unit
+
+    /**
+     * Indicates that balance check has been done. You can implement this optional callback to access the response
+     * returned from balance check.
+     *
+     * @param balanceResult The result of the balance for the gift card that has been added.
+     */
     fun onBalance(balanceResult: BalanceResult) = Unit
+
+    /**
+     * Indicates that a partial payment has been done. This means an order for this payment has been created and
+     * part of the amount is still remaining to be paid. This callback provides you with the necessary objects to
+     * be able to create a new session to complete the payment for the remaining amount.
+     *
+     * @param result The result of the payment.
+     * @param order The order that's been created and used to make the partial payment. To complete the payment
+     * for the remaining amount this has to be passed while creating a new session.
+     * @param sessionModel The object that contains the necessary information about the session that's used to make
+     * the payment. To complete the payment for the remaining amount this has to be passed while creating a new session.
+     */
     fun onPartialPayment(result: SessionPaymentResult, order: Order, sessionModel: SessionModel) = Unit
 }
