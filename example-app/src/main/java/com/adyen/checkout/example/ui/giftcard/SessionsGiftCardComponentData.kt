1d519b14c3793c05bfce5d011c874cacd5fce7aa
ozgur <6615094+ozgur00@users.noreply.github.com>
2023-04-25 13:11:47 +0200

    Add copyrights to example gift card integration files
    COAND-719

--- example-app/src/main/java/com/adyen/checkout/example/ui/giftcard/SessionsGiftCardComponentData.kt
+++ example-app/src/main/java/com/adyen/checkout/example/ui/giftcard/SessionsGiftCardComponentData.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 21/4/2023.
+ */
+
 package com.adyen.checkout.example.ui.giftcard
 
 import com.adyen.checkout.components.core.PaymentMethod
