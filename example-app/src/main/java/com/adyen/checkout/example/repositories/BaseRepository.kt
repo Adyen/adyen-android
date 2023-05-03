455095cc4d6af417cd3fa615afba5f3e845da5f1
jreij <joseph.jreij@adyen.com>
2023-05-03 16:56:05 +0200

    Refactor safeApiCall to return null when a call returns an empty body
    COAND-754

--- example-app/src/main/java/com/adyen/checkout/example/repositories/BaseRepository.kt
+++ example-app/src/main/java/com/adyen/checkout/example/repositories/BaseRepository.kt
@@ -14,27 +14,21 @@ import retrofit2.Response
 open class BaseRepository {
 
     suspend fun <T : Any> safeApiCall(call: suspend () -> Response<T>, errorMessage: String = ""): T? {
-
-        val result: Result<T> = safeApiResult(call, errorMessage)
-        var data: T? = null
-
-        when (result) {
-            is Result.Success ->
-                data = result.data
+        return when (val result = safeApiResult(call, errorMessage)) {
+            is Result.Success -> result.data
+            else -> null
         }
-
-        return data
     }
 
     private suspend fun <T : Any> safeApiResult(call: suspend () -> Response<T>, errorMessage: String = ""): Result<T> {
         val response = call.invoke()
-        if (response.isSuccessful) return Result.Success(response.body()!!)
+        if (response.isSuccessful) return Result.Success(response.body())
 
         return Result.Error(response.errorBody())
     }
 }
 
 sealed class Result<out T : Any> {
-    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Success<out T : Any>(val data: T?) : Result<T>()
     data class Error(val exception: ResponseBody?) : Result<Nothing>()
 }
