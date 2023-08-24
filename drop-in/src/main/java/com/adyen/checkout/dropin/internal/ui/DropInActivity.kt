a119a70892d2b1a84b860901cb0fd750ab30ecd3
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2023-08-24 11:59:32 +0200

    Added check to prevent a crash because supportFragmentManager is already destroyed.

--- drop-in/src/main/java/com/adyen/checkout/dropin/internal/ui/DropInActivity.kt
+++ drop-in/src/main/java/com/adyen/checkout/dropin/internal/ui/DropInActivity.kt
@@ -606,7 +606,7 @@ internal class DropInActivity :
     private fun setLoading(showLoading: Boolean) {
         val loadingDialog = getFragmentByTag(LOADING_FRAGMENT_TAG)
         if (showLoading) {
-            if (loadingDialog == null) {
+            if (loadingDialog == null && !supportFragmentManager.isDestroyed) {
                 LoadingDialogFragment.newInstance().show(supportFragmentManager, LOADING_FRAGMENT_TAG)
             }
         } else {
