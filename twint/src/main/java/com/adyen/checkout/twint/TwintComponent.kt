45a62b9cec54252a8997124cc01db1f512b19a78
Oscar Spruit <oscarspruit@gmail.com>
2023-10-18 16:55:16 +0200

    Create twint module
    COAND-806

---
+++ twint/src/main/java/com/adyen/checkout/twint/TwintComponent.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 18/10/2023.
+ */
+
+package com.adyen.checkout.twint
+
+import androidx.lifecycle.ViewModel
+
+class TwintComponent : ViewModel() {
+}
