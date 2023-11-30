28e4144cfbcd7b6625b07fa59c0154d27f152910
Oscar Spruit <oscarspruit@gmail.com>
2023-11-30 17:13:37 +0100

    Add copyright notice to files that missed it
    COAND-826

--- example-app/src/main/java/com/adyen/checkout/example/ui/theme/ExampleTheme.kt
+++ example-app/src/main/java/com/adyen/checkout/example/ui/theme/ExampleTheme.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 30/11/2023.
+ */
+
 package com.adyen.checkout.example.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
