4490b1f33ecfd972a6afb3eaf57147519fa3214a
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2023-12-28 10:01:40 +0100

    Make BacsVoucherView a duplicate of old SimpleVoucherView with Bacs specific layout to not introduce breaking changes.
    COAND-372

--- voucher/src/main/java/com/adyen/checkout/voucher/internal/ui/view/BacsVoucherView.kt
+++ voucher/src/main/java/com/adyen/checkout/voucher/internal/ui/view/BacsVoucherView.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Adyen N.V.
+ * Copyright (c) 2022 Adyen N.V.
  *
  * This file is open source and available under the MIT license. See the LICENSE file for more info.
  *
@@ -10,23 +10,100 @@ package com.adyen.checkout.voucher.internal.ui.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
 import com.adyen.checkout.components.core.internal.ui.ComponentDelegate
+import com.adyen.checkout.core.internal.util.LogUtil
+import com.adyen.checkout.core.internal.util.Logger
+import com.adyen.checkout.ui.core.internal.ui.ComponentView
+import com.adyen.checkout.ui.core.internal.ui.LogoSize
+import com.adyen.checkout.ui.core.internal.ui.loadLogo
 import com.adyen.checkout.ui.core.internal.util.setLocalizedTextFromStyle
 import com.adyen.checkout.voucher.R
+import com.adyen.checkout.voucher.databinding.BacsVoucherViewBinding
+import com.adyen.checkout.voucher.internal.ui.VoucherDelegate
+import com.adyen.checkout.voucher.internal.ui.model.VoucherOutputData
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-// TODO: After removing BacsVoucherView, make sure to make SimpleVoucherView non open, binding to private and remove Bacs styles.
 internal class BacsVoucherView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : SimpleVoucherView(context, attrs, defStyleAttr) {
-    override fun initView(delegate: ComponentDelegate, coroutineScope: CoroutineScope, localizedContext: Context) {
-        super.initView(delegate, coroutineScope, localizedContext)
+) :
+    LinearLayout(
+        context,
+        attrs,
+        defStyleAttr
+    ),
+    ComponentView {
 
+    private val binding: BacsVoucherViewBinding = BacsVoucherViewBinding.inflate(LayoutInflater.from(context), this)
+
+    private lateinit var localizedContext: Context
+
+    private lateinit var delegate: VoucherDelegate
+
+    init {
+        orientation = VERTICAL
+        val padding = resources.getDimension(R.dimen.standard_margin).toInt()
+        setPadding(padding, padding, padding, padding)
+    }
+
+    override fun initView(delegate: ComponentDelegate, coroutineScope: CoroutineScope, localizedContext: Context) {
+        require(delegate is VoucherDelegate) { "Unsupported delegate type" }
+
+        this.delegate = delegate
+
+        this.localizedContext = localizedContext
+        initLocalizedStrings(localizedContext)
+
+        observeDelegate(delegate, coroutineScope)
+
+        binding.textViewDownload.setOnClickListener { delegate.downloadVoucher(context) }
+    }
+
+    private fun initLocalizedStrings(localizedContext: Context) {
         binding.textViewDescription.setLocalizedTextFromStyle(
             R.style.AdyenCheckout_Voucher_Description_Bacs,
             localizedContext
         )
+        binding.textViewDownload.setLocalizedTextFromStyle(
+            R.style.AdyenCheckout_Voucher_DownloadTextAppearance,
+            localizedContext
+        )
+    }
+
+    private fun observeDelegate(delegate: VoucherDelegate, coroutineScope: CoroutineScope) {
+        delegate.outputDataFlow
+            .onEach { outputDataChanged(it) }
+            .launchIn(coroutineScope)
+    }
+
+    private fun outputDataChanged(outputData: VoucherOutputData) {
+        Logger.d(TAG, "outputDataChanged")
+        loadLogo(outputData.paymentMethodType)
+    }
+
+    private fun loadLogo(paymentMethodType: String?) {
+        if (!paymentMethodType.isNullOrEmpty()) {
+            binding.imageViewLogo.loadLogo(
+                environment = delegate.componentParams.environment,
+                txVariant = paymentMethodType,
+                size = LogoSize.MEDIUM,
+            )
+        }
+    }
+
+    override fun highlightValidationErrors() {
+        // No validation required
+    }
+
+    override fun getView(): View = this
+
+    companion object {
+        private val TAG = LogUtil.getTag()
     }
 }
