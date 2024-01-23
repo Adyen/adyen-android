c93f6a99589606537c2dfbb8acf7e1b56adba943
ozgur <6615094+ozgur00@users.noreply.github.com>
2024-01-23 14:16:34 +0100

    Remove address formatting todo
    COAND-730

--- ui-core/src/main/java/com/adyen/checkout/ui/core/internal/ui/model/AddressOutputData.kt
+++ ui-core/src/main/java/com/adyen/checkout/ui/core/internal/ui/model/AddressOutputData.kt
@@ -35,7 +35,6 @@ data class AddressOutputData(
             country.validation.isValid()
 
     override fun toString(): String {
-        // TODO check order with ios
         return listOf(
             street.value,
             houseNumberOrName.value,
