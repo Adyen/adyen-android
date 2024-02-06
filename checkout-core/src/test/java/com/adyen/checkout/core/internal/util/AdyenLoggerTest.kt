fd0cbe663c4dc64716d55ff3bf9b563121fc4470
Oscar Spruit <oscarspruit@gmail.com>
2024-02-06 18:16:35 +0100

    Exclude inline functions from coverage
    COAND-486

--- checkout-core/src/test/java/com/adyen/checkout/core/internal/util/AdyenLoggerTest.kt
+++ checkout-core/src/test/java/com/adyen/checkout/core/internal/util/AdyenLoggerTest.kt
@@ -8,13 +8,18 @@
 
 package com.adyen.checkout.core.internal.util
 
+import android.util.Log
 import com.adyen.checkout.core.AdyenLogLevel
 import com.adyen.checkout.core.AdyenLogger
 import com.adyen.checkout.core.exception.CheckoutException
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertInstanceOf
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class AdyenLoggerTest {
 
@@ -35,6 +40,7 @@ internal class AdyenLoggerTest {
     @Test
     fun `when logger is set, then logging is correctly propagated`() {
         AdyenLogger.setLogger(logger)
+        AdyenLogger.setLogLevel(AdyenLogLevel.VERBOSE)
         val exception = CheckoutException("Test")
 
         adyenLog(AdyenLogLevel.INFO, exception) { "test" }
@@ -61,16 +67,42 @@ internal class AdyenLoggerTest {
         logger.assertLogLevel(AdyenLogLevel.ASSERT)
     }
 
+    @Test
+    fun `when log level is set, then log is not executed`() {
+        AdyenLogger.setLogger(logger)
+        AdyenLogger.setLogLevel(AdyenLogLevel.ERROR)
+
+        adyenLog(AdyenLogLevel.VERBOSE) { "test" }
+
+        logger.assertLogNotCalled()
+    }
+
+    @Suppress("DEPRECATION")
+    @ParameterizedTest
+    @MethodSource("logLevelSource")
+    fun `when old LogLevel is, then it is mapped to AdyenLogLevel`(
+        @Logger.LogLevel oldLogLevel: Int,
+        adyenLogLevel: AdyenLogLevel
+    ) {
+        AdyenLogger.setLogger(logger)
+
+        AdyenLogger.setLogLevel(oldLogLevel)
+
+        logger.assertLogLevel(adyenLogLevel)
+    }
+
     private class TestLogger : AdyenLogger {
 
-        private var logLevel: AdyenLogLevel = AdyenLogLevel.NONE
+        private var minLogLevel: AdyenLogLevel = AdyenLogLevel.NONE
 
         private var lastLog: LogData? = null
 
-        override fun shouldLog(level: AdyenLogLevel): Boolean = true
+        override fun shouldLog(level: AdyenLogLevel): Boolean {
+            return level.priority >= minLogLevel.priority
+        }
 
         override fun setLogLevel(level: AdyenLogLevel) {
-            this.logLevel = level
+            this.minLogLevel = level
         }
 
         override fun log(level: AdyenLogLevel, tag: String, message: String, throwable: Throwable?) {
@@ -78,11 +110,15 @@ internal class AdyenLoggerTest {
         }
 
         fun assertLogLevel(expected: AdyenLogLevel) {
-            assertEquals(expected, logLevel)
+            assertEquals(expected, minLogLevel)
         }
 
         fun assertLogCalled(expected: LogData) {
             assertEquals(expected, lastLog)
+        }
+
+        fun assertLogNotCalled() {
+            assertNull(lastLog)
         }
     }
 
@@ -92,4 +128,19 @@ internal class AdyenLoggerTest {
         val message: String,
         val throwable: Throwable?,
     )
+
+    companion object {
+
+        @Suppress("DEPRECATION")
+        @JvmStatic
+        fun logLevelSource() = listOf(
+            arguments(Log.VERBOSE, AdyenLogLevel.VERBOSE),
+            arguments(Log.DEBUG, AdyenLogLevel.DEBUG),
+            arguments(Log.INFO, AdyenLogLevel.INFO),
+            arguments(Log.WARN, AdyenLogLevel.WARN),
+            arguments(Log.ERROR, AdyenLogLevel.ERROR),
+            arguments(Log.ASSERT, AdyenLogLevel.NONE),
+            arguments(Logger.NONE, AdyenLogLevel.NONE),
+        )
+    }
 }
