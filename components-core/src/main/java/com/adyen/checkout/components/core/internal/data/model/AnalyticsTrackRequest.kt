0d545baa6cf4446e68b921b5c3a58242442f31c8
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2024-02-12 16:27:06 +0100

    Add backend objects for event tracking
    COAND-844

---
+++ components-core/src/main/java/com/adyen/checkout/components/core/internal/data/model/AnalyticsTrackRequest.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 12/2/2024.
+ */
+
+package com.adyen.checkout.components.core.internal.data.model
+
+import com.adyen.checkout.core.exception.ModelSerializationException
+import com.adyen.checkout.core.internal.data.model.ModelObject
+import com.adyen.checkout.core.internal.data.model.ModelUtils.deserializeOptList
+import com.adyen.checkout.core.internal.data.model.getStringOrNull
+import kotlinx.parcelize.Parcelize
+import org.json.JSONException
+import org.json.JSONObject
+
+@Parcelize
+internal data class AnalyticsTrackRequest(
+    val channel: String?,
+    val info: List<AnalyticsTrackInfo>?,
+    val logs: List<AnalyticsTrackLog>?,
+) : ModelObject() {
+    companion object {
+        private const val CHANNEL = "channel"
+        private const val INFO = "info"
+        private const val LOGS = "logs"
+
+        @JvmField
+        val SERIALIZER: Serializer<AnalyticsTrackRequest> = object : Serializer<AnalyticsTrackRequest> {
+            override fun serialize(modelObject: AnalyticsTrackRequest): JSONObject {
+                try {
+                    return JSONObject().apply {
+                        putOpt(CHANNEL, modelObject.channel)
+                        putOpt(INFO, modelObject.info)
+                        putOpt(LOGS, modelObject.logs)
+                    }
+                } catch (e: JSONException) {
+                    throw ModelSerializationException(AnalyticsTrackRequest::class.java, e)
+                }
+            }
+
+            override fun deserialize(jsonObject: JSONObject): AnalyticsTrackRequest {
+                return try {
+                    with(jsonObject) {
+                        AnalyticsTrackRequest(
+                            channel = getStringOrNull(CHANNEL),
+                            info = deserializeOptList(getJSONArray(INFO), AnalyticsTrackInfo.SERIALIZER),
+                            logs = deserializeOptList(getJSONArray(LOGS), AnalyticsTrackLog.SERIALIZER),
+                        )
+                    }
+                } catch (e: JSONException) {
+                    throw ModelSerializationException(AnalyticsTrackRequest::class.java, e)
+                }
+            }
+        }
+    }
+}
