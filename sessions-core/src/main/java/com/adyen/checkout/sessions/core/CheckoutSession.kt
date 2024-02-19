b1aeedc84bed5d9bded20d75de06c4880e236f07
josephj <joseph.jreij@adyen.com>
2024-02-19 11:17:15 +0100

    Allow creating a default configuration from a CheckoutSession
    COAND-853

--- sessions-core/src/main/java/com/adyen/checkout/sessions/core/CheckoutSession.kt
+++ sessions-core/src/main/java/com/adyen/checkout/sessions/core/CheckoutSession.kt
@@ -8,6 +8,8 @@
 
 package com.adyen.checkout.sessions.core
 
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.components.core.CheckoutConfiguration
 import com.adyen.checkout.components.core.Order
 import com.adyen.checkout.components.core.PaymentMethod
 import com.adyen.checkout.core.Environment
@@ -26,5 +28,13 @@ data class CheckoutSession(
         return sessionSetupResponse.paymentMethodsApiResponse?.paymentMethods.orEmpty().firstOrNull {
             it.type == paymentMethodType
         }
+    }
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    fun getConfiguration(): CheckoutConfiguration {
+        return CheckoutConfiguration(
+            environment = environment,
+            clientKey = clientKey,
+        )
     }
 }
