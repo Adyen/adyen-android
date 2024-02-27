691901f3597e49e8d0c0c6c644c35eca66aa67e0
Oscar Spruit <oscarspruit@gmail.com>
2024-02-27 15:05:11 +0100

    Setup interfaces for analytics architecture
    COAND-844

---
+++ components-core/src/main/java/com/adyen/checkout/components/core/internal/analytics/AnalyticsRemoteDataStore.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 27/2/2024.
+ */
+
+package com.adyen.checkout.components.core.internal.analytics
+
+internal interface AnalyticsRemoteDataStore {
+
+    suspend fun fetchCheckoutAttemptId(): String
+
+    suspend fun sendEvents()
+}
