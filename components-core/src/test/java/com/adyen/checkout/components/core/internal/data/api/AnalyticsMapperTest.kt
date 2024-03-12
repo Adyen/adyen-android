a1eecadae46fd0591a12ef65fb74cffcc873ebdb
josephj <joseph.jreij@adyen.com>
2024-03-12 11:59:11 +0100

    Update version number to 5.3.0
    COAND-863

--- components-core/src/test/java/com/adyen/checkout/components/core/internal/data/api/AnalyticsMapperTest.kt
+++ components-core/src/test/java/com/adyen/checkout/components/core/internal/data/api/AnalyticsMapperTest.kt
@@ -119,7 +119,7 @@ internal class AnalyticsMapperTest {
             )
 
             val expected = AnalyticsSetupRequest(
-                version = "5.2.0",
+                version = "5.3.0",
                 channel = "android",
                 platform = "android",
                 locale = "en_US",
