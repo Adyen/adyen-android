5d8320d1e472c3679f9023f44a944607bf1d6e7b
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2024-03-18 16:15:18 +0100

    Change analytics implementation for DefaultEContextDelegate
    COAND-844

--- econtext/src/main/java/com/adyen/checkout/econtext/internal/ui/DefaultEContextDelegate.kt
+++ econtext/src/main/java/com/adyen/checkout/econtext/internal/ui/DefaultEContextDelegate.kt
@@ -17,7 +17,7 @@ import com.adyen.checkout.components.core.PaymentMethod
 import com.adyen.checkout.components.core.PaymentMethodTypes
 import com.adyen.checkout.components.core.internal.PaymentComponentEvent
 import com.adyen.checkout.components.core.internal.PaymentObserverRepository
-import com.adyen.checkout.components.core.internal.data.api.AnalyticsRepository
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
 import com.adyen.checkout.components.core.internal.ui.model.ButtonComponentParams
 import com.adyen.checkout.components.core.internal.ui.model.FieldState
 import com.adyen.checkout.components.core.internal.ui.model.Validation
@@ -36,7 +36,6 @@ import com.adyen.checkout.ui.core.internal.ui.SubmitHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
 
 @Suppress("TooManyFunctions", "LongParameterList")
 internal class DefaultEContextDelegate<
@@ -47,7 +46,7 @@ internal class DefaultEContextDelegate<
     override val componentParams: ButtonComponentParams,
     private val paymentMethod: PaymentMethod,
     private val order: Order?,
-    private val analyticsRepository: AnalyticsRepository,
+    private val analyticsManager: AnalyticsManager,
     private val submitHandler: SubmitHandler<EContextComponentStateT>,
     private val typedPaymentMethodFactory: () -> EContextPaymentMethodT,
     private val componentStateFactory: (
@@ -76,14 +75,12 @@ internal class DefaultEContextDelegate<
 
     override fun initialize(coroutineScope: CoroutineScope) {
         submitHandler.initialize(coroutineScope, componentStateFlow)
-        setupAnalytics(coroutineScope)
+        initializeAnalytics(coroutineScope)
     }
 
-    private fun setupAnalytics(coroutineScope: CoroutineScope) {
-        adyenLog(AdyenLogLevel.VERBOSE) { "setupAnalytics" }
-        coroutineScope.launch {
-            analyticsRepository.setupAnalytics()
-        }
+    private fun initializeAnalytics(coroutineScope: CoroutineScope) {
+        adyenLog(AdyenLogLevel.VERBOSE) { "initializeAnalytics" }
+        analyticsManager.initialize(this, coroutineScope)
     }
 
     override fun updateInputData(update: EContextInputData.() -> Unit) {
@@ -117,7 +114,7 @@ internal class DefaultEContextDelegate<
     ): EContextComponentStateT {
         val eContextPaymentMethod = typedPaymentMethodFactory().apply {
             type = getPaymentMethodType()
-            checkoutAttemptId = analyticsRepository.getCheckoutAttemptId()
+            checkoutAttemptId = analyticsManager.getCheckoutAttemptId()
             firstName = outputData.firstNameState.value
             lastName = outputData.lastNameState.value
             telephoneNumber = outputData.phoneNumberState.value
@@ -195,6 +192,7 @@ internal class DefaultEContextDelegate<
 
     override fun onCleared() {
         removeObserver()
+        analyticsManager.clear(this)
     }
 
     override fun onSubmit() {
