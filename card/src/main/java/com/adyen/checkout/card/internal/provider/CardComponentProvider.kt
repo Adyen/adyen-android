b5722d226d277762fd3fe4eee3907adf4cdcaf01
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2024-03-18 14:48:19 +0100

    Change analytics implementation for DefaultCardDelegate and StoredCardDelegate
    COAND-844

--- card/src/main/java/com/adyen/checkout/card/internal/provider/CardComponentProvider.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/provider/CardComponentProvider.kt
@@ -33,11 +33,9 @@ import com.adyen.checkout.components.core.PaymentMethod
 import com.adyen.checkout.components.core.StoredPaymentMethod
 import com.adyen.checkout.components.core.internal.DefaultComponentEventHandler
 import com.adyen.checkout.components.core.internal.PaymentObserverRepository
-import com.adyen.checkout.components.core.internal.data.api.AnalyticsMapper
-import com.adyen.checkout.components.core.internal.data.api.AnalyticsRepository
-import com.adyen.checkout.components.core.internal.data.api.AnalyticsRepositoryData
-import com.adyen.checkout.components.core.internal.data.api.AnalyticsService
-import com.adyen.checkout.components.core.internal.data.api.DefaultAnalyticsRepository
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManagerFactory
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsSource
 import com.adyen.checkout.components.core.internal.data.api.DefaultPublicKeyRepository
 import com.adyen.checkout.components.core.internal.data.api.PublicKeyService
 import com.adyen.checkout.components.core.internal.provider.PaymentComponentProvider
@@ -71,7 +69,7 @@ class CardComponentProvider
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 constructor(
     private val dropInOverrideParams: DropInOverrideParams? = null,
-    private val analyticsRepository: AnalyticsRepository? = null,
+    private val analyticsManager: AnalyticsManager? = null,
     private val localeProvider: LocaleProvider = LocaleProvider(),
 ) :
     PaymentComponentProvider<
@@ -136,16 +134,11 @@ constructor(
             val addressRepository = DefaultAddressRepository(addressService)
             val cardValidationMapper = CardValidationMapper()
 
-            val analyticsRepository = analyticsRepository ?: DefaultAnalyticsRepository(
-                analyticsRepositoryData = AnalyticsRepositoryData(
-                    application = application,
-                    componentParams = componentParams,
-                    paymentMethod = paymentMethod,
-                ),
-                analyticsService = AnalyticsService(
-                    HttpClientFactory.getAnalyticsHttpClient(componentParams.environment),
-                ),
-                analyticsMapper = AnalyticsMapper(),
+            val analyticsManager = analyticsManager ?: AnalyticsManagerFactory().provide(
+                componentParams = componentParams,
+                application = application,
+                source = AnalyticsSource.PaymentComponent(paymentMethod.type.orEmpty()),
+                sessionId = null,
             )
 
             val cardDelegate = DefaultCardDelegate(
@@ -154,7 +147,7 @@ constructor(
                 componentParams = componentParams,
                 paymentMethod = paymentMethod,
                 order = order,
-                analyticsRepository = analyticsRepository,
+                analyticsManager = analyticsManager,
                 addressRepository = addressRepository,
                 detectCardTypeRepository = detectCardTypeRepository,
                 cardValidationMapper = cardValidationMapper,
@@ -250,17 +243,11 @@ constructor(
             val addressRepository = DefaultAddressRepository(addressService)
             val cardValidationMapper = CardValidationMapper()
 
-            val analyticsRepository = analyticsRepository ?: DefaultAnalyticsRepository(
-                analyticsRepositoryData = AnalyticsRepositoryData(
-                    application = application,
-                    componentParams = componentParams,
-                    paymentMethod = paymentMethod,
-                    sessionId = checkoutSession.sessionSetupResponse.id,
-                ),
-                analyticsService = AnalyticsService(
-                    HttpClientFactory.getAnalyticsHttpClient(componentParams.environment),
-                ),
-                analyticsMapper = AnalyticsMapper(),
+            val analyticsManager = analyticsManager ?: AnalyticsManagerFactory().provide(
+                componentParams = componentParams,
+                application = application,
+                source = AnalyticsSource.PaymentComponent(paymentMethod.type.orEmpty()),
+                sessionId = checkoutSession.sessionSetupResponse.id,
             )
 
             val cardDelegate = DefaultCardDelegate(
@@ -269,7 +256,7 @@ constructor(
                 componentParams = componentParams,
                 paymentMethod = paymentMethod,
                 order = checkoutSession.order,
-                analyticsRepository = analyticsRepository,
+                analyticsManager = analyticsManager,
                 addressRepository = addressRepository,
                 detectCardTypeRepository = detectCardTypeRepository,
                 cardValidationMapper = cardValidationMapper,
@@ -376,16 +363,11 @@ constructor(
             val publicKeyRepository = DefaultPublicKeyRepository(publicKeyService)
             val cardEncryptor = CardEncryptorFactory.provide()
 
-            val analyticsRepository = analyticsRepository ?: DefaultAnalyticsRepository(
-                analyticsRepositoryData = AnalyticsRepositoryData(
-                    application = application,
-                    componentParams = componentParams,
-                    storedPaymentMethod = storedPaymentMethod,
-                ),
-                analyticsService = AnalyticsService(
-                    HttpClientFactory.getAnalyticsHttpClient(componentParams.environment),
-                ),
-                analyticsMapper = AnalyticsMapper(),
+            val analyticsManager = analyticsManager ?: AnalyticsManagerFactory().provide(
+                componentParams = componentParams,
+                application = application,
+                source = AnalyticsSource.PaymentComponent(storedPaymentMethod.type.orEmpty()),
+                sessionId = null,
             )
 
             val cardDelegate = StoredCardDelegate(
@@ -393,7 +375,7 @@ constructor(
                 storedPaymentMethod = storedPaymentMethod,
                 order = order,
                 componentParams = componentParams,
-                analyticsRepository = analyticsRepository,
+                analyticsManager = analyticsManager,
                 cardEncryptor = cardEncryptor,
                 publicKeyRepository = publicKeyRepository,
                 submitHandler = SubmitHandler(savedStateHandle),
@@ -476,17 +458,11 @@ constructor(
             val publicKeyRepository = DefaultPublicKeyRepository(publicKeyService)
             val cardEncryptor = CardEncryptorFactory.provide()
 
-            val analyticsRepository = analyticsRepository ?: DefaultAnalyticsRepository(
-                analyticsRepositoryData = AnalyticsRepositoryData(
-                    application = application,
-                    componentParams = componentParams,
-                    storedPaymentMethod = storedPaymentMethod,
-                    sessionId = checkoutSession.sessionSetupResponse.id,
-                ),
-                analyticsService = AnalyticsService(
-                    HttpClientFactory.getAnalyticsHttpClient(componentParams.environment),
-                ),
-                analyticsMapper = AnalyticsMapper(),
+            val analyticsManager = analyticsManager ?: AnalyticsManagerFactory().provide(
+                componentParams = componentParams,
+                application = application,
+                source = AnalyticsSource.PaymentComponent(storedPaymentMethod.type.orEmpty()),
+                sessionId = checkoutSession.sessionSetupResponse.id,
             )
 
             val cardDelegate = StoredCardDelegate(
@@ -494,7 +470,7 @@ constructor(
                 storedPaymentMethod = storedPaymentMethod,
                 order = checkoutSession.order,
                 componentParams = componentParams,
-                analyticsRepository = analyticsRepository,
+                analyticsManager = analyticsManager,
                 cardEncryptor = cardEncryptor,
                 publicKeyRepository = publicKeyRepository,
                 submitHandler = SubmitHandler(savedStateHandle),
--- card/src/test/java/com/adyen/checkout/card/internal/ui/DefaultCardDelegateTest.kt
+++ card/src/test/java/com/adyen/checkout/card/internal/ui/DefaultCardDelegateTest.kt
@@ -45,7 +45,7 @@ import com.adyen.checkout.components.core.OrderRequest
 import com.adyen.checkout.components.core.PaymentMethod
 import com.adyen.checkout.components.core.PaymentMethodTypes
 import com.adyen.checkout.components.core.internal.PaymentObserverRepository
-import com.adyen.checkout.components.core.internal.data.api.AnalyticsRepository
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
 import com.adyen.checkout.components.core.internal.data.api.PublicKeyRepository
 import com.adyen.checkout.components.core.internal.test.TestPublicKeyRepository
 import com.adyen.checkout.components.core.internal.ui.model.AddressInputModel
@@ -90,7 +90,9 @@ import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import java.util.Locale
@@ -98,7 +100,7 @@ import java.util.Locale
 @OptIn(ExperimentalCoroutinesApi::class)
 @ExtendWith(MockitoExtension::class, TestDispatcherExtension::class)
 internal class DefaultCardDelegateTest(
-    @Mock private val analyticsRepository: AnalyticsRepository,
+    @Mock private val analyticsManager: AnalyticsManager,
     @Mock private val submitHandler: SubmitHandler<CardComponentState>,
     @Mock private val addressLookupDelegate: AddressLookupDelegate,
 ) {
@@ -978,12 +980,6 @@ internal class DefaultCardDelegateTest(
         }
     }
 
-    @Test
-    fun `when delegate is initialized then analytics event is sent`() = runTest {
-        delegate.initialize(CoroutineScope(UnconfinedTestDispatcher()))
-        verify(analyticsRepository).setupAnalytics()
-    }
-
     @Nested
     inner class SubmitButtonVisibilityTest {
 
@@ -1041,8 +1037,14 @@ internal class DefaultCardDelegateTest(
     inner class AnalyticsTest {
 
         @Test
+        fun `when delegate is initialized then analytics manager is initialized`() {
+            delegate.initialize(CoroutineScope(UnconfinedTestDispatcher()))
+            verify(analyticsManager).initialize(eq(delegate), any())
+        }
+
+        @Test
         fun `when component state is valid then PaymentMethodDetails should contain checkoutAttemptId`() = runTest {
-            whenever(analyticsRepository.getCheckoutAttemptId()) doReturn TEST_CHECKOUT_ATTEMPT_ID
+            whenever(analyticsManager.getCheckoutAttemptId()) doReturn TEST_CHECKOUT_ATTEMPT_ID
 
             delegate.initialize(CoroutineScope(UnconfinedTestDispatcher()))
 
@@ -1055,6 +1057,12 @@ internal class DefaultCardDelegateTest(
 
                 assertEquals(TEST_CHECKOUT_ATTEMPT_ID, expectMostRecentItem().data.paymentMethod?.checkoutAttemptId)
             }
+        }
+
+        @Test
+        fun `when delegate is cleared then analytics manager is cleared`() {
+            delegate.onCleared()
+            verify(analyticsManager).clear(eq(delegate))
         }
     }
 
@@ -1210,7 +1218,7 @@ internal class DefaultCardDelegateTest(
         genericEncryptor: BaseGenericEncryptor = this.genericEncryptor,
         configuration: CheckoutConfiguration = createCheckoutConfiguration(),
         paymentMethod: PaymentMethod = PaymentMethod(type = PaymentMethodTypes.SCHEME),
-        analyticsRepository: AnalyticsRepository = this.analyticsRepository,
+        analyticsManager: AnalyticsManager = this.analyticsManager,
         submitHandler: SubmitHandler<CardComponentState> = this.submitHandler,
         order: OrderRequest? = TEST_ORDER,
         addressLookupDelegate: AddressLookupDelegate = this.addressLookupDelegate
@@ -1237,7 +1245,7 @@ internal class DefaultCardDelegateTest(
             detectCardTypeRepository = detectCardTypeRepository,
             cardValidationMapper = cardValidationMapper,
             genericEncryptor = genericEncryptor,
-            analyticsRepository = analyticsRepository,
+            analyticsManager = analyticsManager,
             submitHandler = submitHandler,
             addressLookupDelegate = addressLookupDelegate,
         )
