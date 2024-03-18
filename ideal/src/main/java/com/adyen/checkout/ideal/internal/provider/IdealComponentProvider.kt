4f96ef7cebeb0a4a8dda8d35d451d4f3637f79b8
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2024-03-18 16:07:42 +0100

    Change analytics implementation for DefaultIssuerListDelegate
    COAND-844

--- ideal/src/main/java/com/adyen/checkout/ideal/internal/provider/IdealComponentProvider.kt
+++ ideal/src/main/java/com/adyen/checkout/ideal/internal/provider/IdealComponentProvider.kt
@@ -14,7 +14,7 @@ import com.adyen.checkout.action.core.internal.ui.GenericActionDelegate
 import com.adyen.checkout.components.core.CheckoutConfiguration
 import com.adyen.checkout.components.core.PaymentComponentData
 import com.adyen.checkout.components.core.internal.ComponentEventHandler
-import com.adyen.checkout.components.core.internal.data.api.AnalyticsRepository
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
 import com.adyen.checkout.components.core.internal.ui.model.DropInOverrideParams
 import com.adyen.checkout.components.core.paymentmethod.IdealPaymentMethod
 import com.adyen.checkout.ideal.IdealComponent
@@ -29,11 +29,11 @@ class IdealComponentProvider
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 constructor(
     dropInOverrideParams: DropInOverrideParams? = null,
-    analyticsRepository: AnalyticsRepository? = null,
+    analyticsManager: AnalyticsManager? = null,
 ) : IssuerListComponentProvider<IdealComponent, IdealConfiguration, IdealPaymentMethod, IdealComponentState>(
     componentClass = IdealComponent::class.java,
     dropInOverrideParams = dropInOverrideParams,
-    analyticsRepository = analyticsRepository,
+    analyticsManager = analyticsManager,
 ) {
 
     override fun createComponent(
