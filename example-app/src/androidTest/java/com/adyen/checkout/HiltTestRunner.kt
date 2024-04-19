911e059d66c969394588aedab244406908c409a5
Oscar Spruit <oscar.spruit@adyen.com>
2024-04-19 15:28:21 +0200

    Add github workflow to run UI tests
    COAND-851

--- example-app/src/androidTest/java/com/adyen/checkout/HiltTestRunner.kt
+++ example-app/src/androidTest/java/com/adyen/checkout/HiltTestRunner.kt
@@ -13,6 +13,8 @@ import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
 
+// This class is used from build.gradle
+@Suppress("Unused")
 class HiltTestRunner : AndroidJUnitRunner() {
 
     override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
