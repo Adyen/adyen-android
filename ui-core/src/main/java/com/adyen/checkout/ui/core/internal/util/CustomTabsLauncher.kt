609fc375a7247f77ff0db71f45af519be3e77e9f
josephj <joseph.jreij@adyen.com>
2024-05-06 17:27:54 +0200

    Add separate style for custom tabs
    COAND-907

--- ui-core/src/main/java/com/adyen/checkout/ui/core/internal/util/CustomTabsLauncher.kt
+++ ui-core/src/main/java/com/adyen/checkout/ui/core/internal/util/CustomTabsLauncher.kt
@@ -10,23 +10,22 @@ package com.adyen.checkout.ui.core.internal.util
 
 import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.res.TypedArray
 import android.net.Uri
 import androidx.annotation.RestrictTo
+import androidx.annotation.StyleableRes
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
+import com.adyen.checkout.ui.core.R
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 object CustomTabsLauncher {
     fun launchCustomTab(context: Context, uri: Uri): Boolean {
-        val defaultColors = CustomTabColorSchemeParams.Builder()
-            .setToolbarColor(ThemeUtil.getPrimaryThemeColor(context))
-            .build()
-
         @Suppress("SwallowedException")
         return try {
             CustomTabsIntent.Builder()
                 .setShowTitle(true)
-                .setDefaultColorSchemeParams(defaultColors)
+                .setDefaultColorSchemeParams(getDefaultColorSchemeParams(context))
                 .build()
                 .launchUrl(context, uri)
             true
@@ -34,4 +33,37 @@ object CustomTabsLauncher {
             false
         }
     }
+
+    private fun getDefaultColorSchemeParams(context: Context): CustomTabColorSchemeParams {
+        val typedArray = context.obtainStyledAttributes(
+            R.style.AdyenCheckout_CustomTabs,
+            R.styleable.AdyenCheckoutCustomTabs,
+        )
+        val toolbarColor = typedArray.getColorOrNull(
+            R.styleable.AdyenCheckoutCustomTabs_adyenCustomTabsToolbarColor,
+        )
+        val secondaryToolbarColor = typedArray.getColorOrNull(
+            R.styleable.AdyenCheckoutCustomTabs_adyenCustomTabsSecondaryToolbarColor,
+        )
+        val navigationBarColor = typedArray.getColorOrNull(
+            R.styleable.AdyenCheckoutCustomTabs_adyenCustomTabsNavigationBarColor,
+        )
+        val navigationBarDividerColor = typedArray.getColorOrNull(
+            R.styleable.AdyenCheckoutCustomTabs_adyenCustomTabsNavigationBarDividerColor,
+        )
+        typedArray.recycle()
+
+        return CustomTabColorSchemeParams.Builder().apply {
+            toolbarColor?.let { setToolbarColor(it) }
+            secondaryToolbarColor?.let { setSecondaryToolbarColor(it) }
+            navigationBarColor?.let { setNavigationBarColor(it) }
+            navigationBarDividerColor?.let { setNavigationBarDividerColor(it) }
+        }.build()
+    }
+
+    private fun TypedArray.getColorOrNull(@StyleableRes index: Int): Int? {
+        return getColor(index, COLOR_NOT_DEFINED).takeIf { it != COLOR_NOT_DEFINED }
+    }
+
+    private const val COLOR_NOT_DEFINED = -1
 }
