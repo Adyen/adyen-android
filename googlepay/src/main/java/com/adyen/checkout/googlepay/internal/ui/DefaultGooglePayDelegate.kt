d17400de1c7eae1b719b0b8a0ff2a10f910cbedb
Oscar Spruit <oscar.spruit@adyen.com>
2024-05-15 16:15:27 +0200

    Setup wrapper view and fragment for Google Pay
    COAND-855

--- googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/DefaultGooglePayDelegate.kt
+++ googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/DefaultGooglePayDelegate.kt
@@ -31,6 +31,7 @@ import com.adyen.checkout.googlepay.GooglePayComponentState
 import com.adyen.checkout.googlepay.internal.data.model.GooglePayPaymentMethodModel
 import com.adyen.checkout.googlepay.internal.ui.model.GooglePayComponentParams
 import com.adyen.checkout.googlepay.internal.util.GooglePayUtils
+import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
 import com.google.android.gms.wallet.AutoResolveHelper
 import com.google.android.gms.wallet.PaymentData
 import com.google.android.gms.wallet.Wallet
@@ -59,6 +60,8 @@ internal class DefaultGooglePayDelegate(
 
     private val submitChannel: Channel<GooglePayComponentState> = bufferedChannel()
     override val submitFlow: Flow<GooglePayComponentState> = submitChannel.receiveAsFlow()
+
+    override val viewFlow: Flow<ComponentViewType?> = MutableStateFlow(GooglePayComponentViewType)
 
     override fun initialize(coroutineScope: CoroutineScope) {
         initializeAnalytics(coroutineScope)
