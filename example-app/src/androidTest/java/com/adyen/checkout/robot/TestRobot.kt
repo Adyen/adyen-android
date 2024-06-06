8cbc30bb7096e98a6c9dd1758c7ce9cb3b5b136d
Oscar Spruit <oscar.spruit@adyen.com>
2024-06-06 10:49:29 +0200

    Setup testing robots for main and drop-in
    COAND-852

---
+++ example-app/src/androidTest/java/com/adyen/checkout/robot/TestRobot.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 6/6/2024.
+ */
+
+package com.adyen.checkout.robot
+
+internal interface TestRobot {
+
+    fun verifyIsOnScreen()
+}
