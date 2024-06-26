8f646490cad73132c33dd17f4f6590bbb401c1ad
Oscar Spruit <oscar.spruit@adyen.com>
2024-06-26 10:48:00 +0200

    Add onAvailabilityResult method to component callbacks
    COAND-943

--- components-core/src/main/java/com/adyen/checkout/components/core/ComponentCallback.kt
+++ components-core/src/main/java/com/adyen/checkout/components/core/ComponentCallback.kt
@@ -86,4 +86,11 @@ interface ComponentCallback<T : PaymentComponentState<*>> : BaseComponentCallbac
         // To be optionally overridden
         permissionCallback.onPermissionRequestNotHandled(requiredPermission)
     }
+
+    /**
+     * Should be overridden for components that require additional availability checks, like Google Pay and WeChat pay.
+     *
+     * @param isAvailable If the payment method is available or not.
+     */
+    fun onAvailabilityResult(isAvailable: Boolean) = Unit
 }
