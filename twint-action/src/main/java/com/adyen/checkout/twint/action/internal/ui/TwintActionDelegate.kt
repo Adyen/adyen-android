c8a962318c7fcb5e3afc6b5b7264ec0ea214795e
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-09 16:16:33 +0200

    Duplicate twint module into twint-action
    COAND-927

---
+++ twint-action/src/main/java/com/adyen/checkout/twint/action/internal/ui/TwintActionDelegate.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 18/10/2023.
+ */
+
+package com.adyen.checkout.twint.action.internal.ui
+
+import androidx.annotation.RestrictTo
+import ch.twint.payment.sdk.TwintPayResult
+import com.adyen.checkout.components.core.internal.ui.ActionDelegate
+import com.adyen.checkout.components.core.internal.ui.DetailsEmittingDelegate
+import com.adyen.checkout.components.core.internal.ui.StatusPollingDelegate
+import com.adyen.checkout.ui.core.internal.ui.ViewProvidingDelegate
+import kotlinx.coroutines.flow.Flow
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+interface TwintActionDelegate :
+    ActionDelegate,
+    DetailsEmittingDelegate,
+    StatusPollingDelegate,
+    ViewProvidingDelegate {
+
+    val payEventFlow: Flow<String>
+
+    fun handleTwintResult(result: TwintPayResult)
+}
---
+++ twint-action/src/main/java/com/adyen/checkout/twint/action/internal/ui/TwintActionViewProvider.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 31/10/2023.
+ */
+
+package com.adyen.checkout.twint.action.internal.ui
+
+import android.content.Context
+import android.view.LayoutInflater
+import com.adyen.checkout.ui.core.internal.ui.ComponentView
+import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
+import com.adyen.checkout.ui.core.internal.ui.ViewProvider
+
+internal object TwintActionViewProvider : ViewProvider {
+
+    override fun getView(
+        viewType: ComponentViewType,
+        context: Context,
+    ): ComponentView = when (viewType) {
+        TwintActionComponentViewType -> TwintActionView(context)
+        else -> throw IllegalArgumentException("Unsupported view type")
+    }
+
+    override fun getView(
+        viewType: ComponentViewType,
+        layoutInflater: LayoutInflater
+    ): ComponentView = when (viewType) {
+        TwintActionComponentViewType -> TwintActionView(layoutInflater)
+        else -> throw IllegalArgumentException("Unsupported view type")
+    }
+}
+
+internal object TwintActionComponentViewType : ComponentViewType {
+    override val viewProvider: ViewProvider = TwintActionViewProvider
+}
