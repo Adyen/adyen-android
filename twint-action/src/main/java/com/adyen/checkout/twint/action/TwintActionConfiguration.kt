474a65b11b8df71b50ba4ded715560f0efd9fda5
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-09 16:16:33 +0200

    Duplicate twint module into twint-action
    COAND-927

---
+++ twint-action/src/main/java/com/adyen/checkout/twint/action/TwintActionConfiguration.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 9/7/2024.
+ */
+
+package com.adyen.checkout.twint.action
+
+import android.content.Context
+import com.adyen.checkout.components.core.Amount
+import com.adyen.checkout.components.core.AnalyticsConfiguration
+import com.adyen.checkout.components.core.CheckoutConfiguration
+import com.adyen.checkout.components.core.internal.BaseConfigurationBuilder
+import com.adyen.checkout.components.core.internal.Configuration
+import com.adyen.checkout.components.core.internal.util.CheckoutConfigurationMarker
+import com.adyen.checkout.core.Environment
+import kotlinx.parcelize.Parcelize
+import java.util.Locale
+
+/**
+ * Configuration class for the [TwintActionComponent].
+ */
+@Parcelize
+class TwintActionConfiguration private constructor(
+    override val shopperLocale: Locale?,
+    override val environment: Environment,
+    override val clientKey: String,
+    override val analyticsConfiguration: AnalyticsConfiguration?,
+    override val amount: Amount?,
+) : Configuration {
+
+    class Builder : BaseConfigurationBuilder<TwintActionConfiguration, Builder> {
+
+        /**
+         * Initialize a configuration builder with the required fields.
+         *
+         * The shopper locale will match the value passed to the API with the sessions flow, or the primary user locale
+         * on the device otherwise. Check out the
+         * [Sessions API documentation](https://docs.adyen.com/api-explorer/Checkout/latest/post/sessions) on how to set
+         * this value.
+         *
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        constructor(environment: Environment, clientKey: String) : super(
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Alternative constructor that uses the [context] to fetch the user locale and use it as a shopper locale.
+         *
+         * @param context A context
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        @Suppress("DEPRECATION")
+        @Deprecated("You can omit the context parameter")
+        constructor(context: Context, environment: Environment, clientKey: String) : super(
+            context,
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Initialize a configuration builder with the required fields.
+         *
+         * @param shopperLocale The [Locale] of the shopper.
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        constructor(
+            shopperLocale: Locale,
+            environment: Environment,
+            clientKey: String
+        ) : super(shopperLocale, environment, clientKey)
+
+        override fun buildInternal(): TwintActionConfiguration {
+            return TwintActionConfiguration(
+                shopperLocale = shopperLocale,
+                environment = environment,
+                clientKey = clientKey,
+                analyticsConfiguration = analyticsConfiguration,
+                amount = amount,
+            )
+        }
+    }
+}
+
+fun CheckoutConfiguration.twintAction(
+    configuration: @CheckoutConfigurationMarker TwintActionConfiguration.Builder.() -> Unit = {}
+): CheckoutConfiguration {
+    val config = TwintActionConfiguration.Builder(environment, clientKey)
+        .apply {
+            shopperLocale?.let { setShopperLocale(it) }
+            amount?.let { setAmount(it) }
+            analyticsConfiguration?.let { setAnalyticsConfiguration(it) }
+        }
+        .apply(configuration)
+        .build()
+    addActionConfiguration(config)
+    return this
+}
+
+internal fun CheckoutConfiguration.getTwintActionConfiguration(): TwintActionConfiguration? {
+    return getActionConfiguration(TwintActionConfiguration::class.java)
+}
+
+internal fun TwintActionConfiguration.toCheckoutConfiguration(): CheckoutConfiguration {
+    return CheckoutConfiguration(
+        shopperLocale = shopperLocale,
+        environment = environment,
+        clientKey = clientKey,
+        amount = amount,
+        analyticsConfiguration = analyticsConfiguration,
+    ) {
+        addActionConfiguration(this@toCheckoutConfiguration)
+    }
+}
