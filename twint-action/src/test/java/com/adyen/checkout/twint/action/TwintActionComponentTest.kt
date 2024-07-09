2563771c78d8301ef4e6dda22988adc32fb3d916
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-09 16:36:58 +0200

    Add type aliases for classes previously in twint module
    This keeps it backwards compatible.
    
    COAND-927

--- twint-action/src/test/java/com/adyen/checkout/twint/action/TwintActionComponentTest.kt
+++ twint-action/src/test/java/com/adyen/checkout/twint/action/TwintActionComponentTest.kt
@@ -20,7 +20,7 @@ import com.adyen.checkout.test.extensions.invokeOnCleared
 import com.adyen.checkout.test.extensions.test
 import com.adyen.checkout.twint.action.internal.ui.TwintActionComponentViewType
 import com.adyen.checkout.twint.action.internal.ui.TwintActionDelegate
-import com.adyen.checkout.ui.core.internal.test.TestComponentViewType
+import com.adyen.checkout.ui.core.internal.ui.TestComponentViewType
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
