9ee6d99b58db15f5ba1a106cf83a1ff052bba305
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-10 13:54:10 +0200

    Add TwintComponentParams
    COAND-927

---
+++ twint/src/main/java/com/adyen/checkout/twint/internal/ui/model/TwintComponentParams.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 10/7/2024.
+ */
+
+package com.adyen.checkout.twint.internal.ui.model
+
+import com.adyen.checkout.components.core.internal.ui.model.ButtonParams
+import com.adyen.checkout.components.core.internal.ui.model.CommonComponentParams
+import com.adyen.checkout.components.core.internal.ui.model.ComponentParams
+
+internal data class TwintComponentParams(
+    private val commonComponentParams: CommonComponentParams,
+    override val isSubmitButtonVisible: Boolean,
+    val showStorePaymentField: Boolean,
+    val storePaymentMethod: Boolean,
+) : ComponentParams by commonComponentParams, ButtonParams
