db7b3e3b396a2749cbdc567a5c9de91ccfbc1d36
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-10 15:09:33 +0200

    Setup base for TwintView
    COAND-927

---
+++ twint/src/main/java/com/adyen/checkout/twint/internal/ui/TwintViewProvider.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 10/7/2024.
+ */
+
+package com.adyen.checkout.twint.internal.ui
+
+import android.content.Context
+import com.adyen.checkout.twint.internal.ui.view.TwintView
+import com.adyen.checkout.ui.core.internal.ui.ButtonComponentViewType
+import com.adyen.checkout.ui.core.internal.ui.ComponentView
+import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
+import com.adyen.checkout.ui.core.internal.ui.ViewProvider
+
+internal class TwintViewProvider : ViewProvider {
+
+    override fun getView(viewType: ComponentViewType, context: Context): ComponentView = when (viewType) {
+        TwintComponentViewType -> TwintView(context)
+        else -> throw IllegalArgumentException("Unsupported view type")
+    }
+}
+
+internal object TwintComponentViewType : ButtonComponentViewType {
+
+    override val viewProvider: ViewProvider get() = TwintViewProvider()
+
+    override val buttonTextResId: Int = ButtonComponentViewType.DEFAULT_BUTTON_TEXT_RES_ID
+}
