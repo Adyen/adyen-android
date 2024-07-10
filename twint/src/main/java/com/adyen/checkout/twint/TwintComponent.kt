c71e7e82d4c58495ee85bbadddfb77fd493fc08a
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-10 13:11:55 +0200

    Setup base for TwintComponent
    COAND-927

---
+++ twint/src/main/java/com/adyen/checkout/twint/TwintComponent.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 10/7/2024.
+ */
+
+package com.adyen.checkout.twint
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.adyen.checkout.action.core.internal.ActionHandlingComponent
+import com.adyen.checkout.action.core.internal.DefaultActionHandlingComponent
+import com.adyen.checkout.action.core.internal.ui.GenericActionDelegate
+import com.adyen.checkout.components.core.PaymentMethodTypes
+import com.adyen.checkout.components.core.internal.ButtonComponent
+import com.adyen.checkout.components.core.internal.ComponentEventHandler
+import com.adyen.checkout.components.core.internal.PaymentComponent
+import com.adyen.checkout.components.core.internal.PaymentComponentEvent
+import com.adyen.checkout.components.core.internal.toActionCallback
+import com.adyen.checkout.components.core.internal.ui.ComponentDelegate
+import com.adyen.checkout.core.AdyenLogLevel
+import com.adyen.checkout.core.internal.util.adyenLog
+import com.adyen.checkout.twint.internal.provider.TwintComponentProvider
+import com.adyen.checkout.twint.internal.ui.DefaultTwintDelegate
+import com.adyen.checkout.twint.internal.ui.TwintDelegate
+import com.adyen.checkout.ui.core.internal.ui.ButtonDelegate
+import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
+import com.adyen.checkout.ui.core.internal.ui.ViewableComponent
+import com.adyen.checkout.ui.core.internal.util.mergeViewFlows
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * A [PaymentComponent] that supports the [PaymentMethodTypes.TWINT] payment method.
+ */
+class TwintComponent internal constructor(
+    private val twintDelegate: TwintDelegate,
+    private val genericActionDelegate: GenericActionDelegate,
+    private val actionHandlingComponent: DefaultActionHandlingComponent,
+    internal val componentEventHandler: ComponentEventHandler<TwintComponentState>,
+) : ViewModel(),
+    PaymentComponent,
+    ViewableComponent,
+    ButtonComponent,
+    ActionHandlingComponent by actionHandlingComponent {
+
+    override val delegate: ComponentDelegate get() = actionHandlingComponent.activeDelegate
+
+    override val viewFlow: Flow<ComponentViewType?> = mergeViewFlows(
+        viewModelScope,
+        twintDelegate.viewFlow,
+        genericActionDelegate.viewFlow,
+    )
+
+    init {
+        twintDelegate.initialize(viewModelScope)
+        genericActionDelegate.initialize(viewModelScope)
+        componentEventHandler.initialize(viewModelScope)
+    }
+
+    internal fun observe(
+        lifecycleOwner: LifecycleOwner,
+        callback: (PaymentComponentEvent<TwintComponentState>) -> Unit
+    ) {
+        twintDelegate.observe(lifecycleOwner, viewModelScope, callback)
+        genericActionDelegate.observe(lifecycleOwner, viewModelScope, callback.toActionCallback())
+    }
+
+    internal fun removeObserver() {
+        twintDelegate.removeObserver()
+        genericActionDelegate.removeObserver()
+    }
+
+    override fun setInteractionBlocked(isInteractionBlocked: Boolean) {
+        (delegate as? DefaultTwintDelegate)?.setInteractionBlocked(isInteractionBlocked)
+            ?: adyenLog(AdyenLogLevel.ERROR) { "Payment component is not interactable, ignoring." }
+    }
+
+    override fun isConfirmationRequired(): Boolean =
+        (twintDelegate as? ButtonDelegate)?.isConfirmationRequired() ?: false
+
+    override fun submit() {
+        (delegate as? ButtonDelegate)?.onSubmit()
+            ?: adyenLog(AdyenLogLevel.ERROR) { "Component is currently not submittable, ignoring." }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        adyenLog(AdyenLogLevel.DEBUG) { "onCleared" }
+        twintDelegate.onCleared()
+        genericActionDelegate.onCleared()
+        componentEventHandler.onCleared()
+    }
+
+    companion object {
+
+        @JvmField
+        val PROVIDER = TwintComponentProvider()
+
+        @JvmField
+        val PAYMENT_METHOD_TYPES = listOf(PaymentMethodTypes.TWINT)
+    }
+}
