b91ece5168f8260b74594574ff0c1f3118e40543
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-10 13:11:55 +0200

    Setup base for TwintComponent
    COAND-927

---
+++ twint/src/main/java/com/adyen/checkout/twint/TwintConfiguration.kt
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 10/7/2024.
+ */
+
+package com.adyen.checkout.twint
+
+import android.content.Context
+import com.adyen.checkout.action.core.GenericActionConfiguration
+import com.adyen.checkout.action.core.internal.ActionHandlingPaymentMethodConfigurationBuilder
+import com.adyen.checkout.components.core.Amount
+import com.adyen.checkout.components.core.AnalyticsConfiguration
+import com.adyen.checkout.components.core.CheckoutConfiguration
+import com.adyen.checkout.components.core.PaymentMethodTypes
+import com.adyen.checkout.components.core.internal.ButtonConfiguration
+import com.adyen.checkout.components.core.internal.ButtonConfigurationBuilder
+import com.adyen.checkout.components.core.internal.Configuration
+import com.adyen.checkout.components.core.internal.util.CheckoutConfigurationMarker
+import com.adyen.checkout.core.Environment
+import kotlinx.parcelize.Parcelize
+import java.util.Locale
+
+/**
+ * Configuration class for the [TwintComponent].
+ */
+@Parcelize
+class TwintConfiguration private constructor(
+    override val shopperLocale: Locale?,
+    override val environment: Environment,
+    override val clientKey: String,
+    override val analyticsConfiguration: AnalyticsConfiguration?,
+    override val amount: Amount?,
+    override val isSubmitButtonVisible: Boolean?,
+    val genericActionConfiguration: GenericActionConfiguration,
+    val showStorePaymentField: Boolean?,
+    val storePaymentMethod: Boolean?,
+) : Configuration, ButtonConfiguration {
+
+    class Builder :
+        ActionHandlingPaymentMethodConfigurationBuilder<TwintConfiguration, Builder>,
+        ButtonConfigurationBuilder {
+
+        private var isSubmitButtonVisible: Boolean? = null
+        private var showStorePaymentField: Boolean? = null
+        private var storePaymentMethod: Boolean? = null
+
+        /**
+         * Initialize a configuration builder with the required fields.
+         *
+         * The shopper locale will match the value passed to the API with the sessions flow, or the primary user locale
+         * on the device otherwise. Check out the
+         * [Sessions API documentation](https://docs.adyen.com/api-explorer/Checkout/latest/post/sessions) on how to set
+         * this value.
+         *
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        constructor(environment: Environment, clientKey: String) : super(
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Alternative constructor that uses the [context] to fetch the user locale and use it as a shopper locale.
+         *
+         * @param context A Context
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        @Deprecated("You can omit the context parameter")
+        constructor(context: Context, environment: Environment, clientKey: String) : super(
+            context,
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Builder with parameters for a [TwintConfiguration].
+         *
+         * @param shopperLocale The [Locale] of the shopper.
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        constructor(shopperLocale: Locale, environment: Environment, clientKey: String) : super(
+            shopperLocale,
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Set if the option to store the shopper's account for future payments should be shown as an input field.
+         *
+         * Default is true.
+         *
+         * Not applicable for the sessions flow. Check out the
+         * [Sessions API documentation](https://docs.adyen.com/api-explorer/Checkout/latest/post/sessions) on how to set
+         * this value.
+         *
+         * @param showStorePaymentField [Boolean]
+         * @return [TwintConfiguration.Builder]
+         */
+        fun setShowStorePaymentField(showStorePaymentField: Boolean): Builder {
+            this.showStorePaymentField = showStorePaymentField
+            return this
+        }
+
+        /**
+         * Set if the shopper's account should be stored, when the store payment method switch is not presented to the
+         * shopper.
+         *
+         * Only applicable if [showStorePaymentField] is false.
+         *
+         * Default is false.
+         *
+         * @param storePaymentMethod [Boolean]
+         * @return [TwintConfiguration.Builder]
+         */
+        fun setStorePaymentMethod(storePaymentMethod: Boolean): Builder {
+            this.storePaymentMethod = storePaymentMethod
+            return this
+        }
+
+        /**
+         * Sets if submit button will be visible or not.
+         *
+         * Default is true.
+         *
+         * @param isSubmitButtonVisible If submit button should be visible or not.
+         */
+        override fun setSubmitButtonVisible(isSubmitButtonVisible: Boolean): Builder {
+            this.isSubmitButtonVisible = isSubmitButtonVisible
+            return this
+        }
+
+        override fun buildInternal() = TwintConfiguration(
+            isSubmitButtonVisible = isSubmitButtonVisible,
+            shopperLocale = shopperLocale,
+            environment = environment,
+            clientKey = clientKey,
+            analyticsConfiguration = analyticsConfiguration,
+            amount = amount,
+            genericActionConfiguration = genericActionConfigurationBuilder.build(),
+            showStorePaymentField = showStorePaymentField,
+            storePaymentMethod = storePaymentMethod,
+        )
+    }
+}
+
+fun CheckoutConfiguration.twint(
+    configuration: @CheckoutConfigurationMarker TwintConfiguration.Builder.() -> Unit = {}
+): CheckoutConfiguration {
+    val config = TwintConfiguration.Builder(environment, clientKey)
+        .apply {
+            shopperLocale?.let { setShopperLocale(it) }
+            amount?.let { setAmount(it) }
+            analyticsConfiguration?.let { setAnalyticsConfiguration(it) }
+        }
+        .apply(configuration)
+        .build()
+    addConfiguration(PaymentMethodTypes.TWINT, config)
+    return this
+}
+
+internal fun CheckoutConfiguration.getTwintConfiguration(): TwintConfiguration? {
+    return getConfiguration(PaymentMethodTypes.TWINT)
+}
+
+internal fun TwintConfiguration.toCheckoutConfiguration(): CheckoutConfiguration {
+    return CheckoutConfiguration(
+        shopperLocale = shopperLocale,
+        environment = environment,
+        clientKey = clientKey,
+        amount = amount,
+        analyticsConfiguration = analyticsConfiguration,
+    ) {
+        addConfiguration(PaymentMethodTypes.TWINT, this@toCheckoutConfiguration)
+
+        genericActionConfiguration.getAllConfigurations().forEach {
+            addActionConfiguration(it)
+        }
+    }
+}
---
+++ twint/src/main/java/com/adyen/checkout/twint/internal/ui/DefaultTwintDelegate.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 10/7/2024.
+ */
+
+package com.adyen.checkout.twint.internal.ui
+
+import androidx.lifecycle.LifecycleOwner
+import com.adyen.checkout.components.core.OrderRequest
+import com.adyen.checkout.components.core.PaymentMethod
+import com.adyen.checkout.components.core.PaymentMethodTypes
+import com.adyen.checkout.components.core.internal.PaymentComponentEvent
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
+import com.adyen.checkout.components.core.internal.ui.model.ComponentParams
+import com.adyen.checkout.twint.TwintComponentState
+import com.adyen.checkout.ui.core.internal.ui.ButtonDelegate
+import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
+import com.adyen.checkout.ui.core.internal.ui.SubmitHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+
+internal class DefaultTwintDelegate(
+    private val submitHandler: SubmitHandler<TwintComponentState>,
+    private val analyticsManager: AnalyticsManager,
+    private val paymentMethod: PaymentMethod,
+    private val order: OrderRequest?,
+    override val componentParams: ComponentParams,
+) : TwintDelegate, ButtonDelegate {
+
+    // TODO
+    private val _viewFlow: MutableStateFlow<ComponentViewType?> = MutableStateFlow(null)
+    override val viewFlow: Flow<ComponentViewType?> = _viewFlow
+
+    override val submitFlow: Flow<TwintComponentState> = submitHandler.submitFlow
+
+    override fun initialize(coroutineScope: CoroutineScope) {
+
+    }
+
+    override fun observe(
+        lifecycleOwner: LifecycleOwner,
+        coroutineScope: CoroutineScope,
+        callback: (PaymentComponentEvent<TwintComponentState>) -> Unit
+    ) {
+
+    }
+
+    override fun removeObserver() {
+
+    }
+
+    override fun onSubmit() {
+
+    }
+
+    override fun isConfirmationRequired(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun shouldShowSubmitButton(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun shouldEnableSubmitButton(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    internal fun setInteractionBlocked(isInteractionBlocked: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPaymentMethodType(): String =
+        paymentMethod.type ?: PaymentMethodTypes.UNKNOWN
+
+    override fun onCleared() {
+        removeObserver()
+    }
+}
