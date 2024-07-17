7c74dac928e78f41354b9db281038b15dd0fd3e7
ozgur <6615094+ozgur00@users.noreply.github.com>
2024-07-17 13:12:35 +0200

    Implement MealVoucherComponentProvider and MealVoucherComponent
    COAND-683

--- mealvoucher/src/main/java/com/adyen/checkout/mealvoucher/MealVoucherComponent.kt
+++ mealvoucher/src/main/java/com/adyen/checkout/mealvoucher/MealVoucherComponent.kt
@@ -8,7 +8,30 @@
 
 package com.adyen.checkout.mealvoucher
 
+import com.adyen.checkout.action.core.internal.DefaultActionHandlingComponent
+import com.adyen.checkout.action.core.internal.ui.GenericActionDelegate
+import com.adyen.checkout.components.core.PaymentMethodTypes
+import com.adyen.checkout.components.core.internal.ComponentEventHandler
 import com.adyen.checkout.giftcard.GiftCardComponent
+import com.adyen.checkout.giftcard.internal.ui.GiftCardDelegate
+import com.adyen.checkout.mealvoucher.internal.provider.MealVoucherComponentProvider
 
-// TODO extend from giftcard
-typealias MealVoucherComponent = GiftCardComponent
+class MealVoucherComponent internal constructor(
+    giftCardDelegate: GiftCardDelegate,
+    genericActionDelegate: GenericActionDelegate,
+    actionHandlingComponent: DefaultActionHandlingComponent,
+    internal val componentEventHandler: ComponentEventHandler<MealVoucherComponentState>,
+) : GiftCardComponent(giftCardDelegate, genericActionDelegate, actionHandlingComponent, componentEventHandler) {
+    companion object {
+        @JvmField
+        val PROVIDER = MealVoucherComponentProvider()
+
+        // TODO update it to correct txVariants
+        @JvmField
+        val PAYMENT_METHOD_TYPES = listOf(
+            PaymentMethodTypes.MEAL_VOUCHER_FR_GROUPEUP,
+            PaymentMethodTypes.MEAL_VOUCHER_FR_NATIXIS,
+            PaymentMethodTypes.MEAL_VOUCHER_FR_SODEXO,
+        )
+    }
+}
