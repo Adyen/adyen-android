c14d443d5c00c752d3769df11a6e1775b43202c8
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2024-07-23 15:06:52 +0200

    Add expiry date to DefaultGiftCardDelegate
    COAND-683

--- giftcard/src/main/java/com/adyen/checkout/giftcard/internal/ui/model/GiftCardInputData.kt
+++ giftcard/src/main/java/com/adyen/checkout/giftcard/internal/ui/model/GiftCardInputData.kt
@@ -10,9 +10,11 @@ package com.adyen.checkout.giftcard.internal.ui.model
 
 import androidx.annotation.RestrictTo
 import com.adyen.checkout.components.core.internal.ui.model.InputData
+import com.adyen.checkout.ui.core.internal.ui.model.ExpiryDate
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 data class GiftCardInputData(
     var cardNumber: String = "",
-    var pin: String = ""
+    var pin: String = "",
+    var expiryDate: ExpiryDate = ExpiryDate.EMPTY_DATE,
 ) : InputData
