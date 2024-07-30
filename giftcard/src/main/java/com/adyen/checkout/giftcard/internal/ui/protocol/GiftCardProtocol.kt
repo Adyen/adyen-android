b7f3c75390fd4934485842586fc43b5ffd7ad392
ozgur <6615094+ozgur00@users.noreply.github.com>
2024-07-30 14:28:27 +0200

    Create GiftCardProtocol
    GiftCardProtocol is used to make and get necessary details for different Gift Card flows such as gift cards and meal vouchers.
    
    COAND-693

---
+++ giftcard/src/main/java/com/adyen/checkout/giftcard/internal/ui/protocol/GiftCardProtocol.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 30/7/2024.
+ */
+
+package com.adyen.checkout.giftcard.internal.ui.protocol
+
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.components.core.PaymentMethod
+import com.adyen.checkout.components.core.paymentmethod.GiftCardPaymentMethod
+import com.adyen.checkout.cse.EncryptedCard
+import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+interface GiftCardProtocol {
+
+    fun getComponentViewType(): ComponentViewType
+
+    fun createPaymentMethod(
+        paymentMethod: PaymentMethod,
+        encryptedCard: EncryptedCard,
+        checkoutAttemptId: String?
+    ): GiftCardPaymentMethod
+}
