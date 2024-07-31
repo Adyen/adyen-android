b81154453acaf5ff2bcecb45bda4768ad81826d5
Oscar Spruit <oscar.spruit@adyen.com>
2024-07-31 11:51:39 +0200

    Create basic StoredTwintDelegate
    COAND-927

---
+++ twint/src/main/java/com/adyen/checkout/twint/internal/ui/StoredTwintDelegate.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 31/7/2024.
+ */
+
+package com.adyen.checkout.twint.internal.ui
+
+import androidx.lifecycle.LifecycleOwner
+import com.adyen.checkout.components.core.OrderRequest
+import com.adyen.checkout.components.core.PaymentMethodTypes
+import com.adyen.checkout.components.core.StoredPaymentMethod
+import com.adyen.checkout.components.core.internal.PaymentComponentEvent
+import com.adyen.checkout.components.core.internal.PaymentObserverRepository
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
+import com.adyen.checkout.twint.TwintComponentState
+import com.adyen.checkout.twint.internal.ui.model.TwintComponentParams
+import com.adyen.checkout.twint.internal.ui.model.TwintInputData
+import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+
+internal class StoredTwintDelegate(
+    private val analyticsManager: AnalyticsManager,
+    private val observerRepository: PaymentObserverRepository,
+    private val paymentMethod: StoredPaymentMethod,
+    private val order: OrderRequest?,
+    override val componentParams: TwintComponentParams,
+) : TwintDelegate {
+
+    override val componentStateFlow: Flow<TwintComponentState> = flow { }
+
+    override val viewFlow: Flow<ComponentViewType?> = MutableStateFlow(null)
+
+    override val submitFlow: Flow<TwintComponentState> = flow { }
+
+    override fun initialize(coroutineScope: CoroutineScope) {
+        // TODO
+    }
+
+    override fun observe(
+        lifecycleOwner: LifecycleOwner,
+        coroutineScope: CoroutineScope,
+        callback: (PaymentComponentEvent<TwintComponentState>) -> Unit
+    ) {
+        // TODO
+    }
+
+    override fun removeObserver() {
+        // TODO
+    }
+
+    override fun updateInputData(update: TwintInputData.() -> Unit) {
+        // TODO
+    }
+
+    override fun getPaymentMethodType(): String {
+        return paymentMethod.type ?: PaymentMethodTypes.UNKNOWN
+    }
+
+    override fun onCleared() {
+        removeObserver()
+        analyticsManager.clear(this)
+    }
+}
