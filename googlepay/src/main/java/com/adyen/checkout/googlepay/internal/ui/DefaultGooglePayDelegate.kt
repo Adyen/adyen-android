6f579b1536058aeef52d1e80a76a8a90c8d5ff57
Oscar Spruit <oscar.spruit@adyen.com>
2024-10-10 10:01:06 +0200

    Support blocking interaction for GooglePayComponent
    COAND-942

--- googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/DefaultGooglePayDelegate.kt
+++ googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/DefaultGooglePayDelegate.kt
@@ -242,6 +242,10 @@ internal class DefaultGooglePayDelegate(
 
     override fun shouldShowSubmitButton(): Boolean = isConfirmationRequired() && componentParams.isSubmitButtonVisible
 
+    internal fun setInteractionBlocked(isInteractionBlocked: Boolean) {
+        submitHandler.setInteractionBlocked(isInteractionBlocked)
+    }
+
     override fun getPaymentMethodType(): String {
         return paymentMethod.type ?: PaymentMethodTypes.UNKNOWN
     }
