74f8f7e33bdb8deacbbf779acfe3d88ad152b315
ozgur <6615094+ozgur00@users.noreply.github.com>
2024-10-21 11:02:07 +0200

    Create paybybank-us component files
    COAND-974

---
+++ paybybank-us/src/main/java/com/adyen/checkout/paybybankus/internal/ui/model/PayByBankUSInputData.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 21/10/2024.
+ */
+
+package com.adyen.checkout.paybybankus.internal.ui.model
+
+class PayByBankUSInputData {
+    // TODO implement
+}
