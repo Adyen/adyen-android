5d0b797293ee8fe3b72a845fa692177794876333
Oscar Spruit <oscar.spruit@adyen.com>
2024-10-30 15:05:44 +0100

    Check Google Pay availability when creating the component
    COAND-943

---
+++ components-core/src/main/java/com/adyen/checkout/components/core/PaymentMethodUnavailableException.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 30/10/2024.
+ */
+
+package com.adyen.checkout.components.core
+
+import com.adyen.checkout.core.exception.CheckoutException
+
+open class PaymentMethodUnavailableException(
+    message: String,
+    cause: Throwable? = null
+) : CheckoutException(message, cause)
