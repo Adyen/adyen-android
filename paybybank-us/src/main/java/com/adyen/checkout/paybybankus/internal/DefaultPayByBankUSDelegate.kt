ab1518ca93dd6206575ec00d4281fe86287cd707
ozgur <6615094+ozgur00@users.noreply.github.com>
2024-11-01 10:36:34 +0100

    Implement DefaultPayByBankUSDelegate
    COAND-974

--- paybybank-us/src/main/java/com/adyen/checkout/paybybankus/internal/DefaultPayByBankUSDelegate.kt
+++ paybybank-us/src/main/java/com/adyen/checkout/paybybankus/internal/DefaultPayByBankUSDelegate.kt
@@ -10,21 +10,28 @@ package com.adyen.checkout.paybybankus.internal
 
 import androidx.lifecycle.LifecycleOwner
 import com.adyen.checkout.components.core.Order
+import com.adyen.checkout.components.core.PaymentComponentData
 import com.adyen.checkout.components.core.PaymentMethod
+import com.adyen.checkout.components.core.PaymentMethodTypes
 import com.adyen.checkout.components.core.internal.PaymentComponentEvent
 import com.adyen.checkout.components.core.internal.PaymentObserverRepository
 import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
+import com.adyen.checkout.components.core.internal.analytics.GenericEvents
 import com.adyen.checkout.components.core.internal.ui.model.ButtonComponentParams
+import com.adyen.checkout.components.core.paymentmethod.PayByBankUSPaymentMethod
+import com.adyen.checkout.core.AdyenLogLevel
+import com.adyen.checkout.core.internal.util.adyenLog
 import com.adyen.checkout.paybybankus.PayByBankUSComponentState
+import com.adyen.checkout.ui.core.internal.ui.ButtonComponentViewType
 import com.adyen.checkout.ui.core.internal.ui.ComponentViewType
 import com.adyen.checkout.ui.core.internal.ui.PaymentComponentUIEvent
 import com.adyen.checkout.ui.core.internal.ui.PaymentComponentUIState
 import com.adyen.checkout.ui.core.internal.ui.SubmitHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
-// TODO remove the suppress annotation after complete implementation
-@Suppress("UnusedPrivateProperty", "UnusedParameter")
+@Suppress("TooManyFunctions")
 internal class DefaultPayByBankUSDelegate(
     private val observerRepository: PaymentObserverRepository,
     private val paymentMethod: PaymentMethod,
@@ -34,23 +41,28 @@ internal class DefaultPayByBankUSDelegate(
     private val submitHandler: SubmitHandler<PayByBankUSComponentState>,
 ) : PayByBankUSDelegate {
 
-    override val componentStateFlow: Flow<PayByBankUSComponentState>
-        get() = TODO("Not yet implemented")
+    private val _componentStateFlow = MutableStateFlow(createComponentState())
+    override val componentStateFlow: Flow<PayByBankUSComponentState> = _componentStateFlow
 
-    override val viewFlow: Flow<ComponentViewType?>
-        get() = TODO("Not yet implemented")
+    // TODO push pay by bank us view after implementing view provider and view
+    private val _viewFlow: MutableStateFlow<ComponentViewType?> = MutableStateFlow(null)
+    override val viewFlow: Flow<ComponentViewType?> = _viewFlow
 
-    override val submitFlow: Flow<PayByBankUSComponentState>
-        get() = TODO("Not yet implemented")
-
-    override val uiStateFlow: Flow<PaymentComponentUIState>
-        get() = TODO("Not yet implemented")
-
-    override val uiEventFlow: Flow<PaymentComponentUIEvent>
-        get() = TODO("Not yet implemented")
+    override val submitFlow: Flow<PayByBankUSComponentState> = submitHandler.submitFlow
+    override val uiStateFlow: Flow<PaymentComponentUIState> = submitHandler.uiStateFlow
+    override val uiEventFlow: Flow<PaymentComponentUIEvent> = submitHandler.uiEventFlow
 
     override fun initialize(coroutineScope: CoroutineScope) {
-        TODO("Not yet implemented")
+        submitHandler.initialize(coroutineScope, componentStateFlow)
+        initializeAnalytics(coroutineScope)
+    }
+
+    private fun initializeAnalytics(coroutineScope: CoroutineScope) {
+        adyenLog(AdyenLogLevel.VERBOSE) { "initializeAnalytics" }
+        analyticsManager.initialize(this, coroutineScope)
+
+        val event = GenericEvents.rendered(paymentMethod.type.orEmpty())
+        analyticsManager.trackEvent(event)
     }
 
     override fun observe(
@@ -58,34 +70,59 @@ internal class DefaultPayByBankUSDelegate(
         coroutineScope: CoroutineScope,
         callback: (PaymentComponentEvent<PayByBankUSComponentState>) -> Unit
     ) {
-        TODO("Not yet implemented")
+        observerRepository.addObservers(
+            stateFlow = componentStateFlow,
+            exceptionFlow = null,
+            submitFlow = submitFlow,
+            lifecycleOwner = lifecycleOwner,
+            coroutineScope = coroutineScope,
+            callback = callback,
+        )
     }
 
     override fun removeObserver() {
-        TODO("Not yet implemented")
+        observerRepository.removeObservers()
     }
 
-    override fun getPaymentMethodType(): String {
-        TODO("Not yet implemented")
+    override fun getPaymentMethodType(): String = paymentMethod.type ?: PaymentMethodTypes.UNKNOWN
+
+    private fun createComponentState(): PayByBankUSComponentState {
+        val payByBankUsPaymentMethod = PayByBankUSPaymentMethod(
+            type = getPaymentMethodType(),
+            checkoutAttemptId = analyticsManager.getCheckoutAttemptId(),
+        )
+
+        val paymentComponentData = PaymentComponentData(
+            paymentMethod = payByBankUsPaymentMethod,
+            order = order,
+            amount = componentParams.amount,
+        )
+
+        return PayByBankUSComponentState(
+            data = paymentComponentData,
+            isInputValid = true,
+            isReady = true,
+        )
     }
 
     override fun onSubmit() {
-        TODO("Not yet implemented")
+        val event = GenericEvents.submit(paymentMethod.type.orEmpty())
+        analyticsManager.trackEvent(event)
+
+        val state = _componentStateFlow.value
+        submitHandler.onSubmit(state = state)
     }
 
-    override fun isConfirmationRequired(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun isConfirmationRequired(): Boolean = _viewFlow.value is ButtonComponentViewType
 
-    override fun shouldShowSubmitButton(): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun shouldShowSubmitButton(): Boolean = isConfirmationRequired() && componentParams.isSubmitButtonVisible
 
     override fun setInteractionBlocked(isInteractionBlocked: Boolean) {
-        TODO("Not yet implemented")
+        submitHandler.setInteractionBlocked(isInteractionBlocked)
     }
 
     override fun onCleared() {
-        TODO("Not yet implemented")
+        removeObserver()
+        analyticsManager.clear(this)
     }
 }
