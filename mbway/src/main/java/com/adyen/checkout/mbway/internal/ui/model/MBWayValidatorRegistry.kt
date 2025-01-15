f8474908e5d17e3e6657a2638fcddad3b3e39124
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-01-15 15:37:26 +0100

    Remove outputData and use MBWayViewState instead Create FieldValidator to validate fields in the Delegate side
    COAND-935

---
+++ mbway/src/main/java/com/adyen/checkout/mbway/internal/ui/model/MBWayValidatorRegistry.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 15/1/2025.
+ */
+
+package com.adyen.checkout.mbway.internal.ui.model
+
+import com.adyen.checkout.components.core.internal.ui.model.Validation
+import com.adyen.checkout.components.core.internal.ui.model.validation.FieldValidator
+import com.adyen.checkout.components.core.internal.ui.model.validation.FieldValidatorRegistry
+import com.adyen.checkout.components.core.internal.util.ValidationUtils
+import com.adyen.checkout.mbway.R
+
+internal class MBWayValidatorRegistry : FieldValidatorRegistry<MBWayFieldId> {
+
+    private val validators = mapOf(
+        MBWayFieldId.LOCAL_PHONE_NUMBER to LocalPhoneNumberValidator(),
+        MBWayFieldId.COUNTRY_CODE to DefaultValidValidator(),
+    )
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T> validate(key: MBWayFieldId, value: T): Validation {
+        val validator = validators[key] as? FieldValidator<T>
+            ?: throw IllegalArgumentException("Unsupported fieldId or invalid type provided")
+        return validator.validate(value)
+    }
+}
+
+internal class LocalPhoneNumberValidator : FieldValidator<String> {
+    override fun validate(input: String) = if (input.isNotEmpty() && ValidationUtils.isPhoneNumberValid(input)) {
+        Validation.Valid
+    } else {
+        Validation.Invalid(R.string.checkout_mbway_phone_number_not_valid)
+    }
+}
+
+// TODO: Could this logic change?
+internal class DefaultValidValidator : FieldValidator<Any> {
+    override fun validate(input: Any) = Validation.Valid
+}
