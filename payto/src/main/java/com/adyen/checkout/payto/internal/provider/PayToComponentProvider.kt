b055055a1774fd87e5dd0da66d52142dd4b2cfb9
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-02-03 12:10:51 +0100

    Add DefaultPayToDelegate and PayToComponentProvider
    COAND-1058

--- payto/src/main/java/com/adyen/checkout/payto/internal/provider/PayToComponentProvider.kt
+++ payto/src/main/java/com/adyen/checkout/payto/internal/provider/PayToComponentProvider.kt
@@ -8,9 +8,269 @@
 
 package com.adyen.checkout.payto.internal.provider
 
+import android.app.Application
 import androidx.annotation.RestrictTo
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.savedstate.SavedStateRegistryOwner
+import com.adyen.checkout.action.core.internal.DefaultActionHandlingComponent
+import com.adyen.checkout.action.core.internal.provider.GenericActionComponentProvider
+import com.adyen.checkout.components.core.CheckoutConfiguration
+import com.adyen.checkout.components.core.ComponentCallback
+import com.adyen.checkout.components.core.Order
+import com.adyen.checkout.components.core.PaymentMethod
+import com.adyen.checkout.components.core.internal.DefaultComponentEventHandler
+import com.adyen.checkout.components.core.internal.PaymentObserverRepository
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsManagerFactory
+import com.adyen.checkout.components.core.internal.analytics.AnalyticsSource
+import com.adyen.checkout.components.core.internal.provider.PaymentComponentProvider
+import com.adyen.checkout.components.core.internal.ui.model.ButtonComponentParamsMapper
+import com.adyen.checkout.components.core.internal.ui.model.CommonComponentParamsMapper
+import com.adyen.checkout.components.core.internal.ui.model.DropInOverrideParams
+import com.adyen.checkout.components.core.internal.util.get
+import com.adyen.checkout.components.core.internal.util.viewModelFactory
+import com.adyen.checkout.core.exception.ComponentException
+import com.adyen.checkout.core.internal.data.api.HttpClientFactory
+import com.adyen.checkout.core.internal.util.LocaleProvider
+import com.adyen.checkout.payto.PayToComponent
+import com.adyen.checkout.payto.PayToComponentState
+import com.adyen.checkout.payto.PayToConfiguration
+import com.adyen.checkout.payto.getPayToConfiguration
+import com.adyen.checkout.payto.internal.ui.DefaultPayToDelegate
+import com.adyen.checkout.payto.toCheckoutConfiguration
+import com.adyen.checkout.sessions.core.CheckoutSession
+import com.adyen.checkout.sessions.core.SessionComponentCallback
+import com.adyen.checkout.sessions.core.internal.SessionComponentEventHandler
+import com.adyen.checkout.sessions.core.internal.SessionInteractor
+import com.adyen.checkout.sessions.core.internal.SessionSavedStateHandleContainer
+import com.adyen.checkout.sessions.core.internal.data.api.SessionRepository
+import com.adyen.checkout.sessions.core.internal.data.api.SessionService
+import com.adyen.checkout.sessions.core.internal.provider.SessionPaymentComponentProvider
+import com.adyen.checkout.sessions.core.internal.ui.model.SessionParamsFactory
+import com.adyen.checkout.ui.core.internal.ui.SubmitHandler
 
-// TODO To be implemented
 class PayToComponentProvider
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-constructor()
+constructor(
+    private val dropInOverrideParams: DropInOverrideParams? = null,
+    private val analyticsManager: AnalyticsManager? = null,
+    private val localeProvider: LocaleProvider = LocaleProvider(),
+) :
+    PaymentComponentProvider<
+        PayToComponent,
+        PayToConfiguration,
+        PayToComponentState,
+        ComponentCallback<PayToComponentState>,
+        >,
+    SessionPaymentComponentProvider<
+        PayToComponent,
+        PayToConfiguration,
+        PayToComponentState,
+        SessionComponentCallback<PayToComponentState>,
+        > {
+
+    override fun get(
+        savedStateRegistryOwner: SavedStateRegistryOwner,
+        viewModelStoreOwner: ViewModelStoreOwner,
+        lifecycleOwner: LifecycleOwner,
+        paymentMethod: PaymentMethod,
+        checkoutConfiguration: CheckoutConfiguration,
+        application: Application,
+        componentCallback: ComponentCallback<PayToComponentState>,
+        order: Order?,
+        key: String?,
+    ): PayToComponent {
+        assertSupported(paymentMethod)
+
+        val genericFactory = viewModelFactory(savedStateRegistryOwner, null) { savedStateHandle ->
+            val componentParams = ButtonComponentParamsMapper(CommonComponentParamsMapper()).mapToParams(
+                checkoutConfiguration = checkoutConfiguration,
+                deviceLocale = localeProvider.getLocale(application),
+                dropInOverrideParams = dropInOverrideParams,
+                componentSessionParams = null,
+                componentConfiguration = checkoutConfiguration.getPayToConfiguration(),
+            )
+
+            val analyticsManager = analyticsManager ?: AnalyticsManagerFactory().provide(
+                componentParams = componentParams,
+                application = application,
+                source = AnalyticsSource.PaymentComponent(paymentMethod.type.orEmpty()),
+                sessionId = null,
+            )
+
+            val payToDelegate = DefaultPayToDelegate(
+                observerRepository = PaymentObserverRepository(),
+                paymentMethod = paymentMethod,
+                order = order,
+                componentParams = componentParams,
+                analyticsManager = analyticsManager,
+                submitHandler = SubmitHandler(savedStateHandle),
+            )
+
+            val genericActionDelegate =
+                GenericActionComponentProvider(analyticsManager, dropInOverrideParams).getDelegate(
+                    checkoutConfiguration = checkoutConfiguration,
+                    savedStateHandle = savedStateHandle,
+                    application = application,
+                )
+
+            PayToComponent(
+                payToDelegate = payToDelegate,
+                genericActionDelegate = genericActionDelegate,
+                actionHandlingComponent = DefaultActionHandlingComponent(genericActionDelegate, payToDelegate),
+                componentEventHandler = DefaultComponentEventHandler(),
+            )
+        }
+
+        return ViewModelProvider(viewModelStoreOwner, genericFactory)[key, PayToComponent::class.java]
+            .also { component ->
+                component.observe(lifecycleOwner) {
+                    component.componentEventHandler.onPaymentComponentEvent(it, componentCallback)
+                }
+            }
+    }
+
+    override fun get(
+        savedStateRegistryOwner: SavedStateRegistryOwner,
+        viewModelStoreOwner: ViewModelStoreOwner,
+        lifecycleOwner: LifecycleOwner,
+        paymentMethod: PaymentMethod,
+        configuration: PayToConfiguration,
+        application: Application,
+        componentCallback: ComponentCallback<PayToComponentState>,
+        order: Order?,
+        key: String?,
+    ): PayToComponent {
+        return get(
+            savedStateRegistryOwner = savedStateRegistryOwner,
+            viewModelStoreOwner = viewModelStoreOwner,
+            lifecycleOwner = lifecycleOwner,
+            paymentMethod = paymentMethod,
+            checkoutConfiguration = configuration.toCheckoutConfiguration(),
+            application = application,
+            componentCallback = componentCallback,
+            order = order,
+            key = key,
+        )
+    }
+
+    @Suppress("LongMethod")
+    override fun get(
+        savedStateRegistryOwner: SavedStateRegistryOwner,
+        viewModelStoreOwner: ViewModelStoreOwner,
+        lifecycleOwner: LifecycleOwner,
+        checkoutSession: CheckoutSession,
+        paymentMethod: PaymentMethod,
+        checkoutConfiguration: CheckoutConfiguration,
+        application: Application,
+        componentCallback: SessionComponentCallback<PayToComponentState>,
+        key: String?
+    ): PayToComponent {
+        assertSupported(paymentMethod)
+
+        val genericFactory = viewModelFactory(savedStateRegistryOwner, null) { savedStateHandle ->
+            val componentParams = ButtonComponentParamsMapper(CommonComponentParamsMapper()).mapToParams(
+                checkoutConfiguration = checkoutConfiguration,
+                deviceLocale = localeProvider.getLocale(application),
+                dropInOverrideParams = dropInOverrideParams,
+                componentSessionParams = SessionParamsFactory.create(checkoutSession),
+                componentConfiguration = checkoutConfiguration.getPayToConfiguration(),
+            )
+
+            val httpClient = HttpClientFactory.getHttpClient(componentParams.environment)
+
+            val analyticsManager = analyticsManager ?: AnalyticsManagerFactory().provide(
+                componentParams = componentParams,
+                application = application,
+                source = AnalyticsSource.PaymentComponent(paymentMethod.type.orEmpty()),
+                sessionId = checkoutSession.sessionSetupResponse.id,
+            )
+
+            val payToDelegate = DefaultPayToDelegate(
+                observerRepository = PaymentObserverRepository(),
+                paymentMethod = paymentMethod,
+                order = checkoutSession.order,
+                componentParams = componentParams,
+                analyticsManager = analyticsManager,
+                submitHandler = SubmitHandler(savedStateHandle),
+            )
+
+            val genericActionDelegate =
+                GenericActionComponentProvider(analyticsManager, dropInOverrideParams).getDelegate(
+                    checkoutConfiguration = checkoutConfiguration,
+                    savedStateHandle = savedStateHandle,
+                    application = application,
+                )
+
+            val sessionSavedStateHandleContainer = SessionSavedStateHandleContainer(
+                savedStateHandle = savedStateHandle,
+                checkoutSession = checkoutSession,
+            )
+
+            val sessionInteractor = SessionInteractor(
+                sessionRepository = SessionRepository(
+                    sessionService = SessionService(httpClient),
+                    clientKey = componentParams.clientKey,
+                ),
+                sessionModel = sessionSavedStateHandleContainer.getSessionModel(),
+                isFlowTakenOver = sessionSavedStateHandleContainer.isFlowTakenOver ?: false,
+                analyticsManager = analyticsManager,
+            )
+
+            val sessionComponentEventHandler = SessionComponentEventHandler<PayToComponentState>(
+                sessionInteractor = sessionInteractor,
+                sessionSavedStateHandleContainer = sessionSavedStateHandleContainer,
+            )
+
+            PayToComponent(
+                payToDelegate = payToDelegate,
+                genericActionDelegate = genericActionDelegate,
+                actionHandlingComponent = DefaultActionHandlingComponent(genericActionDelegate, payToDelegate),
+                componentEventHandler = sessionComponentEventHandler,
+            )
+        }
+
+        return ViewModelProvider(viewModelStoreOwner, genericFactory)[key, PayToComponent::class.java]
+            .also { component ->
+                component.observe(lifecycleOwner) {
+                    component.componentEventHandler.onPaymentComponentEvent(it, componentCallback)
+                }
+            }
+    }
+
+    override fun get(
+        savedStateRegistryOwner: SavedStateRegistryOwner,
+        viewModelStoreOwner: ViewModelStoreOwner,
+        lifecycleOwner: LifecycleOwner,
+        checkoutSession: CheckoutSession,
+        paymentMethod: PaymentMethod,
+        configuration: PayToConfiguration,
+        application: Application,
+        componentCallback: SessionComponentCallback<PayToComponentState>,
+        key: String?
+    ): PayToComponent {
+        return get(
+            savedStateRegistryOwner = savedStateRegistryOwner,
+            viewModelStoreOwner = viewModelStoreOwner,
+            lifecycleOwner = lifecycleOwner,
+            checkoutSession = checkoutSession,
+            paymentMethod = paymentMethod,
+            checkoutConfiguration = configuration.toCheckoutConfiguration(),
+            application = application,
+            componentCallback = componentCallback,
+            key = key,
+        )
+    }
+
+    private fun assertSupported(paymentMethod: PaymentMethod) {
+        if (!isPaymentMethodSupported(paymentMethod)) {
+            throw ComponentException("Unsupported payment method ${paymentMethod.type}")
+        }
+    }
+
+    override fun isPaymentMethodSupported(paymentMethod: PaymentMethod): Boolean {
+        return PayToComponent.PAYMENT_METHOD_TYPES.contains(paymentMethod.type)
+    }
+}
