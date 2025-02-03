05c1b5be9217ee813ff64ca19d57323b008011a6
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-02-03 12:07:14 +0100

    Add PayToConfiguration
    COAND-1058

---
+++ payto/src/main/java/com/adyen/checkout/payto/PayToConfiguration.kt
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 3/2/2025.
+ */
+
+package com.adyen.checkout.payto
+
+import android.content.Context
+import com.adyen.checkout.action.core.GenericActionConfiguration
+import com.adyen.checkout.action.core.internal.ActionHandlingPaymentMethodConfigurationBuilder
+import com.adyen.checkout.components.core.Amount
+import com.adyen.checkout.components.core.AnalyticsConfiguration
+import com.adyen.checkout.components.core.CheckoutConfiguration
+import com.adyen.checkout.components.core.PaymentMethodTypes
+import com.adyen.checkout.components.core.internal.ButtonConfiguration
+import com.adyen.checkout.components.core.internal.ButtonConfigurationBuilder
+import com.adyen.checkout.components.core.internal.Configuration
+import com.adyen.checkout.components.core.internal.util.CheckoutConfigurationMarker
+import com.adyen.checkout.core.Environment
+import kotlinx.parcelize.Parcelize
+import java.util.Locale
+
+/**
+ * Configuration class for the [PayToComponent].
+ */
+@Parcelize
+@Suppress("LongParameterList")
+class PayToConfiguration(
+    override val shopperLocale: Locale?,
+    override val environment: Environment,
+    override val clientKey: String,
+    override val analyticsConfiguration: AnalyticsConfiguration?,
+    override val amount: Amount?,
+    override val isSubmitButtonVisible: Boolean?,
+    internal val genericActionConfiguration: GenericActionConfiguration,
+) : Configuration, ButtonConfiguration {
+
+    /**
+     * Builder to create an [PayToConfiguration].
+     */
+    class Builder :
+        ActionHandlingPaymentMethodConfigurationBuilder<PayToConfiguration, Builder>,
+        ButtonConfigurationBuilder {
+
+        private var isSubmitButtonVisible: Boolean? = null
+
+        /**
+         * Initialize a configuration builder with the required fields.
+         *
+         * The shopper locale will match the value passed to the API with the sessions flow, or the primary user locale
+         * on the device otherwise. Check out the
+         * [Sessions API documentation](https://docs.adyen.com/api-explorer/Checkout/latest/post/sessions) on how to set
+         * this value.
+         *
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        constructor(environment: Environment, clientKey: String) : super(
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Alternative constructor that uses the [context] to fetch the user locale and use it as a shopper locale.
+         *
+         * @param context A context
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        @Deprecated("You can omit the context parameter")
+        constructor(context: Context, environment: Environment, clientKey: String) : super(
+            context,
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Initialize a configuration builder with the required fields and a shopper locale.
+         *
+         * @param shopperLocale The [Locale] of the shopper.
+         * @param environment The [Environment] to be used for internal network calls from the SDK to Adyen.
+         * @param clientKey Your Client Key used for internal network calls from the SDK to Adyen.
+         */
+        constructor(shopperLocale: Locale, environment: Environment, clientKey: String) : super(
+            shopperLocale,
+            environment,
+            clientKey,
+        )
+
+        /**
+         * Sets if submit button will be visible or not.
+         *
+         * Default is True.
+         *
+         * @param isSubmitButtonVisible Is submit button should be visible or not.
+         */
+        override fun setSubmitButtonVisible(isSubmitButtonVisible: Boolean): Builder {
+            this.isSubmitButtonVisible = isSubmitButtonVisible
+            return this
+        }
+
+        override fun buildInternal(): PayToConfiguration {
+            return PayToConfiguration(
+                shopperLocale = shopperLocale,
+                environment = environment,
+                clientKey = clientKey,
+                analyticsConfiguration = analyticsConfiguration,
+                amount = amount,
+                isSubmitButtonVisible = isSubmitButtonVisible,
+                genericActionConfiguration = genericActionConfigurationBuilder.build(),
+            )
+        }
+    }
+}
+
+fun CheckoutConfiguration.payTo(
+    configuration: @CheckoutConfigurationMarker PayToConfiguration.Builder.() -> Unit = {}
+): CheckoutConfiguration {
+    val config = PayToConfiguration.Builder(environment, clientKey)
+        .apply {
+            shopperLocale?.let { setShopperLocale(it) }
+            amount?.let { setAmount(it) }
+            analyticsConfiguration?.let { setAnalyticsConfiguration(it) }
+        }
+        .apply(configuration)
+        .build()
+    addConfiguration(PaymentMethodTypes.PAY_TO, config)
+    return this
+}
+
+internal fun CheckoutConfiguration.getPayToConfiguration(): PayToConfiguration? {
+    return getConfiguration(PaymentMethodTypes.PAY_TO)
+}
+
+internal fun PayToConfiguration.toCheckoutConfiguration(): CheckoutConfiguration {
+    return CheckoutConfiguration(
+        shopperLocale = shopperLocale,
+        environment = environment,
+        clientKey = clientKey,
+        amount = amount,
+        analyticsConfiguration = analyticsConfiguration,
+    ) {
+        addConfiguration(PaymentMethodTypes.PAY_TO, this@toCheckoutConfiguration)
+
+        genericActionConfiguration.getAllConfigurations().forEach {
+            addActionConfiguration(it)
+        }
+    }
+}
