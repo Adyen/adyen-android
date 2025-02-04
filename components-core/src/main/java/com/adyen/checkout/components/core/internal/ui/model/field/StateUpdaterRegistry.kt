1559604758c298ebdb2e169361b619a9a704b603
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-02-04 13:31:58 +0100

    Create StateUpdaterRegistry and StateManager to handle all state updates
    COAND-935

---
+++ components-core/src/main/java/com/adyen/checkout/components/core/internal/ui/model/field/StateUpdaterRegistry.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 4/2/2025.
+ */
+
+package com.adyen.checkout.components.core.internal.ui.model.field
+
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.components.core.internal.ui.model.ComponentFieldDelegateState
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+interface StateUpdaterRegistry<K, S> {
+    fun <T> getFieldState(key: K, state: S): ComponentFieldDelegateState<T>
+
+    fun <T> updateFieldState(key: K, state: S, fieldState: ComponentFieldDelegateState<T>): S
+}
