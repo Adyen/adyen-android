2987647bdb5c2f69cf6bda96cef5cec18274bb17
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-02-10 10:41:33 +0100

    Add initial PayToView setup
    COAND-1058

--- payto/src/main/java/com/adyen/checkout/payto/internal/ui/view/PayToView.kt
+++ payto/src/main/java/com/adyen/checkout/payto/internal/ui/view/PayToView.kt
@@ -10,9 +10,15 @@ package com.adyen.checkout.payto.internal.ui.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import com.adyen.checkout.components.core.internal.ui.ComponentDelegate
+import com.adyen.checkout.core.AdyenLogLevel
+import com.adyen.checkout.core.internal.util.adyenLog
+import com.adyen.checkout.payto.databinding.PaytoViewBinding
+import com.adyen.checkout.payto.internal.ui.PayToDelegate
+import com.adyen.checkout.ui.core.R
 import com.adyen.checkout.ui.core.internal.ui.ComponentView
 import kotlinx.coroutines.CoroutineScope
 
@@ -22,12 +28,30 @@ internal class PayToView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr), ComponentView {
 
+    private val binding = PaytoViewBinding.inflate(LayoutInflater.from(context), this)
+
+    private lateinit var localizedContext: Context
+
+    private lateinit var delegate: PayToDelegate
+
+    init {
+        orientation = VERTICAL
+
+        val padding = resources.getDimension(R.dimen.standard_margin).toInt()
+        setPadding(padding, padding, padding, 0)
+    }
+
     override fun initView(delegate: ComponentDelegate, coroutineScope: CoroutineScope, localizedContext: Context) {
-        // TODO To be implemented
+        require(delegate is PayToDelegate) { "Unsupported delegate type" }
+        this.delegate = delegate
+        this.localizedContext = localizedContext
+
+        // TODO Initialize views
     }
 
     override fun highlightValidationErrors() {
-        // TODO To be implemented
+        adyenLog(AdyenLogLevel.DEBUG) { "highlightValidationErrors" }
+        // TODO Do validation
     }
 
     override fun getView(): View = this
