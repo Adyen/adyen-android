7d052cda6e524afcaa4ffaec5a7ff3ed07eb85bd
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-02-11 15:27:07 +0100

    Make DelegateStateManager generic for all delegates and create a factory to generate default state
    COAND-935

--- components-core/src/main/java/com/adyen/checkout/components/core/internal/ui/model/state/DelegateState.kt
+++ components-core/src/main/java/com/adyen/checkout/components/core/internal/ui/model/state/DelegateState.kt
@@ -3,16 +3,14 @@
  *
  * This file is open source and available under the MIT license. See the LICENSE file for more info.
  *
- * Created by ararat on 4/2/2025.
+ * Created by ararat on 6/2/2025.
  */
 
-package com.adyen.checkout.components.core.internal.ui.model.field
+package com.adyen.checkout.components.core.internal.ui.model.state
 
 import androidx.annotation.RestrictTo
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-interface StateUpdater<S, FS> {
-    fun getFieldState(state: S): FS
-
-    fun updateFieldState(state: S, fieldState: FS): S
+interface DelegateState {
+    val isValid: Boolean
 }
