ad3c30757a6afda282462901826b5a510834b14c
Oscar Spruit <oscar.spruit@adyen.com>
2025-02-18 13:46:28 +0100

    Implement scanning flow
    Just a simple button is used for now, it will be improved later.
    
    COAND-429

--- card/src/main/java/com/adyen/checkout/card/internal/ui/view/CardFragment.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/view/CardFragment.kt
@@ -8,10 +8,85 @@
 
 package com.adyen.checkout.card.internal.ui.view
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.adyen.checkout.card.databinding.FragmentCardBinding
 import com.adyen.checkout.card.internal.ui.CardDelegate
+import com.adyen.checkout.card.internal.util.CardScannerWrapper
+import com.adyen.checkout.core.ui.model.ExpiryDate
+import kotlinx.coroutines.launch
 
 internal class CardFragment : Fragment() {
 
-    fun initialize(delegate: CardDelegate) {}
+    private var _binding: FragmentCardBinding? = null
+    private val binding: FragmentCardBinding get() = requireNotNull(_binding)
+
+    private var delegate: CardDelegate? = null
+    private var cardScanner: CardScannerWrapper? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentCardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.scanButton.setOnClickListener {
+            cardScanner?.startScanner(this, REQUEST_CODE)
+        }
+    }
+
+    fun initialize(delegate: CardDelegate) {
+        this.delegate = delegate
+        this.cardScanner = CardScannerWrapper()
+        resetScanner()
+    }
+
+    private fun resetScanner() {
+        val cardScanner = cardScanner ?: return
+        val delegate = delegate ?: return
+        cardScanner.terminate()
+        cardScanner.initialize(requireContext(), delegate.componentParams.environment)
+        viewLifecycleOwner.lifecycleScope.launch {
+            binding.scanButton.isVisible = cardScanner.isAvailable()
+        }
+    }
+
+    @Suppress("OVERRIDE_DEPRECATION", "DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != REQUEST_CODE) return
+
+        val result = cardScanner?.getResult(data)
+
+        result?.let {
+            delegate?.updateInputData {
+                val (pan, expiryMonth, expiryYear) = result
+                cardNumber = pan.orEmpty()
+                if (expiryMonth != null && expiryYear != null) {
+                    expiryDate = ExpiryDate(expiryMonth, expiryYear)
+                }
+            }
+        }
+        resetScanner()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        delegate = null
+        cardScanner?.terminate()
+        cardScanner = null
+        super.onDestroyView()
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 101
+    }
 }
