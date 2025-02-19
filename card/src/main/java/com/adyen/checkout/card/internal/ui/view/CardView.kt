b711b719babc5eb02c0eaf364df33996be57b203
Oscar Spruit <oscar.spruit@adyen.com>
2025-02-19 17:00:07 +0100

    Combine initialization and availability check of scanner in one
    This reduces friction in the flow and is easier to follow. It also reduces object we would have to clean up.
    
    COAND-429

--- card/src/main/java/com/adyen/checkout/card/internal/ui/view/CardView.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/view/CardView.kt
@@ -135,7 +135,7 @@ class CardView @JvmOverloads constructor(
         initPostalCodeInput()
         initAddressFormInput(coroutineScope)
         initAddressLookup()
-        initFragment(delegate)
+        initFeatureFragment(delegate)
 
         binding.switchStorePaymentMethod.setOnCheckedChangeListener { _, isChecked ->
             delegate.updateInputData { isStorePaymentMethodSwitchChecked = isChecked }
@@ -561,7 +561,7 @@ class CardView @JvmOverloads constructor(
         }
     }
 
-    private fun initFragment(delegate: CardDelegate) {
+    private fun initFeatureFragment(delegate: CardDelegate) {
         binding.fragmentContainer.getFragment<CardFragment?>()?.initialize(delegate)
     }
 
