56f8ab4a62a8cec6815b7dcbb37b0f4e82fb715d
Oscar Spruit <oscar.spruit@adyen.com>
2025-04-10 15:46:45 +0200

    Add default text styles
    COAND-1084

--- ui/src/main/java/com/adyen/checkout/ui/theme/AdyenTextStyles.kt
+++ ui/src/main/java/com/adyen/checkout/ui/theme/AdyenTextStyles.kt
@@ -9,16 +9,30 @@
 package com.adyen.checkout.ui.theme
 
 data class AdyenTextStyles(
-    val title: TextStyle,
-    val subtitle: TextStyle,
-    val label: TextStyle,
-    val body: TextStyle,
-    val caption: TextStyle,
+    val title: AdyenTextStyle,
+    val subtitle: AdyenTextStyle,
+    val label: AdyenTextStyle,
+    val body: AdyenTextStyle,
+    val caption: AdyenTextStyle,
 )
 
-data class TextStyle(
+data class AdyenTextStyle(
     val size: Int,
     val weight: Int,
     val lineHeight: Int,
-    val fontResId: Int,
+    val fontResId: Int?,
+)
+
+fun adyenCheckoutTextStyles(
+    title: AdyenTextStyle = AdyenTextStyle(size = 36, weight = 400, lineHeight = 44, fontResId = null),
+    subtitle: AdyenTextStyle = AdyenTextStyle(size = 22, weight = 400, lineHeight = 28, fontResId = null),
+    label: AdyenTextStyle = AdyenTextStyle(size = 14, weight = 500, lineHeight = 20, fontResId = null),
+    body: AdyenTextStyle = AdyenTextStyle(size = 14, weight = 400, lineHeight = 20, fontResId = null),
+    caption: AdyenTextStyle = AdyenTextStyle(size = 12, weight = 400, lineHeight = 16, fontResId = null),
+) = AdyenTextStyles(
+    title = title,
+    subtitle = subtitle,
+    label = label,
+    body = body,
+    caption = caption,
 )
