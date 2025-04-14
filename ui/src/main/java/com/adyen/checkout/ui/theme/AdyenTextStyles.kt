55aff5313d69bce987f62271e39f2fa3bdcc9e64
Oscar Spruit <oscar.spruit@adyen.com>
2025-04-14 11:39:21 +0200

    Move default theming functions into companion objects
    COAND-1084

--- ui/src/main/java/com/adyen/checkout/ui/theme/AdyenTextStyles.kt
+++ ui/src/main/java/com/adyen/checkout/ui/theme/AdyenTextStyles.kt
@@ -9,14 +9,6 @@
 package com.adyen.checkout.ui.theme
 
 // TODO - Add KDocs
-data class AdyenTextStyles(
-    val title: AdyenTextStyle,
-    val subtitle: AdyenTextStyle,
-    val label: AdyenTextStyle,
-    val body: AdyenTextStyle,
-    val caption: AdyenTextStyle,
-)
-
 data class AdyenTextStyle(
     val size: Int,
     val weight: Int,
@@ -24,16 +16,28 @@ data class AdyenTextStyle(
     val fontResId: Int?,
 )
 
-fun adyenCheckoutTextStyles(
-    title: AdyenTextStyle = AdyenTextStyle(size = 36, weight = 400, lineHeight = 44, fontResId = null),
-    subtitle: AdyenTextStyle = AdyenTextStyle(size = 22, weight = 400, lineHeight = 28, fontResId = null),
-    label: AdyenTextStyle = AdyenTextStyle(size = 14, weight = 500, lineHeight = 20, fontResId = null),
-    body: AdyenTextStyle = AdyenTextStyle(size = 14, weight = 400, lineHeight = 20, fontResId = null),
-    caption: AdyenTextStyle = AdyenTextStyle(size = 12, weight = 400, lineHeight = 16, fontResId = null),
-) = AdyenTextStyles(
-    title = title,
-    subtitle = subtitle,
-    label = label,
-    body = body,
-    caption = caption,
-)
+data class AdyenTextStyles(
+    val title: AdyenTextStyle,
+    val subtitle: AdyenTextStyle,
+    val label: AdyenTextStyle,
+    val body: AdyenTextStyle,
+    val caption: AdyenTextStyle,
+) {
+
+    companion object {
+
+        fun default(
+            title: AdyenTextStyle = AdyenTextStyle(size = 36, weight = 400, lineHeight = 44, fontResId = null),
+            subtitle: AdyenTextStyle = AdyenTextStyle(size = 22, weight = 400, lineHeight = 28, fontResId = null),
+            label: AdyenTextStyle = AdyenTextStyle(size = 14, weight = 500, lineHeight = 20, fontResId = null),
+            body: AdyenTextStyle = AdyenTextStyle(size = 14, weight = 400, lineHeight = 20, fontResId = null),
+            caption: AdyenTextStyle = AdyenTextStyle(size = 12, weight = 400, lineHeight = 16, fontResId = null),
+        ) = AdyenTextStyles(
+            title = title,
+            subtitle = subtitle,
+            label = label,
+            body = body,
+            caption = caption,
+        )
+    }
+}
