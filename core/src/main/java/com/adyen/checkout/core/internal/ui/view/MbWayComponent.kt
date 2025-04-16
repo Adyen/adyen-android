cf813ed0ea00d9dd3b8ea806b66a502db5b2d03f
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-04-16 14:36:16 +0200

    Add FieldChangeListener to the MbWayComponent
    COAND-1073

--- core/src/main/java/com/adyen/checkout/core/internal/ui/view/MbWayComponent.kt
+++ core/src/main/java/com/adyen/checkout/core/internal/ui/view/MbWayComponent.kt
@@ -14,40 +14,48 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import com.adyen.checkout.core.internal.ui.state.FieldChangeListener
+import com.adyen.checkout.core.internal.ui.state.FieldId
 
 @Composable
 fun MbWayComponent(
+    fieldChangeListener: FieldChangeListener<MBWayFieldId>,
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
         // CountryCode
         OutlinedTextField(
             modifier = Modifier
-                .onFocusChanged {
-                    // TODO uiStateDelegate.onFieldFocusChanged()
+                .onFocusChanged { focusState ->
+                    fieldChangeListener.onFieldFocusChanged(MBWayFieldId.COUNTRY_CODE, focusState.hasFocus)
                 },
             label = {
                 Text("Country Code")
             },
             value = "",
-            onValueChange = {
-                // TODO uiStateDelegate.onFieldValueChanged()
+            onValueChange = { value ->
+                fieldChangeListener.onFieldValueChanged(MBWayFieldId.COUNTRY_CODE, value)
             },
         )
 
         // PhoneNumber
         OutlinedTextField(
             modifier = Modifier
-                .onFocusChanged {
-                    // TODO uiStateDelegate.onFieldFocusChanged()
+                .onFocusChanged { focusState ->
+                    fieldChangeListener.onFieldFocusChanged(MBWayFieldId.PHONE_NUMBER, focusState.hasFocus)
                 },
             label = {
                 Text("Phone Number")
             },
             value = "",
-            onValueChange = {
-                // TODO uiStateDelegate.onFieldValueChanged()
+            onValueChange = { value ->
+                fieldChangeListener.onFieldValueChanged(MBWayFieldId.PHONE_NUMBER, value)
             },
         )
     }
+}
+
+enum class MBWayFieldId : FieldId {
+    COUNTRY_CODE,
+    PHONE_NUMBER
 }
