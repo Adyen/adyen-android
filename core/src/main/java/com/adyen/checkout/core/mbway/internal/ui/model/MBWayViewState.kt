c458f9ff5065e957b7ef14c7c9f54b19c2b96a61
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2025-04-16 17:10:12 +0200

    Add state management implementation for MBWay
    COAND-1073

---
+++ core/src/main/java/com/adyen/checkout/core/mbway/internal/ui/model/MBWayViewState.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 14/1/2025.
+ */
+
+package com.adyen.checkout.core.mbway.internal.ui.model
+
+import com.adyen.checkout.core.internal.ui.state.model.ViewFieldState
+import com.adyen.checkout.core.internal.ui.state.model.toViewFieldState
+
+internal data class MBWayViewState(
+    val countries: List<String>,
+    val countryCodeFieldState: ViewFieldState<String>,
+    val phoneNumberFieldState: ViewFieldState<String>,
+)
+
+internal fun MBWayDelegateState.toViewState() = MBWayViewState(
+    countries = this.countries,
+    countryCodeFieldState = this.countryCodeFieldState.toViewFieldState(),
+    phoneNumberFieldState = this.localPhoneNumberFieldState.toViewFieldState(),
+)
