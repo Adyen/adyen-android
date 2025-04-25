0eac5594257be8e052ecf2b37a71921ab9578982
Oscar Spruit <oscar.spruit@adyen.com>
2025-04-25 15:27:02 +0200

    Auto focus on first input in Giftcard
    COSDK-138

--- giftcard/src/main/java/com/adyen/checkout/giftcard/internal/ui/view/GiftCardView.kt
+++ giftcard/src/main/java/com/adyen/checkout/giftcard/internal/ui/view/GiftCardView.kt
@@ -74,18 +74,24 @@ internal class GiftCardView @JvmOverloads constructor(
             localizedContext,
         )
 
-        binding.editTextGiftcardNumber.setOnChangeListener {
-            giftCardDelegate.updateInputData { cardNumber = binding.editTextGiftcardNumber.rawValue }
-            binding.textInputLayoutGiftcardNumber.hideError()
-        }
-
-        binding.editTextGiftcardNumber.onFocusChangeListener = OnFocusChangeListener { _: View?, hasFocus: Boolean ->
-            val cardNumberValidation = giftCardDelegate.outputData.numberFieldState.validation
-            if (hasFocus) {
+        binding.editTextGiftcardNumber.apply {
+            setOnChangeListener {
+                giftCardDelegate.updateInputData { cardNumber = binding.editTextGiftcardNumber.rawValue }
                 binding.textInputLayoutGiftcardNumber.hideError()
-            } else if (cardNumberValidation is Validation.Invalid) {
-                binding.textInputLayoutGiftcardNumber.showError(localizedContext.getString(cardNumberValidation.reason))
             }
+
+            onFocusChangeListener = OnFocusChangeListener { _: View?, hasFocus: Boolean ->
+                val cardNumberValidation = giftCardDelegate.outputData.numberFieldState.validation
+                if (hasFocus) {
+                    binding.textInputLayoutGiftcardNumber.hideError()
+                } else if (cardNumberValidation is Validation.Invalid) {
+                    binding.textInputLayoutGiftcardNumber.showError(
+                        localizedContext.getString(cardNumberValidation.reason),
+                    )
+                }
+            }
+
+            requestFocus()
         }
     }
 
