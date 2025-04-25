dfd0a7fbee14d12a7690313b9656ee108e47d1d1
Oscar Spruit <oscar.spruit@adyen.com>
2025-04-25 14:59:37 +0200

    Auto focus on first input in BACS
    COSDK-138

--- bacs/src/main/java/com/adyen/checkout/bacs/internal/ui/view/BacsDirectDebitInputView.kt
+++ bacs/src/main/java/com/adyen/checkout/bacs/internal/ui/view/BacsDirectDebitInputView.kt
@@ -173,18 +173,22 @@ internal class BacsDirectDebitInputView @JvmOverloads constructor(
     }
 
     private fun initHolderNameInput() {
-        val holderNameEditText = binding.editTextHolderName as? AdyenTextInputEditText
-        holderNameEditText?.setOnChangeListener {
-            bacsDelegate.updateInputData { holderName = it.toString() }
-            binding.textInputLayoutHolderName.hideError()
-        }
-        holderNameEditText?.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
-            val holderNameValidation = bacsDelegate.outputData.holderNameState.validation
-            if (hasFocus) {
+        binding.editTextHolderName.apply {
+            setOnChangeListener {
+                bacsDelegate.updateInputData { holderName = it.toString() }
                 binding.textInputLayoutHolderName.hideError()
-            } else if (holderNameValidation is Validation.Invalid) {
-                binding.textInputLayoutHolderName.showError(localizedContext.getString(holderNameValidation.reason))
             }
+
+            onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+                val holderNameValidation = bacsDelegate.outputData.holderNameState.validation
+                if (hasFocus) {
+                    binding.textInputLayoutHolderName.hideError()
+                } else if (holderNameValidation is Validation.Invalid) {
+                    binding.textInputLayoutHolderName.showError(localizedContext.getString(holderNameValidation.reason))
+                }
+            }
+
+            requestFocus()
         }
     }
 
