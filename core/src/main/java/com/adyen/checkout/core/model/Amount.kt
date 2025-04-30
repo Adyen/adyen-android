1214254aec271e18a9914a0a61125e92d6f037c8
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-04-30 13:55:25 +0200

    Copy files for session initialization in AdyenCheckout
    COSDK-441

---
+++ core/src/main/java/com/adyen/checkout/core/model/Amount.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 30/4/2025.
+ */
+
+package com.adyen.checkout.core.model
+
+import com.adyen.checkout.core.exception.ModelSerializationException
+import com.adyen.checkout.core.internal.data.model.ModelObject
+import com.adyen.checkout.core.internal.data.model.getLongOrNull
+import com.adyen.checkout.core.internal.data.model.getStringOrNull
+import kotlinx.parcelize.Parcelize
+import org.json.JSONException
+import org.json.JSONObject
+
+@Parcelize
+data class Amount(
+    var currency: String? = null,
+    var value: Long = 0L,
+) : ModelObject() {
+
+    companion object {
+        private const val CURRENCY = "currency"
+        private const val VALUE = "value"
+
+        @JvmField
+        val SERIALIZER: Serializer<Amount> = object : Serializer<Amount> {
+            override fun serialize(modelObject: Amount): JSONObject {
+                return try {
+                    JSONObject().apply {
+                        putOpt(CURRENCY, modelObject.currency)
+                        putOpt(VALUE, modelObject.value)
+                    }
+                } catch (e: JSONException) {
+                    throw ModelSerializationException(Amount::class.java, e)
+                }
+            }
+
+            override fun deserialize(jsonObject: JSONObject): Amount {
+                return Amount(
+                    currency = jsonObject.getStringOrNull(CURRENCY),
+                    value = jsonObject.getLongOrNull(VALUE) ?: EMPTY_VALUE,
+                )
+            }
+        }
+    }
+}
+
+// TODO - Originally in AmountExt
+private const val EMPTY_VALUE = -1L
