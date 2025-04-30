6242dbb811b59c081d4af7b1ec5aaf1b93543686
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-04-30 14:59:21 +0200

    Add CheckoutSessionProvider

---
+++ core/src/main/java/com/adyen/checkout/core/sessions/data/SessionSetupRequest.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 30/4/2025.
+ */
+
+package com.adyen.checkout.core.sessions.data
+
+import com.adyen.checkout.core.exception.ModelSerializationException
+import com.adyen.checkout.core.internal.data.model.ModelObject
+import com.adyen.checkout.core.internal.data.model.getStringOrNull
+import kotlinx.parcelize.Parcelize
+import org.json.JSONException
+import org.json.JSONObject
+
+@Parcelize
+data class SessionSetupRequest(
+    val sessionData: String,
+    // TODO - Partial Payment Flow
+//    val order: OrderRequest?
+) : ModelObject() {
+
+    companion object {
+        private const val SESSION_DATA = "sessionData"
+//        private const val ORDER = "order"
+
+        @JvmField
+        val SERIALIZER: Serializer<SessionSetupRequest> = object : Serializer<SessionSetupRequest> {
+            override fun serialize(modelObject: SessionSetupRequest): JSONObject {
+                val jsonObject = JSONObject()
+                try {
+                    jsonObject.putOpt(SESSION_DATA, modelObject.sessionData)
+//                    jsonObject.putOpt(ORDER, ModelUtils.serializeOpt(modelObject.order, OrderRequest.SERIALIZER))
+                } catch (e: JSONException) {
+                    throw ModelSerializationException(SessionSetupRequest::class.java, e)
+                }
+                return jsonObject
+            }
+
+            override fun deserialize(jsonObject: JSONObject): SessionSetupRequest {
+                return try {
+                    SessionSetupRequest(
+                        sessionData = jsonObject.getStringOrNull(SESSION_DATA).orEmpty(),
+//                        order = ModelUtils.deserializeOpt(jsonObject.optJSONObject(ORDER), OrderRequest.SERIALIZER)
+                    )
+                } catch (e: JSONException) {
+                    throw ModelSerializationException(SessionSetupRequest::class.java, e)
+                }
+            }
+        }
+    }
+}
