ccee116af4786292452ab025d9ee94f7c7077e8c
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-05-06 11:18:08 +0200

    Add ComponentParamsMapper related files to core module
    COSDK-442

---
+++ core/src/main/java/com/adyen/checkout/core/internal/ui/model/ComponentParams.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 6/5/2025.
+ */
+
+package com.adyen.checkout.core.internal.ui.model
+
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.core.Environment
+import com.adyen.checkout.core.data.model.Amount
+import java.util.Locale
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+interface ComponentParams {
+    val shopperLocale: Locale
+    val environment: Environment
+    val clientKey: String
+
+    // TODO - Analytics
+//    val analyticsParams: AnalyticsParams
+    val isCreatedByDropIn: Boolean
+    val amount: Amount?
+}
---
+++ core/src/main/java/com/adyen/checkout/core/internal/ui/model/DropInOverrideParams.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 6/5/2025.
+ */
+
+package com.adyen.checkout.core.internal.ui.model
+
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.core.data.model.Amount
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+data class DropInOverrideParams(
+    val amount: Amount?,
+    val sessionParams: SessionParams?,
+    val isSubmitButtonVisible: Boolean = true
+)
