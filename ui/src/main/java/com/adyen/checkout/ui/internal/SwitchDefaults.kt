9f9fe9d08faeaccdaa13b37a4cdc61b3a5be5efa
Oscar Spruit <oscar.spruit@adyen.com>
2025-05-09 14:52:29 +0200

    Add switch composable
    COSDK-147

---
+++ ui/src/main/java/com/adyen/checkout/ui/internal/SwitchDefaults.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 9/5/2025.
+ */
+
+package com.adyen.checkout.ui.internal
+
+import androidx.compose.runtime.Composable
+import com.adyen.checkout.ui.theme.AdyenSwitchStyle
+
+internal object SwitchDefaults {
+
+    @Composable
+    fun switchStyle(style: AdyenSwitchStyle?): InternalSwitchStyle {
+        val colors = AdyenCheckoutTheme.colors
+        return InternalSwitchStyle(
+            checkedHandleColor = style?.checkedHandleColor?.toCompose() ?: colors.background,
+            checkedTrackColor = style?.checkedHandleColor?.toCompose() ?: colors.primary,
+            uncheckedHandleColor = style?.checkedHandleColor?.toCompose() ?: colors.primary,
+            uncheckedTrackColor = style?.checkedHandleColor?.toCompose() ?: colors.background,
+        )
+    }
+}
