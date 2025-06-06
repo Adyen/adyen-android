ad4f351ef0b35b6e6d2da7d038709e75001fbff4
Ararat Mnatsakanyan <ararat.mnatsakanyan@adyen.com>
2025-06-06 13:49:22 +0200

    Add analytics classes to core module
    COSDK-488

---
+++ core/src/main/java/com/adyen/checkout/core/internal/analytics/data/remote/AnalyticsSetupProvider.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 6/6/2025.
+ */
+
+package com.adyen.checkout.core.internal.analytics.data.remote
+
+import com.adyen.checkout.core.internal.data.model.AnalyticsSetupRequest
+
+internal interface AnalyticsSetupProvider {
+    fun provide(): AnalyticsSetupRequest
+}
---
+++ core/src/main/java/com/adyen/checkout/core/internal/data/model/AnalyticsTrackRequest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 6/6/2025.
+ */
+
+package com.adyen.checkout.core.internal.data.model
+
+import com.adyen.checkout.core.exception.ModelSerializationException
+import com.adyen.checkout.core.internal.data.model.ModelUtils.deserializeOptList
+import kotlinx.parcelize.Parcelize
+import org.json.JSONException
+import org.json.JSONObject
+
+@Parcelize
+internal data class AnalyticsTrackRequest(
+    val channel: String?,
+    val platform: String?,
+    val info: List<AnalyticsTrackInfo>?,
+    val logs: List<AnalyticsTrackLog>?,
+    val errors: List<AnalyticsTrackError>?,
+) : ModelObject() {
+    companion object {
+        private const val CHANNEL = "channel"
+        private const val PLATFORM = "platform"
+        private const val INFO = "info"
+        private const val LOGS = "logs"
+        private const val ERRORS = "errors"
+
+        @JvmField
+        val SERIALIZER: Serializer<AnalyticsTrackRequest> = object : Serializer<AnalyticsTrackRequest> {
+            override fun serialize(modelObject: AnalyticsTrackRequest): JSONObject {
+                try {
+                    return JSONObject().apply {
+                        putOpt(CHANNEL, modelObject.channel)
+                        putOpt(PLATFORM, modelObject.platform)
+                        putOpt(
+                            INFO,
+                            ModelUtils.serializeOptList(modelObject.info, AnalyticsTrackInfo.SERIALIZER),
+                        )
+                        putOpt(
+                            LOGS,
+                            ModelUtils.serializeOptList(modelObject.logs, AnalyticsTrackLog.SERIALIZER),
+                        )
+                        putOpt(
+                            ERRORS,
+                            ModelUtils.serializeOptList(modelObject.errors, AnalyticsTrackError.SERIALIZER),
+                        )
+                    }
+                } catch (e: JSONException) {
+                    throw ModelSerializationException(AnalyticsTrackRequest::class.java, e)
+                }
+            }
+
+            override fun deserialize(jsonObject: JSONObject): AnalyticsTrackRequest {
+                return try {
+                    with(jsonObject) {
+                        AnalyticsTrackRequest(
+                            channel = getStringOrNull(CHANNEL),
+                            platform = getStringOrNull(PLATFORM),
+                            info = deserializeOptList(getJSONArray(INFO), AnalyticsTrackInfo.SERIALIZER),
+                            logs = deserializeOptList(getJSONArray(LOGS), AnalyticsTrackLog.SERIALIZER),
+                            errors = deserializeOptList(getJSONArray(ERRORS), AnalyticsTrackError.SERIALIZER),
+                        )
+                    }
+                } catch (e: JSONException) {
+                    throw ModelSerializationException(AnalyticsTrackRequest::class.java, e)
+                }
+            }
+        }
+    }
+}
