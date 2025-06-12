b782adac7a5516d8cdfa5e244b7f6fb0ba6caa05
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-06-12 14:35:07 +0200

    Pass component params to PaymentFacilitator
    COSDK-142

--- core/src/main/java/com/adyen/checkout/core/internal/PaymentFacilitator.kt
+++ core/src/main/java/com/adyen/checkout/core/internal/PaymentFacilitator.kt
@@ -13,43 +13,24 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import com.adyen.checkout.core.CheckoutCallback
-import com.adyen.checkout.core.CheckoutConfiguration
 import com.adyen.checkout.core.internal.ui.PaymentDelegate
-import com.adyen.checkout.core.internal.ui.model.ButtonComponentParamsMapper
-import com.adyen.checkout.core.internal.ui.model.CommonComponentParamsMapper
-import com.adyen.checkout.core.internal.ui.model.SessionParamsFactory
+import com.adyen.checkout.core.internal.ui.model.ButtonComponentParams
 import com.adyen.checkout.core.mbway.internal.ui.MBWayComponentState
 import com.adyen.checkout.core.mbway.internal.ui.MBWayDelegate
-import com.adyen.checkout.core.mbway.internal.ui.getMBWayConfiguration
-import com.adyen.checkout.core.sessions.CheckoutSession
 import com.adyen.checkout.core.sessions.SessionInteractor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import java.util.Locale
 
-// TODO - Have separate facilitators for sessions and advanced?
 internal class PaymentFacilitator(
     private val coroutineScope: CoroutineScope,
-    private val checkoutSession: CheckoutSession?,
-    private val checkoutConfiguration: CheckoutConfiguration,
     private val checkoutCallback: CheckoutCallback,
-    private val sessionInteractor: SessionInteractor?
+    private val sessionInteractor: SessionInteractor?,
+
+    // TODO - Switch to Component Params
+    private val componentParams: ButtonComponentParams,
 ) {
-
-    private val componentParams = ButtonComponentParamsMapper(CommonComponentParamsMapper()).mapToParams(
-        checkoutConfiguration = checkoutConfiguration,
-
-        // TODO - Add locale support, For now it's hardcoded to US
-//        deviceLocale = localeProvider.getLocale(application)
-        deviceLocale = Locale.US,
-        dropInOverrideParams = null,
-        componentSessionParams = checkoutSession?.let {
-            SessionParamsFactory.create(checkoutSession)
-        },
-        componentConfiguration = checkoutConfiguration.getMBWayConfiguration(),
-    )
 
     // TODO - Make it a val, initialize it based on txVariant?
     private val paymentDelegate: PaymentDelegate<MBWayComponentState> = MBWayDelegate(coroutineScope, componentParams)
