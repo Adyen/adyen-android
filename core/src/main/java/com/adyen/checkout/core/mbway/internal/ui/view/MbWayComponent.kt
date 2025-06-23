af304ac0a2f38001b2b9da2ad9a2489a5a268687
Oscar Spruit <oscar.spruit@adyen.com>
2025-06-23 10:45:31 +0200

    Show selected state of country code item
    COSDK-506

--- core/src/main/java/com/adyen/checkout/core/mbway/internal/ui/view/MbWayComponent.kt
+++ core/src/main/java/com/adyen/checkout/core/mbway/internal/ui/view/MbWayComponent.kt
@@ -8,6 +8,8 @@
 
 package com.adyen.checkout.core.mbway.internal.ui.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,17 +17,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -124,10 +133,38 @@ internal fun MbWayComponent(
                         .padding(16.dp),
                 ) {
                     viewState.countries.forEach { country ->
+                        val isSelected = country == viewState.countryCodeFieldState.value
                         Row(
-                            modifier = Modifier.padding(12.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(AdyenCheckoutTheme.elements.cornerRadius.dp))
+                                .let {
+                                    if (isSelected) {
+                                        it.background(AdyenCheckoutTheme.colors.container)
+                                    } else {
+                                        it
+                                    }
+                                }
+                                .clickable(
+                                    interactionSource = null,
+                                    indication = ripple(color = AdyenCheckoutTheme.colors.text),
+                                ) {
+                                    fieldChangeListener.onFieldValueChanged(MBWayFieldId.COUNTRY_CODE, country)
+                                    showCountryCodeDialog = false
+                                }
+                                .fillMaxWidth()
+                                .padding(12.dp),
                         ) {
                             BodyEmphasized(country.toShortString())
+
+                            if (isSelected) {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(com.adyen.checkout.test.R.drawable.ic_checkmark),
+                                    contentDescription = null,
+                                    tint = AdyenCheckoutTheme.colors.text,
+                                )
+                            }
                         }
                     }
                 }
