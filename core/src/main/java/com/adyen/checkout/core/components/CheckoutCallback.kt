751cf57656cd9cee140e44008df676dc38f3d7e1
Oscar Spruit <oscar.spruit@adyen.com>
2025-07-07 12:41:33 +0200

    Create PoC for callback DSL

--- core/src/main/java/com/adyen/checkout/core/components/CheckoutCallback.kt
+++ core/src/main/java/com/adyen/checkout/core/components/CheckoutCallback.kt
@@ -10,12 +10,66 @@ package com.adyen.checkout.core.components
 
 import com.adyen.checkout.core.components.paymentmethod.PaymentComponentState
 
-interface CheckoutCallback {
+//interface CheckoutCallback {
+//
+//    fun beforeSubmit(paymentComponentState: PaymentComponentState<*>): Boolean = false
+//
+//    fun onSubmit(
+//        paymentComponentState: PaymentComponentState<*>,
+//        onCompletion: (CheckoutResult) -> Unit
+//    )
+//}
 
-    fun beforeSubmit(paymentComponentState: PaymentComponentState<*>): Boolean = false
+class CheckoutCallback(
+    onSubmit: OnSubmitCallback,
+    beforeSubmit: BeforeSubmitCallback = BeforeSubmitCallback { false },
+    additionalCallbacks: CheckoutCallback.() -> Unit = {},
+) {
 
-    fun onSubmit(
-        paymentComponentState: PaymentComponentState<*>,
-        onCompletion: (CheckoutResult) -> Unit
+    private val callbacks = mutableMapOf(
+        "beforeSubmit" to beforeSubmit,
+        "onSubmit" to onSubmit,
     )
+
+    init {
+        apply(additionalCallbacks)
+    }
+
+    internal fun addCallback(name: String, callback: Callback) {
+        callbacks[name] = callback
+    }
+
+    internal fun <T : Callback> getCallback(name: String): T? {
+        @Suppress("UNCHECKED_CAST")
+        return callbacks[name] as? T?
+    }
+}
+
+interface Callback
+
+fun interface BeforeSubmitCallback : Callback {
+    fun beforeSubmit(paymentComponentState: PaymentComponentState<*>): Boolean
+}
+
+fun interface OnSubmitCallback : Callback {
+    fun onSubmit(paymentComponentState: PaymentComponentState<*>)
+}
+
+fun interface OnBinLookupCallback : Callback {
+    fun onBinLookup(bin: String)
+}
+
+fun CheckoutCallback.onBinLookup(onBinLookup: OnBinLookupCallback) {
+    addCallback("onBinLookup", onBinLookup)
+}
+
+object ExampleImplementation {
+
+    val checkoutCallback = CheckoutCallback(
+        onSubmit = {},
+    ) {
+        onBinLookup { bin ->
+            // Do something with the bin
+        }
+    }
 }
