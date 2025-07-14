6e9be43ed4bbbb5629b8b2c17e437f54dc155ec2
Oscar Spruit <oscar.spruit@adyen.com>
2025-07-14 15:57:32 +0200

    Add deprecation warnings to online banking sk

--- online-banking-sk/src/main/java/com/adyen/checkout/onlinebankingsk/OnlineBankingSKConfiguration.kt
+++ online-banking-sk/src/main/java/com/adyen/checkout/onlinebankingsk/OnlineBankingSKConfiguration.kt
@@ -25,6 +25,7 @@ import java.util.Locale
  */
 @Suppress("LongParameterList")
 @Parcelize
+@Deprecated("Configuration classes are deprecated, use CheckoutConfiguration instead.")
 class OnlineBankingSKConfiguration private constructor(
     override val shopperLocale: Locale?,
     override val environment: Environment,
@@ -38,6 +39,7 @@ class OnlineBankingSKConfiguration private constructor(
     /**
      * Builder to create an [OnlineBankingSKConfiguration].
      */
+    @Deprecated("Configuration builders are deprecated, use CheckoutConfiguration instead.")
     class Builder : OnlineBankingConfigurationBuilder<OnlineBankingSKConfiguration, Builder> {
 
         /**
