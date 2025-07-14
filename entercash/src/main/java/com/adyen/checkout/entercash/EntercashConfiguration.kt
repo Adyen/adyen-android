ed2a2a158a2597e034d69c3d3867ae156a00942e
Oscar Spruit <oscar.spruit@adyen.com>
2025-07-14 15:00:20 +0200

    Add deprecation warnings to entercash

--- entercash/src/main/java/com/adyen/checkout/entercash/EntercashConfiguration.kt
+++ entercash/src/main/java/com/adyen/checkout/entercash/EntercashConfiguration.kt
@@ -25,6 +25,7 @@ import java.util.Locale
  */
 @Parcelize
 @Suppress("LongParameterList")
+@Deprecated("Configuration classes are deprecated, use CheckoutConfiguration instead.")
 class EntercashConfiguration private constructor(
     override val shopperLocale: Locale?,
     override val environment: Environment,
@@ -40,6 +41,7 @@ class EntercashConfiguration private constructor(
     /**
      * Builder to create an [EntercashConfiguration].
      */
+    @Deprecated("Configuration builders are deprecated, use CheckoutConfiguration instead.")
     class Builder : IssuerListBuilder<EntercashConfiguration, Builder> {
 
         /**
