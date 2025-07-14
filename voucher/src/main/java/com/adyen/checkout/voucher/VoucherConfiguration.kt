ab43477883b539970015ad57fe15ed2f31dde7f9
Oscar Spruit <oscar.spruit@adyen.com>
2025-07-14 16:08:10 +0200

    Add deprecation warnings to voucher

--- voucher/src/main/java/com/adyen/checkout/voucher/VoucherConfiguration.kt
+++ voucher/src/main/java/com/adyen/checkout/voucher/VoucherConfiguration.kt
@@ -23,6 +23,7 @@ import java.util.Locale
  * Configuration class for the [VoucherComponent].
  */
 @Parcelize
+@Deprecated("Configuration classes are deprecated, use CheckoutConfiguration instead.")
 class VoucherConfiguration private constructor(
     override val shopperLocale: Locale?,
     override val environment: Environment,
@@ -34,6 +35,7 @@ class VoucherConfiguration private constructor(
     /**
      * Builder to create a [VoucherConfiguration].
      */
+    @Deprecated("Configuration builders are deprecated, use CheckoutConfiguration instead.")
     class Builder : BaseConfigurationBuilder<VoucherConfiguration, Builder> {
 
         /**
