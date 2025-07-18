32e1029525e5d0020fb7df9b031f162b0864745b
Oscar Spruit <oscar.spruit@adyen.com>
2025-07-18 10:28:56 +0200

    Add payment component state implementation for testing to :core
    COSDK-144

---
+++ core/src/test/java/com/adyen/checkout/core/components/paymentmethod/TestComponentState.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 18/7/2025.
+ */
+
+package com.adyen.checkout.core.components.paymentmethod
+
+import com.adyen.checkout.core.components.data.PaymentComponentData
+import com.adyen.checkout.core.components.data.model.Amount
+
+class TestComponentState(
+    override val data: PaymentComponentData<TestPaymentMethod> = PaymentComponentData(
+        paymentMethod = TestPaymentMethod(),
+        order = null,
+        amount = Amount(),
+    ),
+    override val isValid: Boolean = true,
+) : PaymentComponentState<TestPaymentMethod>
