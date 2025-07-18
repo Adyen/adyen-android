ba2effe3c1f0d270ab0701a1164656f7e029a391
Oscar Spruit <oscar.spruit@adyen.com>
2025-07-18 15:29:11 +0200

    Duplicate session request api call with new models

--- example-app/src/main/java/com/adyen/checkout/example/ui/googlepay/compose/SessionsGooglePayViewModel.kt
+++ example-app/src/main/java/com/adyen/checkout/example/ui/googlepay/compose/SessionsGooglePayViewModel.kt
@@ -92,7 +92,7 @@ internal class SessionsGooglePayViewModel @Inject constructor(
     }
 
     private suspend fun getSession(paymentMethodType: String): CheckoutSession? {
-        val sessionModel = paymentsRepository.createSession(
+        val sessionModel = paymentsRepository.createSessionOld(
             getSessionRequest(
                 merchantAccount = keyValueStorage.getMerchantAccount(),
                 shopperReference = keyValueStorage.getShopperReference(),
