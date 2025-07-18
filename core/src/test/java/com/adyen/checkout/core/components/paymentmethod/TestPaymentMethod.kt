5eff5b3b4ab5409aa4baa74f1f0379f1420bc7b7
Oscar Spruit <oscar.spruit@adyen.com>
2025-07-18 10:29:14 +0200

    Add payment method details implementation for testing to :core
    COSDK-144

---
+++ core/src/test/java/com/adyen/checkout/core/components/paymentmethod/TestPaymentMethod.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 18/7/2025.
+ */
+
+package com.adyen.checkout.core.components.paymentmethod
+
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class TestPaymentMethod(
+    override var type: String? = "test",
+    override var checkoutAttemptId: String? = null
+) : PaymentMethodDetails()
