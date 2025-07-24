777f1f8a2819cde0768b534770403cb8ce48f467
Ararat Mnatsakanyan <ararat.mnatsakanyan@adyen.com>
2025-07-24 12:30:22 +0200

    Add Await Component composable
    COSDK-547

---
+++ ui/src/main/java/com/adyen/checkout/ui/internal/ProgressBar.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 23/7/2025.
+ */
+
+package com.adyen.checkout.ui.internal
+
+import androidx.annotation.RestrictTo
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.adyen.checkout.ui.theme.AdyenCheckoutTheme as Theme
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+@Composable
+fun ProgressBar(
+    modifier: Modifier = Modifier,
+) {
+    val colors = AdyenCheckoutTheme.colors
+
+    CircularProgressIndicator(
+        modifier = modifier,
+        color = colors.primary,
+        trackColor = colors.disabled,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProgressBarPreview(
+    @PreviewParameter(ThemePreviewParameterProvider::class) theme: Theme,
+) {
+    AdyenCheckoutTheme(theme) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(Dimensions.Large),
+            modifier = Modifier
+                .background(AdyenCheckoutTheme.colors.background)
+                .padding(Dimensions.Large),
+        ) {
+            ProgressBar()
+        }
+    }
+}
