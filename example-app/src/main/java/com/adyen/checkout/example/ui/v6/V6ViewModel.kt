4bf99cc66036857ed6a3bcacac190099cdd98f05
Oscar Spruit <oscar.spruit@adyen.com>
2025-08-11 16:45:04 +0200

    Make payments request on submit

--- example-app/src/main/java/com/adyen/checkout/example/ui/v6/V6ViewModel.kt
+++ example-app/src/main/java/com/adyen/checkout/example/ui/v6/V6ViewModel.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.adyen.checkout.core.action.data.Action
 import com.adyen.checkout.core.action.data.ActionComponentData
 import com.adyen.checkout.core.common.Environment
 import com.adyen.checkout.core.components.Checkout
@@ -22,14 +23,17 @@ import com.adyen.checkout.core.components.CheckoutConfiguration
 import com.adyen.checkout.core.components.CheckoutContext
 import com.adyen.checkout.core.components.CheckoutResult
 import com.adyen.checkout.core.components.ComponentError
+import com.adyen.checkout.core.components.data.PaymentComponentData
 import com.adyen.checkout.core.components.paymentmethod.PaymentComponentState
 import com.adyen.checkout.example.BuildConfig
 import com.adyen.checkout.example.data.storage.KeyValueStorage
 import com.adyen.checkout.example.extensions.getLogTag
 import com.adyen.checkout.example.repositories.PaymentsRepository
+import com.adyen.checkout.example.service.createPaymentRequest
 import com.adyen.checkout.example.service.getPaymentMethodRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 import javax.inject.Inject
 
 @HiltViewModel
@@ -86,16 +90,39 @@ internal class V6ViewModel @Inject constructor(
         }
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    private fun onSubmit(paymentComponentState: PaymentComponentState<*>): CheckoutResult {
-        // TODO - make payments call
-        return CheckoutResult.Finished()
+    private suspend fun onSubmit(paymentComponentState: PaymentComponentState<*>): CheckoutResult {
+        val paymentComponentData = PaymentComponentData.SERIALIZER.serialize(paymentComponentState.data)
+        val paymentRequest = createPaymentRequest(
+            paymentComponentData = paymentComponentData,
+            shopperReference = keyValueStorage.getShopperReference(),
+            amount = keyValueStorage.getAmount(),
+            countryCode = keyValueStorage.getCountry(),
+            merchantAccount = keyValueStorage.getMerchantAccount(),
+            // TODO - Replace with correct URL once redirects are implemented
+            redirectUrl = "test",
+            threeDSMode = keyValueStorage.getThreeDSMode(),
+            shopperEmail = keyValueStorage.getShopperEmail(),
+        )
+        val response = paymentsRepository.makePaymentsRequest(paymentRequest)
+        return handleResponse(response)
     }
 
     @Suppress("UnusedParameter")
     private fun onAdditionalDetails(actionComponentData: ActionComponentData): CheckoutResult {
         // TODO - make payments details call
         return CheckoutResult.Finished()
+    }
+
+    private fun handleResponse(json: JSONObject?): CheckoutResult {
+        return when {
+            json == null -> CheckoutResult.Error(ComponentError(RuntimeException("Network error")))
+            json.has("action") -> {
+                val action = Action.SERIALIZER.deserialize(json.getJSONObject("action"))
+                CheckoutResult.Action(action)
+            }
+
+            else -> CheckoutResult.Finished()
+        }
     }
 
     @Suppress("UnusedParameter")
