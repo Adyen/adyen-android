81dcca33620e7574bf182a060fe771e8dd010298
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-08-27 10:13:35 +0200

    Move ImageLoader related classes to core and ui modules
    COSDK-518

---
+++ ui/src/main/java/com/adyen/checkout/ui/internal/LogoSize.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 26/8/2025.
+ */
+
+package com.adyen.checkout.ui.internal
+
+import androidx.annotation.RestrictTo
+
+/**
+ * The logo size.
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+enum class LogoSize {
+    /**
+     * Size for small logos (height: 26dp).
+     */
+    SMALL,
+
+    /**
+     * Size for medium logos (height: 50dp).
+     */
+    MEDIUM,
+
+    /**
+     * Size for large logos (height: 100dp).
+     */
+    LARGE;
+
+    override fun toString(): String {
+        return name.lowercase()
+    }
+}
