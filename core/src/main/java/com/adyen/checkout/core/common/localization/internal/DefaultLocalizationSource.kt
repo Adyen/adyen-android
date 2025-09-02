f0b200231be395c7cf89f9f729cd0c0c20e42072
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-09-02 17:03:26 +0200

    Add internal classes to resolve localizations
    COSDK-146

---
+++ core/src/main/java/com/adyen/checkout/core/common/localization/internal/DefaultLocalizationSource.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 2/9/2025.
+ */
+
+package com.adyen.checkout.core.common.localization.internal
+
+import android.content.Context
+import com.adyen.checkout.core.common.localization.CheckoutLocalizationKey
+import java.util.Locale
+
+internal class DefaultLocalizationSource {
+
+    @Suppress("UnusedParameter")
+    fun getString(context: Context, locale: Locale, key: CheckoutLocalizationKey): String {
+        TODO("Not yet implemented")
+    }
+}
