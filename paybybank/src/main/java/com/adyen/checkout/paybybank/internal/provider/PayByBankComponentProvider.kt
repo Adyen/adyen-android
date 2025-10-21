54d9a95202d4c2605e3736c740547e5706ad2197
Ararat Mnatsakanyan <ararat.mnatsakanyan@adyen.com>
2025-10-21 15:07:38 +0200

    Use SdkDataProvider to create SdkData in PayByBankDelegate
    COSDK-513

--- paybybank/src/main/java/com/adyen/checkout/paybybank/internal/provider/PayByBankComponentProvider.kt
+++ paybybank/src/main/java/com/adyen/checkout/paybybank/internal/provider/PayByBankComponentProvider.kt
@@ -26,6 +26,7 @@ import com.adyen.checkout.components.core.internal.analytics.AnalyticsManager
 import com.adyen.checkout.components.core.internal.analytics.AnalyticsManagerFactory
 import com.adyen.checkout.components.core.internal.analytics.AnalyticsSource
 import com.adyen.checkout.components.core.internal.provider.PaymentComponentProvider
+import com.adyen.checkout.components.core.internal.provider.SdkDataProvider
 import com.adyen.checkout.components.core.internal.ui.model.CommonComponentParamsMapper
 import com.adyen.checkout.components.core.internal.ui.model.DropInOverrideParams
 import com.adyen.checkout.components.core.internal.ui.model.GenericComponentParamsMapper
@@ -105,6 +106,7 @@ constructor(
                 componentParams = componentParams,
                 analyticsManager = analyticsManager,
                 submitHandler = SubmitHandler(savedStateHandle),
+                sdkDataProvider = SdkDataProvider(analyticsManager),
             )
 
             val genericActionDelegate =
@@ -192,6 +194,7 @@ constructor(
                 componentParams = componentParams,
                 analyticsManager = analyticsManager,
                 submitHandler = SubmitHandler(savedStateHandle),
+                sdkDataProvider = SdkDataProvider(analyticsManager),
             )
 
             val genericActionDelegate =
