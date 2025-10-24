acb2632444b6c3c31dffc98ee84da015848e6e42
Ararat Mnatsakanyan <ararat.mnatsakanyan@adyen.com>
2025-10-24 13:52:42 +0200

    Remove `ModelObject` implementation for SdkData data classes and implement create a serialize() function
    COSDK-513

--- components-core/src/main/java/com/adyen/checkout/components/core/internal/data/model/sdkData/SdkData.kt
+++ components-core/src/main/java/com/adyen/checkout/components/core/internal/data/model/sdkData/SdkData.kt
@@ -8,24 +8,25 @@
 
 package com.adyen.checkout.components.core.internal.data.model.sdkData
 
-import com.adyen.checkout.core.exception.ModelSerializationException
-import com.adyen.checkout.core.internal.data.model.ModelObject
-import com.adyen.checkout.core.internal.data.model.ModelUtils.deserializeOpt
-import com.adyen.checkout.core.internal.data.model.ModelUtils.serializeOpt
-import com.adyen.checkout.core.internal.data.model.getBooleanOrNull
-import com.adyen.checkout.core.internal.data.model.getLongOrNull
-import kotlinx.parcelize.Parcelize
 import org.json.JSONException
 import org.json.JSONObject
 
-@Parcelize
 internal data class SdkData(
     val schemaVersion: Int,
     val analytics: Analytics? = null,
     val authentication: Authentication? = null,
     val createdAt: Long? = null,
     val supportNativeRedirect: Boolean? = null,
-) : ModelObject() {
+) {
+
+    @Throws(JSONException::class)
+    fun serialize() = JSONObject().apply {
+        putOpt(SCHEMA_VERSION, schemaVersion)
+        putOpt(ANALYTICS, analytics?.serialize())
+        putOpt(AUTHENTICATION, authentication?.serialize())
+        putOpt(CREATED_AT, createdAt)
+        putOpt(SUPPORT_NATIVE_REDIRECT, supportNativeRedirect)
+    }
 
     companion object {
         private const val SCHEMA_VERSION = "schemaVersion"
@@ -33,39 +34,5 @@ internal data class SdkData(
         private const val AUTHENTICATION = "authentication"
         private const val CREATED_AT = "createdAt"
         private const val SUPPORT_NATIVE_REDIRECT = "supportNativeRedirect"
-
-        @JvmField
-        val SERIALIZER: Serializer<SdkData> = object : Serializer<SdkData> {
-            override fun serialize(modelObject: SdkData): JSONObject {
-                return try {
-                    JSONObject().apply {
-                        putOpt(SCHEMA_VERSION, modelObject.schemaVersion)
-                        putOpt(ANALYTICS, serializeOpt(modelObject.analytics, Analytics.SERIALIZER))
-                        putOpt(AUTHENTICATION, serializeOpt(modelObject.authentication, Authentication.SERIALIZER))
-                        putOpt(CREATED_AT, modelObject.createdAt)
-                        putOpt(SUPPORT_NATIVE_REDIRECT, modelObject.supportNativeRedirect)
-                    }
-                } catch (e: JSONException) {
-                    throw ModelSerializationException(SdkData::class.java, e)
-                }
-            }
-
-            override fun deserialize(jsonObject: JSONObject): SdkData {
-                return try {
-                    SdkData(
-                        schemaVersion = jsonObject.getInt(SCHEMA_VERSION),
-                        analytics = deserializeOpt(jsonObject.optJSONObject(ANALYTICS), Analytics.SERIALIZER),
-                        authentication = deserializeOpt(
-                            jsonObject.optJSONObject(AUTHENTICATION),
-                            Authentication.SERIALIZER,
-                        ),
-                        createdAt = jsonObject.getLongOrNull(CREATED_AT),
-                        supportNativeRedirect = jsonObject.getBooleanOrNull(SUPPORT_NATIVE_REDIRECT),
-                    )
-                } catch (e: JSONException) {
-                    throw ModelSerializationException(SdkData::class.java, e)
-                }
-            }
-        }
     }
 }
