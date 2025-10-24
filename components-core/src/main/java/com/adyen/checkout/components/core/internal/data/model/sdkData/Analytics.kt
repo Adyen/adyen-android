8b995f89733663e9826c3927072214e87decd09d
Ararat Mnatsakanyan <ararat.mnatsakanyan@adyen.com>
2025-10-24 13:52:42 +0200

    Remove `ModelObject` implementation for SdkData data classes and implement create a serialize() function
    COSDK-513

--- components-core/src/main/java/com/adyen/checkout/components/core/internal/data/model/sdkData/Analytics.kt
+++ components-core/src/main/java/com/adyen/checkout/components/core/internal/data/model/sdkData/Analytics.kt
@@ -8,38 +8,19 @@
 
 package com.adyen.checkout.components.core.internal.data.model.sdkData
 
-import com.adyen.checkout.core.exception.ModelSerializationException
-import com.adyen.checkout.core.internal.data.model.ModelObject
-import com.adyen.checkout.core.internal.data.model.getStringOrNull
-import kotlinx.parcelize.Parcelize
 import org.json.JSONException
 import org.json.JSONObject
 
-@Parcelize
 internal data class Analytics(
     val checkoutAttemptId: String? = null
-) : ModelObject() {
+) {
+
+    @Throws(JSONException::class)
+    fun serialize() = JSONObject().apply {
+        putOpt(CHECKOUT_ATTEMPT_ID, checkoutAttemptId)
+    }
 
     companion object {
         private const val CHECKOUT_ATTEMPT_ID = "checkoutAttemptId"
-
-        @JvmField
-        val SERIALIZER: Serializer<Analytics> = object : Serializer<Analytics> {
-            override fun serialize(modelObject: Analytics): JSONObject {
-                return try {
-                    JSONObject().apply {
-                        putOpt(CHECKOUT_ATTEMPT_ID, modelObject.checkoutAttemptId)
-                    }
-                } catch (e: JSONException) {
-                    throw ModelSerializationException(Analytics::class.java, e)
-                }
-            }
-
-            override fun deserialize(jsonObject: JSONObject): Analytics {
-                return Analytics(
-                    checkoutAttemptId = jsonObject.getStringOrNull(CHECKOUT_ATTEMPT_ID),
-                )
-            }
-        }
     }
 }
