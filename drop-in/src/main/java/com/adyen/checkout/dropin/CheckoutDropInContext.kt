cb7e625e69538fb50def2810b57cd7c426f96f85
Oscar Spruit <oscar.spruit@adyen.com>
2025-11-10 13:41:14 +0100

    Add initialization functions for drop-in
    COSDK-719

---
+++ drop-in/src/main/java/com/adyen/checkout/dropin/CheckoutDropInContext.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 10/11/2025.
+ */
+
+package com.adyen.checkout.dropin
+
+import com.adyen.checkout.core.components.CheckoutConfiguration
+import com.adyen.checkout.core.components.data.model.PaymentMethodsApiResponse
+import com.adyen.checkout.core.sessions.CheckoutSession
+
+interface CheckoutDropInContext {
+
+    data class Sessions internal constructor(
+        val checkoutSession: CheckoutSession,
+        val checkoutConfiguration: CheckoutConfiguration,
+        internal val publicKey: String?,
+    ) : CheckoutDropInContext
+
+    data class Advanced internal constructor(
+        val paymentMethodsApiResponse: PaymentMethodsApiResponse,
+        val checkoutConfiguration: CheckoutConfiguration,
+        internal val publicKey: String?,
+    ) : CheckoutDropInContext
+}
