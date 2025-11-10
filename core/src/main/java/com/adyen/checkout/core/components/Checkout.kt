df1fbca27e3d4ffbd432702a51fd1a2d56456283
Oscar Spruit <oscar.spruit@adyen.com>
2025-11-10 13:34:36 +0100

    Extract checkout initialization logic into internal object
    This way we can reuse the logic for drop-in without exposing anything inside of the Checkout object.
    
    COSDK-719

--- core/src/main/java/com/adyen/checkout/core/components/Checkout.kt
+++ core/src/main/java/com/adyen/checkout/core/components/Checkout.kt
@@ -8,16 +8,9 @@
 
 package com.adyen.checkout.core.components
 
-import com.adyen.checkout.core.common.AdyenLogLevel
-import com.adyen.checkout.core.common.internal.api.HttpClientFactory
-import com.adyen.checkout.core.common.internal.data.api.DefaultPublicKeyRepository
-import com.adyen.checkout.core.common.internal.data.api.PublicKeyService
-import com.adyen.checkout.core.common.internal.helper.adyenLog
 import com.adyen.checkout.core.components.data.model.PaymentMethodsApiResponse
-import com.adyen.checkout.core.sessions.CheckoutSession
-import com.adyen.checkout.core.sessions.CheckoutSessionResult
+import com.adyen.checkout.core.components.internal.CheckoutInitializer
 import com.adyen.checkout.core.sessions.SessionModel
-import com.adyen.checkout.core.sessions.internal.CheckoutSessionProvider
 
 object Checkout {
 
@@ -26,19 +19,22 @@ object Checkout {
         checkoutConfiguration: CheckoutConfiguration,
         checkoutCallbacks: CheckoutCallbacks,
     ): Result {
-        // TODO - Fetch checkoutAttemptId
-        val checkoutSession = getCheckoutSession(sessionModel, checkoutConfiguration)
-        val publicKey = fetchPublicKey(checkoutConfiguration)
+        val initializationData = CheckoutInitializer.initialize(
+            checkoutConfiguration = checkoutConfiguration,
+            sessionModel = sessionModel,
+        )
+
         return when {
-            checkoutSession == null -> {
+            initializationData.checkoutSession == null -> {
                 Result.Error("Failed to initialize sessions.")
             }
+
             else -> Result.Success(
                 checkoutContext = CheckoutContext.Sessions(
-                    checkoutSession = checkoutSession,
+                    checkoutSession = initializationData.checkoutSession,
                     checkoutConfiguration = checkoutConfiguration,
                     checkoutCallbacks = checkoutCallbacks,
-                    publicKey = publicKey,
+                    publicKey = initializationData.publicKey,
                 ),
             )
         }
@@ -49,49 +45,18 @@ object Checkout {
         checkoutConfiguration: CheckoutConfiguration,
         checkoutCallbacks: CheckoutCallbacks,
     ): Result {
-        val publicKey = fetchPublicKey(checkoutConfiguration)
-        // TODO - Fetch checkoutAttemptId
+        val initializationData = CheckoutInitializer.initialize(
+            checkoutConfiguration = checkoutConfiguration,
+            sessionModel = null,
+        )
+
         return Result.Success(
             CheckoutContext.Advanced(
                 paymentMethodsApiResponse = paymentMethodsApiResponse,
                 checkoutConfiguration = checkoutConfiguration,
                 checkoutCallbacks = checkoutCallbacks,
-                publicKey = publicKey
+                publicKey = initializationData.publicKey,
             ),
-        )
-    }
-
-    private suspend fun getCheckoutSession(
-        sessionModel: SessionModel,
-        checkoutConfiguration: CheckoutConfiguration,
-    ): CheckoutSession? {
-        return when (
-            val result = CheckoutSessionProvider.createSession(sessionModel, checkoutConfiguration)
-        ) {
-            is CheckoutSessionResult.Success -> result.checkoutSession
-            is CheckoutSessionResult.Error -> null
-        }
-    }
-
-    private suspend fun fetchPublicKey(checkoutConfiguration: CheckoutConfiguration): String? {
-        val httpClient = HttpClientFactory.getHttpClient(checkoutConfiguration.environment)
-        val publicKeyRepository = DefaultPublicKeyRepository(PublicKeyService(httpClient))
-        publicKeyRepository.fetchPublicKey(
-            environment = checkoutConfiguration.environment,
-            clientKey = checkoutConfiguration.clientKey,
-        ).fold(
-            onSuccess = { key ->
-                adyenLog(AdyenLogLevel.DEBUG) { "Public key fetched" }
-                return key
-            },
-            onFailure = { e ->
-                adyenLog(AdyenLogLevel.ERROR) { "Unable to fetch public key" }
-
-                // TODO - Public Key. Analytics.
-//                val event = GenericEvents.error(paymentMethod.type.orEmpty(), ErrorEvent.API_PUBLIC_KEY)
-//                analyticsManager.trackEvent(event)
-                return null
-            },
         )
     }
 
---
+++ core/src/main/java/com/adyen/checkout/core/components/internal/CheckoutInitializer.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 10/11/2025.
+ */
+
+package com.adyen.checkout.core.components.internal
+
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.core.common.AdyenLogLevel
+import com.adyen.checkout.core.common.internal.api.HttpClientFactory
+import com.adyen.checkout.core.common.internal.data.api.DefaultPublicKeyRepository
+import com.adyen.checkout.core.common.internal.data.api.PublicKeyService
+import com.adyen.checkout.core.common.internal.helper.adyenLog
+import com.adyen.checkout.core.components.CheckoutConfiguration
+import com.adyen.checkout.core.sessions.CheckoutSession
+import com.adyen.checkout.core.sessions.CheckoutSessionResult
+import com.adyen.checkout.core.sessions.SessionModel
+import com.adyen.checkout.core.sessions.internal.CheckoutSessionProvider
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+object CheckoutInitializer {
+
+    suspend fun initialize(
+        checkoutConfiguration: CheckoutConfiguration,
+        sessionModel: SessionModel?,
+    ): InitializationData {
+        // TODO - Fetch checkoutAttemptId
+        val checkoutSession = sessionModel?.let { getCheckoutSession(sessionModel, checkoutConfiguration) }
+        val publicKey = fetchPublicKey(checkoutConfiguration)
+        return InitializationData(
+            checkoutSession = checkoutSession,
+            publicKey = publicKey,
+        )
+    }
+
+    private suspend fun getCheckoutSession(
+        sessionModel: SessionModel,
+        checkoutConfiguration: CheckoutConfiguration,
+    ): CheckoutSession? {
+        return when (
+            val result = CheckoutSessionProvider.createSession(sessionModel, checkoutConfiguration)
+        ) {
+            is CheckoutSessionResult.Success -> result.checkoutSession
+            is CheckoutSessionResult.Error -> null
+        }
+    }
+
+    private suspend fun fetchPublicKey(checkoutConfiguration: CheckoutConfiguration): String? {
+        val httpClient = HttpClientFactory.getHttpClient(checkoutConfiguration.environment)
+        val publicKeyRepository = DefaultPublicKeyRepository(PublicKeyService(httpClient))
+        publicKeyRepository.fetchPublicKey(
+            environment = checkoutConfiguration.environment,
+            clientKey = checkoutConfiguration.clientKey,
+        ).fold(
+            onSuccess = { key ->
+                adyenLog(AdyenLogLevel.DEBUG) { "Public key fetched" }
+                return key
+            },
+            onFailure = { e ->
+                adyenLog(AdyenLogLevel.ERROR) { "Unable to fetch public key" }
+
+                // TODO - Public Key. Analytics.
+//                val event = GenericEvents.error(paymentMethod.type.orEmpty(), ErrorEvent.API_PUBLIC_KEY)
+//                analyticsManager.trackEvent(event)
+                return null
+            },
+        )
+    }
+}
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+data class InitializationData(
+    val checkoutSession: CheckoutSession?,
+    val publicKey: String?,
+)
