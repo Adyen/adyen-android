c95fa3a6c79c9b2e11b0a38b26950424d7b4da1a
Oscar Spruit <oscar.spruit@adyen.com>
2025-11-10 13:41:14 +0100

    Add initialization functions for drop-in
    COSDK-719

--- drop-in/src/main/java/com/adyen/checkout/dropin/CheckoutExt.kt
+++ drop-in/src/main/java/com/adyen/checkout/dropin/CheckoutExt.kt
@@ -11,30 +11,74 @@ package com.adyen.checkout.dropin
 import android.app.Activity
 import android.content.Intent
 import com.adyen.checkout.core.components.Checkout
+import com.adyen.checkout.core.components.Checkout.Result
 import com.adyen.checkout.core.components.CheckoutConfiguration
 import com.adyen.checkout.core.components.data.model.PaymentMethodsApiResponse
+import com.adyen.checkout.core.components.internal.CheckoutInitializer
 import com.adyen.checkout.core.sessions.SessionModel
 import com.adyen.checkout.dropin.internal.ui.DropInActivity
 
-// TODO - Add launcher and service
+@Suppress("UnusedReceiverParameter")
+suspend fun Checkout.initialize(
+    sessionModel: SessionModel,
+    checkoutConfiguration: CheckoutConfiguration,
+): Result<CheckoutDropInContext.Sessions> {
+    val initializationData = CheckoutInitializer.initialize(
+        checkoutConfiguration = checkoutConfiguration,
+        sessionModel = sessionModel,
+    )
+
+    return when (val session = initializationData.checkoutSession) {
+        null -> {
+            Result.Error("Failed to initialize sessions.")
+        }
+
+        else -> Result.Success(
+            checkoutContext = CheckoutDropInContext.Sessions(
+                checkoutSession = session,
+                checkoutConfiguration = checkoutConfiguration,
+                publicKey = initializationData.publicKey,
+            ),
+        )
+    }
+}
+
+@Suppress("UnusedReceiverParameter")
+suspend fun Checkout.initialize(
+    paymentMethodsApiResponse: PaymentMethodsApiResponse,
+    checkoutConfiguration: CheckoutConfiguration,
+): Result<CheckoutDropInContext.Advanced> {
+    val initializationData = CheckoutInitializer.initialize(
+        checkoutConfiguration = checkoutConfiguration,
+        sessionModel = null,
+    )
+
+    return Result.Success(
+        checkoutContext = CheckoutDropInContext.Advanced(
+            paymentMethodsApiResponse = paymentMethodsApiResponse,
+            checkoutConfiguration = checkoutConfiguration,
+            publicKey = initializationData.publicKey,
+        ),
+    )
+}
+
+// TODO - Remove after migrating the example implementation
 @Suppress("UnusedReceiverParameter", "UnusedParameter")
 fun Checkout.startDropIn(
     activity: Activity,
     sessionModel: SessionModel,
     checkoutConfiguration: CheckoutConfiguration,
 ) {
-    // TODO - Call Checkout.initialize internally
     startDropInActivity(activity)
 }
 
-// TODO - Add launcher and service
+// TODO - Remove after migrating the example implementation
 @Suppress("UnusedReceiverParameter", "UnusedParameter")
 fun Checkout.startDropIn(
     activity: Activity,
     paymentMethodsApiResponse: PaymentMethodsApiResponse,
     checkoutConfiguration: CheckoutConfiguration,
 ) {
-    // TODO - Call Checkout.initialize internally
     startDropInActivity(activity)
 }
 
