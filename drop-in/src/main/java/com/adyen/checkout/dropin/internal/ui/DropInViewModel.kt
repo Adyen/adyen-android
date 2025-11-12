71567dc7494636adf89a32c696118e62c3bc67e3
Oscar Spruit <oscar.spruit@adyen.com>
2025-11-12 10:16:14 +0100

    Move back stack to the view model
    This will allow us to determine the starting nav key based on logic.
    
    COSDK-719

--- drop-in/src/main/java/com/adyen/checkout/dropin/internal/ui/DropInViewModel.kt
+++ drop-in/src/main/java/com/adyen/checkout/dropin/internal/ui/DropInViewModel.kt
@@ -8,21 +8,36 @@
 
 package com.adyen.checkout.dropin.internal.ui
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.navigation3.runtime.NavKey
 import com.adyen.checkout.dropin.internal.DropInResultContract
+import kotlin.reflect.KClass
 
 internal class DropInViewModel(
     input: DropInResultContract.Input?,
 ) : ViewModel() {
+
+    lateinit var backStack: SnapshotStateList<NavKey>
+
+    init {
+        initializeBackStack()
+    }
+
+    private fun initializeBackStack() {
+        // TODO - Check if there are stored payment methods, if not replace preselected with payment method list
+        backStack = mutableStateListOf(EmptyNavKey, PreselectedPaymentMethodNavKey)
+    }
 
     class Factory(
         private val inputProvider: () -> DropInResultContract.Input?,
     ) : ViewModelProvider.Factory {
 
         @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        override fun <T : ViewModel> create(modelClass: KClass<T>, extras: CreationExtras): T {
             return DropInViewModel(
                 input = inputProvider(),
             ) as T
