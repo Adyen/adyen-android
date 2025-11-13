de31fbcf591dcf0ed2df9b52aa0ebc8f140e9b86
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-11-13 16:57:15 +0100

    Add redirect navigation key
    COSDK-690

---
+++ redirect/src/main/java/com/adyen/checkout/redirect/RedirectMainNavigationKey.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 13/11/2025.
+ */
+
+package com.adyen.checkout.redirect
+
+import com.adyen.checkout.core.components.navigation.CheckoutNavigationKey
+
+/**
+ * Represent the main content of the [RedirectAction].
+ */
+data object RedirectMainNavigationKey : CheckoutNavigationKey
