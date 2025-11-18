5b6d0541a0a28f18211207befcf37382fcdc691d
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-11-18 15:22:42 +0100

    Trigger bin lookup callbacks
    COSDK-788

--- card/src/main/java/com/adyen/checkout/card/internal/ui/CardComponent.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/CardComponent.kt
@@ -18,11 +18,13 @@ import com.adyen.checkout.card.OnBinLookupCallback
 import com.adyen.checkout.card.OnBinValueCallback
 import com.adyen.checkout.card.internal.data.api.DetectCardTypeRepository
 import com.adyen.checkout.card.internal.data.model.DetectedCardType
+import com.adyen.checkout.card.internal.helper.toBinLookupData
 import com.adyen.checkout.card.internal.ui.model.CardComponentParams
 import com.adyen.checkout.card.internal.ui.state.CardChangeListener
 import com.adyen.checkout.card.internal.ui.state.CardComponentState
 import com.adyen.checkout.card.internal.ui.state.CardPaymentComponentState
 import com.adyen.checkout.card.internal.ui.state.CardViewState
+import com.adyen.checkout.card.internal.ui.state.binValue
 import com.adyen.checkout.card.internal.ui.view.CardComponent
 import com.adyen.checkout.core.analytics.internal.AnalyticsManager
 import com.adyen.checkout.core.analytics.internal.ErrorEvent
@@ -115,11 +117,16 @@ internal class CardComponent(
             coroutineScope = coroutineScope,
             type = CardPaymentMethod.PAYMENT_METHOD_TYPE,
         )
+
+        val oldBinValue = stateManager.viewState.value.binValue
         stateManager.updateViewStateAndValidate {
             copy(
                 cardNumber = cardNumber.updateText(newCardNumber),
             )
         }
+        val newBinValue = stateManager.viewState.value.binValue
+
+        onBinValueChange(oldBin = oldBinValue, newBin = newBinValue)
     }
 
     override fun onCardNumberFocusChanged(hasFocus: Boolean) {
@@ -271,12 +278,20 @@ internal class CardComponent(
             "New detected card types emitted - detectedCardTypes: ${detectedCardTypes.map { it.cardBrand }} " +
                 "- isReliable: ${detectedCardTypes.firstOrNull()?.isReliable}"
         }
+        val isReliable = detectedCardTypes.any { it.isReliable }
+        if (stateManager.componentState.detectedCardTypes != detectedCardTypes && isReliable) {
+            onBinLookupCallback?.onBinLookup(
+                binLookupData = detectedCardTypes.map(DetectedCardType::toBinLookupData),
+            )
+        }
         stateManager.updateComponentState {
             copy(detectedCardTypes = detectedCardTypes)
         }
-        // TODO - Card. Optional bin lookup callback.
-//        if (detectedCardTypes != outputData.detectedCardTypes) {
-//            onBinLookupListener?.invoke(detectedCardTypes.map(DetectedCardType::toBinLookupData))
-//        }
+    }
+
+    private fun onBinValueChange(oldBin: String, newBin: String) {
+        if (oldBin != newBin) {
+            onBinValueCallback?.onBinValue(newBin)
+        }
     }
 }
