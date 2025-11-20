ef6294d9e9dab7d4d6b6908c9a3c94e7b68763a5
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-11-20 12:23:22 +0100

    Extract card payment component state creation
    COSDK-570

---
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/state/CardViewStateExt.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 20/11/2025.
+ */
+
+package com.adyen.checkout.card.internal.ui.state
+
+import com.adyen.checkout.card.internal.ui.model.CardComponentParams
+import com.adyen.checkout.card.internal.ui.model.selectedBrand
+import com.adyen.checkout.core.common.CardBrand
+import com.adyen.checkout.core.common.helper.runCompileOnly
+import com.adyen.checkout.core.common.ui.model.ExpiryDate
+import com.adyen.checkout.core.components.data.PaymentComponentData
+import com.adyen.checkout.core.components.paymentmethod.CardPaymentMethod
+import com.adyen.checkout.cse.EncryptedCard
+import com.adyen.checkout.cse.EncryptionException
+import com.adyen.checkout.cse.UnencryptedCard
+import com.adyen.checkout.cse.internal.BaseCardEncryptor
+import com.adyen.threeds2.ThreeDS2Service
+
+@Suppress("ReturnCount")
+internal fun CardViewState.toPaymentComponentState(
+    componentParams: CardComponentParams,
+    cardEncryptor: BaseCardEncryptor,
+    checkoutAttemptId: String,
+    onEncryptionError: () -> Unit,
+): CardPaymentComponentState {
+    val unencryptedCardBuilder = UnencryptedCard.Builder()
+
+    val publicKey = componentParams.publicKey
+    if (publicKey == null) {
+        return CardPaymentComponentState(
+            data = PaymentComponentData(null, null, null),
+            isValid = false,
+        )
+    }
+
+    val encryptedCard: EncryptedCard = try {
+        unencryptedCardBuilder.setNumber(cardNumber.text)
+        // TODO - Card. Add isCvcHidden check
+//            if (!isCvcHidden()) {
+        val cvc = securityCode.text
+        if (cvc.isNotEmpty()) unencryptedCardBuilder.setCvc(cvc)
+//            }
+        if (expiryDate.text.isNotBlank()) {
+            val expiryDate = ExpiryDate.from(expiryDate.text)
+            unencryptedCardBuilder.setExpiryDate(
+                expiryMonth = expiryDate.expiryMonth.toString(),
+                expiryYear = expiryDate.expiryYear.toString(),
+            )
+        }
+
+        cardEncryptor.encryptFields(unencryptedCardBuilder.build(), publicKey)
+    } catch (_: EncryptionException) {
+        onEncryptionError()
+        return CardPaymentComponentState(
+            data = PaymentComponentData(null, null, null),
+            isValid = false,
+        )
+    }
+    val cardBrand = dualBrandData?.selectedBrand ?: detectedCardBrands.firstOrNull()
+    val holderName = if (componentParams.isHolderNameRequired && holderName.text.isNotBlank()) {
+        holderName.text
+    } else {
+        null
+    }
+
+    return mapComponentState(encryptedCard, holderName, cardBrand, componentParams, checkoutAttemptId)
+}
+
+private fun mapComponentState(
+    encryptedCard: EncryptedCard,
+    holderName: String?,
+    cardBrand: CardBrand?,
+    componentParams: CardComponentParams,
+    checkoutAttemptId: String,
+): CardPaymentComponentState {
+    val cardPaymentMethod = CardPaymentMethod(
+        type = CardPaymentMethod.PAYMENT_METHOD_TYPE,
+        checkoutAttemptId = checkoutAttemptId,
+        encryptedCardNumber = encryptedCard.encryptedCardNumber,
+        encryptedExpiryMonth = encryptedCard.encryptedExpiryMonth,
+        encryptedExpiryYear = encryptedCard.encryptedExpiryYear,
+        // TODO - Card. Add isCvcHidden check
+        encryptedSecurityCode = encryptedCard.encryptedSecurityCode,
+        holderName = holderName,
+        threeDS2SdkVersion = runCompileOnly { ThreeDS2Service.INSTANCE.sdkVersion },
+        brand = cardBrand?.txVariant,
+    )
+
+    val paymentComponentData = PaymentComponentData(
+        paymentMethod = cardPaymentMethod,
+        storePaymentMethod = null,
+        shopperReference = componentParams.shopperReference,
+        order = null,
+        amount = componentParams.amount,
+    )
+
+    return CardPaymentComponentState(
+        data = paymentComponentData,
+        isValid = true,
+    )
+}
