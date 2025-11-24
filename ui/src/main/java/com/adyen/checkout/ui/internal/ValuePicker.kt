19d3f4d0130fa3b02434b74ec56805cb34881783
Oscar Spruit <oscar.spruit@adyen.com>
2025-11-24 10:01:49 +0100

    Reuse SelectableListItem in ValuePicker
    COSDK-147

--- ui/src/main/java/com/adyen/checkout/ui/internal/ValuePicker.kt
+++ ui/src/main/java/com/adyen/checkout/ui/internal/ValuePicker.kt
@@ -9,27 +9,12 @@
 package com.adyen.checkout.ui.internal
 
 import androidx.annotation.RestrictTo
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.adyen.checkout.test.R
 
 @Composable
 internal fun ValuePicker(
@@ -45,10 +30,12 @@ internal fun ValuePicker(
             items = items,
             key = { it.id },
         ) { item ->
-            ValuePickerItem(
-                item,
-                onItemClick,
-                Modifier.animateItem(),
+            SelectableListItem(
+                title = item.title,
+                subtitle = item.subtitle,
+                isSelected = item.isSelected,
+                onClick = { onItemClick(item) },
+                modifier = Modifier.animateItem(),
             )
         }
     }
@@ -62,53 +49,6 @@ data class ValuePickerItem(
     val isSelected: Boolean,
 )
 
-@Composable
-private fun ValuePickerItem(
-    item: ValuePickerItem,
-    onClick: (ValuePickerItem) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .clip(RoundedCornerShape(CheckoutThemeProvider.attributes.cornerRadius.dp))
-            .let {
-                if (item.isSelected) {
-                    it.background(CheckoutThemeProvider.colors.container)
-                } else {
-                    it
-                }
-            }
-            .clickable(
-                interactionSource = null,
-                indication = ripple(color = CheckoutThemeProvider.colors.text),
-            ) {
-                onClick(item)
-            }
-            .fillMaxWidth()
-            .padding(12.dp),
-    ) {
-        Column {
-            BodyEmphasized(item.title)
-            SubHeadline(
-                text = item.subtitle,
-                color = CheckoutThemeProvider.colors.textSecondary,
-            )
-        }
-
-        if (item.isSelected) {
-            Icon(
-                imageVector = ImageVector.vectorResource(
-                    R.drawable.ic_checkmark,
-                ),
-                contentDescription = null,
-                tint = CheckoutThemeProvider.colors.text,
-            )
-        }
-    }
-}
-
 @Suppress("MagicNumber")
 @Preview(showBackground = true)
 @Composable
@@ -120,16 +60,5 @@ private fun ValuePickerPreview() {
     ValuePicker(
         items = items,
         onItemClick = {},
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun ValuePickerItemPreview() {
-    val item = ValuePickerItem(id = "id", title = "Title", subtitle = "Subtitle", isSelected = true)
-
-    ValuePickerItem(
-        item = item,
-        onClick = {},
     )
 }
