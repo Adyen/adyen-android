ebeb980cf8958e43322af8424d51963801e0a464
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-12-03 18:54:48 +0100

    Add StoredPaymentMethodFactory interface
    COSDK-840

---
+++ core/src/main/java/com/adyen/checkout/core/components/internal/StoredPaymentMethodFactory.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 2/12/2025.
+ */
+
+package com.adyen.checkout.core.components.internal
+
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.core.analytics.internal.AnalyticsManager
+import com.adyen.checkout.core.components.CheckoutCallbacks
+import com.adyen.checkout.core.components.CheckoutConfiguration
+import com.adyen.checkout.core.components.data.model.StoredPaymentMethod
+import com.adyen.checkout.core.components.internal.ui.PaymentComponent
+import com.adyen.checkout.core.components.internal.ui.model.CommonComponentParams
+import com.adyen.checkout.core.components.internal.ui.model.ComponentParamsBundle
+import com.adyen.checkout.core.sessions.internal.model.SessionParams
+import kotlinx.coroutines.CoroutineScope
+
+/**
+ * Factory interface for creating stored payment method components.
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+interface StoredPaymentMethodFactory<CS : BasePaymentComponentState, T : PaymentComponent<CS>> {
+
+    /**
+     * Creates a [PaymentComponent] for a stored payment method.
+     *
+     * @param storedPaymentMethod The stored payment method to create a component for.
+     * @param coroutineScope Coroutine Scope.
+     * @param analyticsManager Analytics manager for tracking component events.
+     * @param checkoutConfiguration Checkout Configuration.
+     * @param componentParamsBundle The object which contains [CommonComponentParams] and [SessionParams].
+     * @param checkoutCallbacks Callbacks for component events.
+     *
+     * @return A [PaymentComponent] instance for the stored payment method.
+     */
+    @Suppress("LongParameterList")
+    fun create(
+        storedPaymentMethod: StoredPaymentMethod,
+        coroutineScope: CoroutineScope,
+        analyticsManager: AnalyticsManager,
+        checkoutConfiguration: CheckoutConfiguration,
+        componentParamsBundle: ComponentParamsBundle,
+        checkoutCallbacks: CheckoutCallbacks,
+    ): T
+}
