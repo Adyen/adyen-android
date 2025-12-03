014d1d07a2ca8eba8d7444b48d6360c2c58ab610
Oscar Spruit <oscar.spruit@adyen.com>
2025-12-03 13:48:25 +0100

    Create Detekt convention plugin

--- build-logic/convention/src/main/kotlin/CheckoutAndroidLibraryPlugin.kt
+++ build-logic/convention/src/main/kotlin/CheckoutAndroidLibraryPlugin.kt
@@ -7,25 +7,30 @@
  */
 
 import com.android.build.api.dsl.LibraryExtension
+import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.the
 import org.jetbrains.kotlin.gradle.plugin.extraProperties
 
 class CheckoutAndroidLibraryPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
+            val libs = the<LibrariesForLibs>()
+
             apply(plugin = "com.android.library")
             apply(plugin = "org.jetbrains.kotlin.android")
             apply(plugin = "checkout.dependency.list.generate")
+            apply(plugin = "checkout.detekt")
 
             extensions.configure<LibraryExtension> {
-                compileSdk = 36
+                compileSdk = libs.versions.compile.sdk.get().toInt()
 
                 defaultConfig {
-                    minSdk = 23
+                    minSdk = libs.versions.min.sdk.get().toInt()
 
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                     consumerProguardFiles("consumer-rules.pro")
