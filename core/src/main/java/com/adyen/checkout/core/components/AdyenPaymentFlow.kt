0615b068a934f802c40cb664ecd19ca8af0d47ab
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-12-04 18:09:25 +0100

    Introduce InternalAdyenPaymentFlow
    COSDK-840

--- core/src/main/java/com/adyen/checkout/core/components/AdyenPaymentFlow.kt
+++ core/src/main/java/com/adyen/checkout/core/components/AdyenPaymentFlow.kt
@@ -10,19 +10,14 @@ package com.adyen.checkout.core.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.adyen.checkout.core.common.CheckoutContext
 import com.adyen.checkout.core.common.localization.CheckoutLocalizationProvider
 import com.adyen.checkout.core.components.data.model.PaymentMethod
 import com.adyen.checkout.core.components.data.model.StoredPaymentMethod
-import com.adyen.checkout.core.components.internal.AdyenComponent
 import com.adyen.checkout.core.components.internal.DefaultPaymentFacilitatorProvider
 import com.adyen.checkout.core.components.internal.StoredPaymentFacilitatorProvider
+import com.adyen.checkout.core.components.internal.ui.view.InternalAdyenPaymentFlow
 import com.adyen.checkout.core.components.navigation.CheckoutNavigationProvider
-import com.adyen.checkout.ui.internal.theme.InternalCheckoutTheme
 import com.adyen.checkout.ui.theme.CheckoutTheme
 
 // TODO - Change Name?
@@ -37,27 +32,21 @@ fun AdyenPaymentFlow(
     localizationProvider: CheckoutLocalizationProvider? = null,
     navigationProvider: CheckoutNavigationProvider? = null,
 ) {
-    val applicationContext = LocalContext.current.applicationContext
-    // TODO - Move Creation Logic to Adyen Checkout
-    // TODO - Verify that this does not keep observing the previous values and adds extra observables
-    val adyenComponent = viewModel(key = paymentMethod.hashCode().toString()) {
-        val paymentFacilitatorProvider = DefaultPaymentFacilitatorProvider(
-            paymentMethod = paymentMethod,
-            checkoutContext = checkoutContext,
-            checkoutCallbacks = checkoutCallbacks,
-            checkoutController = checkoutController,
-        )
+    val paymentFacilitatorProvider = DefaultPaymentFacilitatorProvider(
+        paymentMethod = paymentMethod,
+        checkoutContext = checkoutContext,
+        checkoutCallbacks = checkoutCallbacks,
+        checkoutController = checkoutController,
+    )
 
-        AdyenComponent(
-            applicationContext = applicationContext,
-            savedStateHandle = createSavedStateHandle(),
-            paymentFacilitatorProvider = paymentFacilitatorProvider,
-        )
-    }.apply { observe(LocalLifecycleOwner.current.lifecycle) }
-
-    InternalCheckoutTheme(theme) {
-        adyenComponent.ViewFactory(modifier, localizationProvider, navigationProvider)
-    }
+    InternalAdyenPaymentFlow(
+        key = paymentMethod.hashCode().toString(),
+        paymentFacilitatorProvider = paymentFacilitatorProvider,
+        modifier = modifier,
+        theme = theme,
+        localizationProvider = localizationProvider,
+        navigationProvider = navigationProvider,
+    )
 }
 
 @Composable
@@ -71,23 +60,19 @@ fun AdyenPaymentFlow(
     localizationProvider: CheckoutLocalizationProvider? = null,
     navigationProvider: CheckoutNavigationProvider? = null,
 ) {
-    val applicationContext = LocalContext.current.applicationContext
-    val storedAdyenComponent = viewModel(key = storedPaymentMethod.hashCode().toString()) {
-        val paymentFacilitatorProvider = StoredPaymentFacilitatorProvider(
-            storedPaymentMethod = storedPaymentMethod,
-            checkoutContext = checkoutContext,
-            checkoutCallbacks = checkoutCallbacks,
-            checkoutController = checkoutController,
-        )
+    val paymentFacilitatorProvider = StoredPaymentFacilitatorProvider(
+        storedPaymentMethod = storedPaymentMethod,
+        checkoutContext = checkoutContext,
+        checkoutCallbacks = checkoutCallbacks,
+        checkoutController = checkoutController,
+    )
 
-        AdyenComponent(
-            applicationContext = applicationContext,
-            savedStateHandle = createSavedStateHandle(),
-            paymentFacilitatorProvider = paymentFacilitatorProvider,
-        )
-    }.apply { observe(LocalLifecycleOwner.current.lifecycle) }
-
-    InternalCheckoutTheme(theme) {
-        storedAdyenComponent.ViewFactory(modifier, localizationProvider, navigationProvider)
-    }
+    InternalAdyenPaymentFlow(
+        key = storedPaymentMethod.hashCode().toString(),
+        paymentFacilitatorProvider = paymentFacilitatorProvider,
+        modifier = modifier,
+        theme = theme,
+        localizationProvider = localizationProvider,
+        navigationProvider = navigationProvider,
+    )
 }
