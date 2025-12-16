cd5c70db70eb0c3cd8e4f7c0531cc23f5d1955eb
Ararat Mnatsakanyan <ararat.mnatsakanyan@adyen.com>
2025-12-16 15:58:26 +0100

    Add base CheckoutError hierarchy
    COSDK-85

---
+++ core/src/main/java/com/adyen/checkout/core/common/exception/NetworkError.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 16/12/2025.
+ */
+
+package com.adyen.checkout.core.common.exception
+
+/**
+ * Errors related to network operations and API calls.
+ *
+ * These errors occur when there are issues with network connectivity,
+ * API responses, or communication with Adyen servers.
+ *
+ * @param message A human-readable description of the error.
+ * @param cause The underlying cause of this error, if any.
+ */
+abstract class NetworkError(
+    message: String,
+    cause: Throwable? = null,
+) : CheckoutError(message, cause)
