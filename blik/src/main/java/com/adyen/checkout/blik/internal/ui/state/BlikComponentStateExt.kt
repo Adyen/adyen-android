a198626a575b5fb1c5e02cadb07908c0e1f47648
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-12-17 16:40:06 +0100

    Add BlikComponentStateExt for state conversion.
    COSDK-883

---
+++ blik/src/main/java/com/adyen/checkout/blik/internal/ui/state/BlikComponentStateExt.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 17/12/2025.
+ */
+
+package com.adyen.checkout.blik.internal.ui.state
+
+import com.adyen.checkout.core.components.data.PaymentComponentData
+import com.adyen.checkout.core.components.data.model.Amount
+import com.adyen.checkout.core.components.paymentmethod.BlikPaymentMethod
+
+internal fun BlikComponentState.toPaymentComponentState(
+    checkoutAttemptId: String,
+    amount: Amount?,
+): BlikPaymentComponentState {
+    val paymentMethod = BlikPaymentMethod(
+        type = BlikPaymentMethod.PAYMENT_METHOD_TYPE,
+        checkoutAttemptId = checkoutAttemptId,
+        blikCode = blikCode.text,
+        storedPaymentMethodId = null,
+    )
+
+    val paymentComponentData = PaymentComponentData(
+        paymentMethod = paymentMethod,
+        order = null,
+        amount = amount,
+    )
+
+    return BlikPaymentComponentState(
+        data = paymentComponentData,
+        isValid = true,
+    )
+}
