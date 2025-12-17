004c4e672ad7646b79bcf104767e22923be5b257
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-12-17 16:52:19 +0100

    Implement BlikFactory for component creation.
    COSDK-883

--- blik/src/main/java/com/adyen/checkout/blik/internal/ui/BlikFactory.kt
+++ blik/src/main/java/com/adyen/checkout/blik/internal/ui/BlikFactory.kt
@@ -8,5 +8,38 @@
 
 package com.adyen.checkout.blik.internal.ui
 
-class BlikFactory {
+import com.adyen.checkout.blik.internal.ui.state.BlikComponentStateFactory
+import com.adyen.checkout.blik.internal.ui.state.BlikComponentStateReducer
+import com.adyen.checkout.blik.internal.ui.state.BlikComponentStateValidator
+import com.adyen.checkout.blik.internal.ui.state.BlikPaymentComponentState
+import com.adyen.checkout.blik.internal.ui.state.BlikViewStateProducer
+import com.adyen.checkout.core.analytics.internal.AnalyticsManager
+import com.adyen.checkout.core.components.CheckoutCallbacks
+import com.adyen.checkout.core.components.CheckoutConfiguration
+import com.adyen.checkout.core.components.data.model.PaymentMethod
+import com.adyen.checkout.core.components.internal.PaymentMethodFactory
+import com.adyen.checkout.core.components.internal.ui.model.ComponentParamsBundle
+import kotlinx.coroutines.CoroutineScope
+
+internal class BlikFactory : PaymentMethodFactory<BlikPaymentComponentState, BlikComponent> {
+
+    override fun create(
+        paymentMethod: PaymentMethod,
+        coroutineScope: CoroutineScope,
+        analyticsManager: AnalyticsManager,
+        checkoutConfiguration: CheckoutConfiguration,
+        componentParamsBundle: ComponentParamsBundle,
+        checkoutCallbacks: CheckoutCallbacks,
+    ): BlikComponent {
+        val componentParams = componentParamsBundle.commonComponentParams
+        return BlikComponent(
+            componentParams = componentParams,
+            analyticsManager = analyticsManager,
+            componentStateFactory = BlikComponentStateFactory(),
+            componentStateReducer = BlikComponentStateReducer(),
+            componentStateValidator = BlikComponentStateValidator(),
+            viewStateProducer = BlikViewStateProducer(),
+            coroutineScope = coroutineScope,
+        )
+    }
 }
