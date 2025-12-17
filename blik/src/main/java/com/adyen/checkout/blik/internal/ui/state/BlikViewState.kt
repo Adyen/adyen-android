8a90ee9a641fc429ea1fee0cd104fb84e60ad8dd
ozgur <6615094+ozgur00@users.noreply.github.com>
2025-12-17 15:04:38 +0100

    Add blik scaffold

---
+++ blik/src/main/java/com/adyen/checkout/blik/internal/ui/state/BlikViewState.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 17/12/2025.
+ */
+
+package com.adyen.checkout.blik.internal.ui.state
+
