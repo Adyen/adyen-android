e1c682c0d5d6ee46196a4a28e25c13e18d13fa53
Ararat Mnatsakanyan <ararat.mnatsakanyan@adyen.com>
2025-12-29 15:57:23 +0400

    Add specific error types: ThreeDS2Error, RedirectError, StatusPollingError, EncryptionError.
    COSDK-85

---
+++ core/src/main/java/com/adyen/checkout/core/common/exception/EncryptionError.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 29/12/2025.
+ */
+
+package com.adyen.checkout.core.common.exception
+
+// TODO - Platform alignment: Review error name and structure after iOS alignment.
+/**
+ * Errors related to encryption operations.
+ *
+ * These errors occur during card data encryption or other cryptographic operations.
+ *
+ * @param message A human-readable description of the error.
+ * @param cause The underlying cause of this error, if any.
+ */
+class EncryptionError(
+    message: String,
+    cause: Throwable? = null,
+) : InternalError(message, cause)
---
+++ core/src/main/java/com/adyen/checkout/core/common/exception/StatusPollingError.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ararat on 29/12/2025.
+ */
+
+package com.adyen.checkout.core.common.exception
+
+// TODO - Platform alignment: Review error name and structure after iOS alignment.
+/**
+ * Errors related to status polling operations.
+ *
+ * These errors occur during polling-based payment flows such as await actions.
+ *
+ * @param message A human-readable description of the error.
+ * @param cause The underlying cause of this error, if any.
+ */
+class StatusPollingError(
+    message: String,
+    cause: Throwable? = null,
+) : InternalError(message, cause)
