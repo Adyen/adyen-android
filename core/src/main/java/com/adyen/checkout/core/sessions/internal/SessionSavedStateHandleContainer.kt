a820b1c5edd2af4f92da66e7439792ea108438d9
Robert Schulze Dieckhoff <robert.schulzedieckhoff@adyen.com>
2026-01-07 16:17:08 +0100

    Rename map and get model methods as well

--- core/src/main/java/com/adyen/checkout/core/sessions/internal/SessionSavedStateHandleContainer.kt
+++ core/src/main/java/com/adyen/checkout/core/sessions/internal/SessionSavedStateHandleContainer.kt
@@ -15,7 +15,7 @@ import com.adyen.checkout.core.sessions.CheckoutSession
 import com.adyen.checkout.core.sessions.SessionResponse
 import com.adyen.checkout.core.sessions.internal.data.model.SessionDetails
 import com.adyen.checkout.core.sessions.internal.data.model.mapToDetails
-import com.adyen.checkout.core.sessions.internal.data.model.mapToModel
+import com.adyen.checkout.core.sessions.internal.data.model.mapToSessionResponse
 
 internal class SessionSavedStateHandleContainer(
     override val savedStateHandle: SavedStateHandle,
@@ -38,8 +38,8 @@ internal class SessionSavedStateHandleContainer(
         sessionDetails = sessionDetails?.copy(sessionData = sessionData)
     }
 
-    fun getSessionModel(): SessionResponse {
-        return requireNotNull(sessionDetails).mapToModel()
+    fun getSessionResponse(): SessionResponse {
+        return requireNotNull(sessionDetails).mapToSessionResponse()
     }
 
     companion object {
--- core/src/main/java/com/adyen/checkout/core/sessions/internal/SessionsPaymentFacilitatorFactory.kt
+++ core/src/main/java/com/adyen/checkout/core/sessions/internal/SessionsPaymentFacilitatorFactory.kt
@@ -85,7 +85,7 @@ internal class SessionsPaymentFacilitatorFactory(
             ),
             sessionSavedStateHandleContainer = sessionSavedStateHandleContainer,
             analyticsManager = analyticsManager,
-            sessionResponse = sessionSavedStateHandleContainer.getSessionModel(),
+            sessionResponse = sessionSavedStateHandleContainer.getSessionResponse(),
             isFlowTakenOver = sessionSavedStateHandleContainer.isFlowTakenOver ?: false,
         )
 
