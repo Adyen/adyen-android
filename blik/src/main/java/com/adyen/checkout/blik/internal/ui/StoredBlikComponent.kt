8c2ced596647ae18b4406a91ab5d5f3c7d172211
ozgur <6615094+ozgur00@users.noreply.github.com>
2026-01-08 16:20:22 +0100

    Add StoredBlikComponent and UI composable.
    COSDK-883

---
+++ blik/src/main/java/com/adyen/checkout/blik/internal/ui/StoredBlikComponent.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2025 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 8/1/2026.
+ */
+
+package com.adyen.checkout.blik.internal.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import com.adyen.checkout.blik.StoredBlikNavigationKey
+import com.adyen.checkout.blik.internal.ui.state.BlikPaymentComponentState
+import com.adyen.checkout.blik.internal.ui.state.StoredBlikViewState
+import com.adyen.checkout.blik.internal.ui.view.StoredBlikComponent
+import com.adyen.checkout.core.common.internal.helper.bufferedChannel
+import com.adyen.checkout.core.components.data.PaymentComponentData
+import com.adyen.checkout.core.components.data.model.StoredPaymentMethod
+import com.adyen.checkout.core.components.internal.PaymentComponentEvent
+import com.adyen.checkout.core.components.internal.ui.PaymentComponent
+import com.adyen.checkout.core.components.internal.ui.model.ComponentParams
+import com.adyen.checkout.core.components.internal.ui.navigation.CheckoutNavEntry
+import com.adyen.checkout.core.components.paymentmethod.BlikPaymentMethod
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+
+internal class StoredBlikComponent(
+    private val storedPaymentMethod: StoredPaymentMethod,
+    private val componentParams: ComponentParams,
+) : PaymentComponent<BlikPaymentComponentState> {
+
+    override val navigation: Map<NavKey, CheckoutNavEntry> = mapOf(
+        StoredBlikNavKey to CheckoutNavEntry(StoredBlikNavKey, StoredBlikNavigationKey) { backStack ->
+            MainScreen(backStack)
+        },
+    )
+
+    override val navigationStartingPoint: NavKey = StoredBlikNavKey
+
+    private val eventChannel = bufferedChannel<PaymentComponentEvent<BlikPaymentComponentState>>()
+    override val eventFlow: Flow<PaymentComponentEvent<BlikPaymentComponentState>> =
+        eventChannel.receiveAsFlow()
+
+    private val _isLoading = MutableStateFlow(false)
+
+    override fun submit() {
+        val paymentComponentState = createPaymentComponentState()
+        eventChannel.trySend(PaymentComponentEvent.Submit(paymentComponentState))
+    }
+
+    override fun setLoading(isLoading: Boolean) {
+        _isLoading.value = isLoading
+    }
+
+    private fun createPaymentComponentState(): BlikPaymentComponentState {
+        val paymentMethod = BlikPaymentMethod(
+            type = BlikPaymentMethod.PAYMENT_METHOD_TYPE,
+            blikCode = null,
+            storedPaymentMethodId = storedPaymentMethod.id,
+        )
+
+        val paymentComponentData = PaymentComponentData(
+            paymentMethod = paymentMethod,
+            order = null,
+            amount = componentParams.amount,
+        )
+
+        return BlikPaymentComponentState(
+            data = paymentComponentData,
+            isValid = true,
+        )
+    }
+
+    @Composable
+    @Suppress("UNUSED_PARAMETER")
+    private fun MainScreen(backStack: NavBackStack<NavKey>) {
+        val isLoading by _isLoading.collectAsStateWithLifecycle()
+
+        StoredBlikComponent(
+            viewState = StoredBlikViewState(isLoading = isLoading),
+            onSubmitClick = ::submit,
+        )
+    }
+}
