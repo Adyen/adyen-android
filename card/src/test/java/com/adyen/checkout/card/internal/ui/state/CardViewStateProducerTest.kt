98cdf12a7cf0c033ee82e92c37fc1cb44f60353c
Andrii Mamchenko <andrii.mamchenko@adyen.com>
2026-01-26 10:09:34 +0100

    Fix header year

--- card/src/test/java/com/adyen/checkout/card/internal/ui/state/CardViewStateProducerTest.kt
+++ card/src/test/java/com/adyen/checkout/card/internal/ui/state/CardViewStateProducerTest.kt
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2025 Adyen N.V.
+ * Copyright (c) 2026 Adyen N.V.
  *
  * This file is open source and available under the MIT license. See the LICENSE file for more info.
  *
- * Created by andriim on 22/1/2025.
+ * Created by andriim on 22/1/2026.
  */
 
 package com.adyen.checkout.card.internal.ui.state
