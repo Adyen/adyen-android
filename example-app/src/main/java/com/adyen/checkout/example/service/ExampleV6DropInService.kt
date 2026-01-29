946da074dfc934511bb042e432008b2ef0127696
ozgur <6615094+ozgur00@users.noreply.github.com>
2026-01-29 15:56:30 +0100

    Implement onSubmit call in advanced drop-in example
    COSDK-972

--- example-app/src/main/java/com/adyen/checkout/example/service/ExampleV6DropInService.kt
+++ example-app/src/main/java/com/adyen/checkout/example/service/ExampleV6DropInService.kt
@@ -8,12 +8,20 @@
 
 package com.adyen.checkout.example.service
 
+import android.util.Log
+import com.adyen.checkout.core.action.data.Action
+import com.adyen.checkout.core.common.exception.HttpError
 import com.adyen.checkout.core.components.CheckoutResult
+import com.adyen.checkout.core.components.data.PaymentComponentData
 import com.adyen.checkout.core.components.paymentmethod.PaymentComponentState
 import com.adyen.checkout.dropin.DropInService
 import com.adyen.checkout.example.data.storage.KeyValueStorage
+import com.adyen.checkout.example.extensions.getLogTag
+import com.adyen.checkout.example.extensions.toStringPretty
 import com.adyen.checkout.example.repositories.PaymentsRepository
+import com.adyen.checkout.redirect.old.RedirectComponent
 import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONObject
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -26,6 +34,55 @@ class ExampleV6DropInService : DropInService() {
     lateinit var keyValueStorage: KeyValueStorage
 
     override suspend fun onSubmit(state: PaymentComponentState<*>): CheckoutResult {
-        return CheckoutResult.Finished()
+        val paymentComponentJson = PaymentComponentData.SERIALIZER.serialize(state.data)
+        // Check out the documentation of this method on the parent DropInService class
+        val paymentRequest = createPaymentRequest(
+            paymentComponentData = paymentComponentJson,
+            shopperReference = keyValueStorage.getShopperReference(),
+            amount = keyValueStorage.getAmount(),
+            countryCode = keyValueStorage.getCountry(),
+            merchantAccount = keyValueStorage.getMerchantAccount(),
+            // TODO - Move return url creation to the new redirect component
+            redirectUrl = RedirectComponent.getReturnUrl(applicationContext),
+            threeDSMode = keyValueStorage.getThreeDSMode(),
+            shopperEmail = keyValueStorage.getShopperEmail(),
+        )
+
+        Log.v(TAG, "paymentComponentJson - ${paymentComponentJson.toStringPretty()}")
+        val response = paymentsRepository.makePaymentsRequest(paymentRequest)
+        return handleResponse(response)
+    }
+
+    private fun handleResponse(jsonResponse: JSONObject?): CheckoutResult {
+        return when {
+            jsonResponse == null -> {
+                Log.e(TAG, "FAILED")
+                CheckoutResult.Error(HttpError(code = 200, message = "IOException", errorBody = null))
+            }
+
+            isAction(jsonResponse) -> {
+                Log.d(TAG, "Received action")
+                val action = Action.SERIALIZER.deserialize(jsonResponse.getJSONObject("action"))
+                CheckoutResult.Action(action)
+            }
+
+            else -> {
+                Log.d(TAG, "Final result - ${jsonResponse.toStringPretty()}")
+                val resultCode = if (jsonResponse.has("resultCode")) {
+                    jsonResponse.get("resultCode").toString()
+                } else {
+                    "EMPTY"
+                }
+                CheckoutResult.Finished(resultCode)
+            }
+        }
+    }
+
+    private fun isAction(jsonResponse: JSONObject): Boolean {
+        return jsonResponse.has("action")
+    }
+
+    companion object {
+        private val TAG = getLogTag()
     }
 }
