b29f1dfa7237aab036757013c3ed9198fa774eb6
ozgur <6615094+ozgur00@users.noreply.github.com>
2026-02-03 15:31:11 +0100

    Add GooglePay public navigation keys.
    COSDK-936

---
+++ googlepay/src/main/java/com/adyen/checkout/googlepay/GooglePayMainNavigationKey.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2026 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 3/2/2026.
+ */
+
+package com.adyen.checkout.googlepay
+
+import com.adyen.checkout.core.components.navigation.CheckoutNavigationKey
+import com.adyen.checkout.core.components.paymentmethod.PaymentMethodTypes
+
+/**
+ * Represent the main form of the [PaymentMethodTypes.GOOGLE_PAY] payment method.
+ */
+data object GooglePayMainNavigationKey : CheckoutNavigationKey
