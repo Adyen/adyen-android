a3aa1643447d7380f5bb7ea72a6be6f765f2c49c
ozgur <6615094+ozgur00@users.noreply.github.com>
2026-02-03 14:57:10 +0100

    Create scaffold for googlepay
    COSDK-936

---
+++ googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/state/GooglePayComponentStateReducer.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by ozgur on 3/2/2026.
+ */
+
+package com.adyen.checkout.googlepay.internal.ui.state
+
+@Suppress("EmptyClassBlock", "NoEmptyClassBody")
+internal class GooglePayComponentStateReducer
