cec4e7835b99dd54bae7e64a6195813c8b2500e0
ozgur <6615094+ozgur00@users.noreply.github.com>
2026-02-03 15:37:32 +0100

    Implement GooglePay state classes.
    COSDK-936

--- googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/state/GooglePayComponentState.kt
+++ googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/state/GooglePayComponentState.kt
@@ -8,5 +8,12 @@
 
 package com.adyen.checkout.googlepay.internal.ui.state
 
-@Suppress("EmptyClassBlock", "NoEmptyClassBody")
-internal class GooglePayComponentState
+import com.adyen.checkout.core.components.internal.ui.state.ComponentState
+import com.google.android.gms.wallet.PaymentData
+
+internal data class GooglePayComponentState(
+    val isButtonVisible: Boolean,
+    val isLoading: Boolean,
+    val isAvailable: Boolean,
+    val paymentData: PaymentData?,
+) : ComponentState
