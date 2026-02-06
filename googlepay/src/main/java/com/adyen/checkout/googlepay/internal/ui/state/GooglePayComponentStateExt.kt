39e2d6914348b689d52efa2967a9f6a897cf0e11
ozgur <6615094+ozgur00@users.noreply.github.com>
2026-02-06 15:45:30 +0100

    Implement GooglePayComponentStateExt. COSDK-936

--- googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/state/GooglePayComponentStateExt.kt
+++ googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/state/GooglePayComponentStateExt.kt
@@ -8,5 +8,32 @@
 
 package com.adyen.checkout.googlepay.internal.ui.state
 
-@Suppress("EmptyClassBlock", "NoEmptyClassBody")
-internal class GooglePayComponentStateExt
+import com.adyen.checkout.core.components.data.PaymentComponentData
+import com.adyen.checkout.core.components.data.model.Amount
+import com.adyen.checkout.core.components.internal.data.provider.SdkDataProvider
+import com.adyen.checkout.googlepay.internal.helper.GooglePayUtils
+
+internal fun GooglePayComponentState.toPaymentComponentState(
+    amount: Amount?,
+    paymentMethodType: String?,
+    sdkDataProvider: SdkDataProvider,
+): GooglePayPaymentComponentState {
+    val paymentMethod = GooglePayUtils.createGooglePayPaymentMethod(
+        paymentData = paymentData,
+        paymentMethodType = paymentMethodType,
+        sdkData = sdkDataProvider.createEncodedSdkData(),
+    )
+
+    val paymentComponentData = PaymentComponentData(
+        paymentMethod = paymentMethod,
+        order = null,
+        amount = amount,
+    )
+
+    val isValid = paymentData != null && GooglePayUtils.findToken(paymentData).isNotEmpty()
+
+    return GooglePayPaymentComponentState(
+        data = paymentComponentData,
+        isValid = isValid,
+    )
+}
