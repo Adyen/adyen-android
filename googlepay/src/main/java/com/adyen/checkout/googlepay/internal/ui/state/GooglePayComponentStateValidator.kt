c46ad86f8822615cb9e93082f4d43cf31a2db0a2
ozgur <6615094+ozgur00@users.noreply.github.com>
2026-02-25 10:29:57 +0100

    Fix import in GooglePayComponentStateValidator
    COSDK-936

--- googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/state/GooglePayComponentStateValidator.kt
+++ googlepay/src/main/java/com/adyen/checkout/googlepay/internal/ui/state/GooglePayComponentStateValidator.kt
@@ -9,7 +9,7 @@
 package com.adyen.checkout.googlepay.internal.ui.state
 
 import com.adyen.checkout.core.components.internal.ui.state.ComponentStateValidator
-import com.adyen.checkout.googlepay.old.internal.util.GooglePayUtils
+import com.adyen.checkout.googlepay.internal.helper.GooglePayUtils
 
 internal class GooglePayComponentStateValidator : ComponentStateValidator<GooglePayComponentState> {
 
@@ -19,6 +19,7 @@ internal class GooglePayComponentStateValidator : ComponentStateValidator<Google
 
     override fun isValid(state: GooglePayComponentState): Boolean {
         val paymentData = state.paymentData ?: return false
-        return GooglePayUtils.findToken(paymentData).isNotEmpty()
+        return runCatching { GooglePayUtils.findToken(paymentData) }
+            .getOrNull()?.isNotEmpty() == true
     }
 }
