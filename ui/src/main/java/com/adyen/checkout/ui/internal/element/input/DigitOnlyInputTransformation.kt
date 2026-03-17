7f5e818fb3dc23413d42982e870bfba94450a78a
josephj <>
2026-03-17 15:22:50 +0100

    Extract DigitOnlyInputTransformation into separate class that can be reused
    COSDK-921

--- ui/src/main/java/com/adyen/checkout/ui/internal/element/input/DigitOnlyInputTransformation.kt
+++ ui/src/main/java/com/adyen/checkout/ui/internal/element/input/DigitOnlyInputTransformation.kt
@@ -12,41 +12,22 @@ import androidx.annotation.RestrictTo
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.TextFieldBuffer
-import androidx.compose.foundation.text.input.delete
 import androidx.compose.ui.text.input.KeyboardType
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 class DigitOnlyInputTransformation(
-    private val allowedSeparators: List<Char> = emptyList(),
-    private val maxLengthWithoutSeparators: Int? = null
+    allowedSeparators: List<Char> = emptyList(),
+    maxLengthWithoutSeparators: Int? = null,
 ) : InputTransformation {
+
+    private val digitOnlyTextFieldBufferTransformation = DigitOnlyTextFieldBufferTransformation(
+        allowedSeparators,
+        maxLengthWithoutSeparators,
+    )
 
     override val keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
 
     override fun TextFieldBuffer.transformInput() {
-        val text = asCharSequence()
-
-        // only allow digits and separators
-        if (text.any { !it.isDigit() && it !in allowedSeparators }) {
-            revertAllChanges()
-            return
-        }
-
-        if (maxLengthWithoutSeparators != null) {
-            // enforce max length
-            val digitsOnlyText = text.filter { it.isDigit() }
-            if (digitsOnlyText.length > maxLengthWithoutSeparators) {
-                revertAllChanges()
-                return
-            }
-        }
-
-        // set the raw value as digits only so that the OutputTransformation can format cleanly
-        // we surgically remove non-digit characters one by one to preserve cursor position
-        for (charIndex in length - 1 downTo 0) {
-            if (!text[charIndex].isDigit()) {
-                delete(charIndex, charIndex + 1)
-            }
-        }
+        digitOnlyTextFieldBufferTransformation.transformInput(this)
     }
 }
