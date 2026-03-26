37577cf2e18099fac2879a5e15e8c9085dbc9a0c
josephj <joseph.jreij@adyen.com>
2026-03-26 18:04:25 +0100

    Add compose preview to Card KCP fields
    COSDK-921

--- card/src/main/java/com/adyen/checkout/card/internal/ui/view/KCPCardPasswordField.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/view/KCPCardPasswordField.kt
@@ -8,11 +8,17 @@
 
 package com.adyen.checkout.card.internal.ui.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.adyen.checkout.card.internal.ui.state.CardIntent
 import com.adyen.checkout.core.common.internal.properties.KCPCardPasswordProperties.KCP_CARD_PASSWORD_MAX_LENGTH
 import com.adyen.checkout.core.common.localization.CheckoutLocalizationKey
@@ -20,6 +26,11 @@ import com.adyen.checkout.core.common.localization.internal.helper.resolveString
 import com.adyen.checkout.core.components.internal.ui.state.model.TextInputViewState
 import com.adyen.checkout.ui.internal.element.input.CheckoutTextField
 import com.adyen.checkout.ui.internal.element.input.DigitOnlyInputTransformation
+import com.adyen.checkout.ui.internal.element.input.TextFieldStylePreviewParameterProvider
+import com.adyen.checkout.ui.internal.theme.Dimensions
+import com.adyen.checkout.ui.internal.theme.InternalCheckoutTheme
+import com.adyen.checkout.ui.internal.theme.toCompose
+import com.adyen.checkout.ui.theme.CheckoutTheme
 
 @Composable
 internal fun KCPCardPasswordField(
@@ -52,4 +63,26 @@ internal fun KCPCardPasswordField(
         inputTransformation = inputTransformation,
         isSecureField = true,
     )
+}
+
+@Preview
+@Composable
+private fun KCPCardPasswordFieldPreview(
+    @PreviewParameter(TextFieldStylePreviewParameterProvider::class) theme: CheckoutTheme,
+) {
+    InternalCheckoutTheme(theme) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(Dimensions.Spacing.Large),
+            modifier = Modifier
+                .background(theme.colors.background.toCompose())
+                .padding(Dimensions.Spacing.Large),
+        ) {
+            KCPCardPasswordField(
+                kcpCardPasswordState = TextInputViewState(
+                    text = "12",
+                ),
+                onIntent = {},
+            )
+        }
+    }
 }
