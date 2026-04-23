c1d2613dcb991c0252ae9aa5b7da71b091e6f1e5
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2026-04-23 10:03:53 +0200

    Test: add public API stubs in await and blik
    Temporary commit to exercise the validate_public_api workflow. Adds a
    new public top-level function in two modules without running apiDump,
    so CI should produce a PR comment with two module sections (## await,
    ## blik), each showing the new public symbol as an addition hunk.
    
    COSDK-1121

---
+++ await/src/main/java/com/adyen/checkout/await/old/AwaitApiTestStub.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Temporary stub used to exercise the validate_public_api workflow (COSDK-1121).
+ * To be removed before merge.
+ */
+
+package com.adyen.checkout.await.old
+
+/** Dummy public API used to verify the public-API change detection flow. */
+fun awaitApiTestStub(): String = "await"
