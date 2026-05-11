954e25fe7e4a93af5120a35ca69e5dac415a8bcc
Temirlan Kuntubayev <temirlan.kuntubayev@adyen.com>
2026-05-11 14:42:26 +0200

    Replace the RestrictTo annotation with the internal visibility modifier for PostalCodeProperties

--- card/src/main/java/com/adyen/checkout/card/internal/ui/properties/PostalCodeProperties.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/properties/PostalCodeProperties.kt
@@ -1,8 +1,5 @@
 package com.adyen.checkout.card.internal.ui.properties
 
-import androidx.annotation.RestrictTo
-
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-object PostalCodeProperties {
+internal object PostalCodeProperties {
     const val POSTAL_CODE_MAX_LENGTH = 10
 }
