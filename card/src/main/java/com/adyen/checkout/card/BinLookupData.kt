3230a0d6fd597dae77a1e1c6945fe706d34442d3
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2026-05-13 15:03:59 +0200

    Reorder BinLookupData parameters to match alignment doc
    COSDK-1204

--- card/src/main/java/com/adyen/checkout/card/BinLookupData.kt
+++ card/src/main/java/com/adyen/checkout/card/BinLookupData.kt
@@ -3,10 +3,10 @@ package com.adyen.checkout.card
 /**
  * Data about the result of a BIN lookup for the entered card number.
  *
- * @param brands The list of detected card brands.
  * @param issuingCountryCode The country code of the card issuer, or `null` if not available.
+ * @param brands The list of detected card brands.
  */
 data class BinLookupData(
-    val brands: List<BinLookupBrand>,
     val issuingCountryCode: String?,
+    val brands: List<BinLookupBrand>,
 )
