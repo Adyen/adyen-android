bf5840e95806c47d9291d64caf505fb1df70de08
josephj <joseph.jreij@adyen.com>
2026-05-18 15:59:22 -0700

    Create new internal card scanning API.
    COSDK-1195

---
+++ card-scanning/src/main/java/com/adyen/checkout/card/scanning/internal/CardScannerController.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2026 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by josephj on 18/5/2026.
+ */
+
+package com.adyen.checkout.card.scanning.internal
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentSender
+import androidx.annotation.RestrictTo
+import com.adyen.checkout.core.common.AdyenLogLevel
+import com.adyen.checkout.core.common.internal.helper.adyenLog
+import com.google.android.gms.wallet.PaymentCardRecognitionResult
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+class CardScannerController internal constructor(
+    pendingIntent: PendingIntent,
+) {
+
+    private var paymentCardRecognitionPendingIntent: PendingIntent? = pendingIntent
+
+    fun getIntentSender(): IntentSender? {
+        if (paymentCardRecognitionPendingIntent == null) {
+            adyenLog(AdyenLogLevel.ERROR) { "CardScannerController has been terminated." }
+            return null
+        }
+        return paymentCardRecognitionPendingIntent?.intentSender
+    }
+
+    fun parseResult(intent: Intent?): CardScanResult? {
+        intent ?: return null
+        return PaymentCardRecognitionResult.getFromIntent(intent)
+            ?.let { result ->
+                CardScanResult(
+                    pan = result.pan,
+                    expiryMonth = result.creditCardExpirationDate?.month,
+                    expiryYear = result.creditCardExpirationDate?.year,
+                )
+            }
+    }
+
+    fun terminate() {
+        paymentCardRecognitionPendingIntent = null
+    }
+}
