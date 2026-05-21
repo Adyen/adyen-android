404707359115d814a4f5eb9c3f65c057fccfbd6e
josephj <joseph.jreij@adyen.com>
2026-05-21 14:06:06 -0700

    Integrate card scanning in v6 CardComponent logic layer.
    COSDK-1195

---
+++ card/src/main/java/com/adyen/checkout/card/internal/util/CardScannerWrapper.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2026 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by josephj on 21/5/2026.
+ */
+
+package com.adyen.checkout.card.internal.util
+
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import com.adyen.checkout.card.scanning.internal.CardScannerController
+import com.adyen.checkout.card.scanning.internal.CardScannerInitializer
+import com.adyen.checkout.core.common.Environment
+import com.adyen.checkout.core.common.helper.runCompileOnly
+
+internal class CardScannerWrapper {
+
+    private var controller: CardScannerController? = null
+
+    suspend fun initialize(context: Context, environment: Environment): Boolean {
+        controller = runCompileOnly { CardScannerInitializer.initialize(context, environment) }.getOrNull()
+        return controller != null
+    }
+
+    fun getIntentSender(): IntentSender? {
+        return controller?.getIntentSender()
+    }
+
+    fun parseResult(intent: Intent?): ScanResult? {
+        val result = controller?.parseResult(intent) ?: return null
+        return ScanResult(
+            pan = result.pan,
+            expiryMonth = result.expiryMonth,
+            expiryYear = result.expiryYear,
+        )
+    }
+
+    fun terminate() {
+        controller?.terminate()
+        controller = null
+    }
+
+    internal data class ScanResult(
+        val pan: String?,
+        val expiryMonth: Int?,
+        val expiryYear: Int?,
+    )
+}
