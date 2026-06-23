f8b933efdc593f00e27207b6c486333d0bea4d84
Oscar Spruit <oscar.spruit@adyen.com>
2026-06-23 20:40:36 +0200

    Gracefully handle errors in CheckoutControllerFactory

---
+++ core/src/main/java/com/adyen/checkout/core/components/internal/FailureCheckoutFlow.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2026 Adyen N.V.
+ *
+ * This file is open source and available under the MIT license. See the LICENSE file for more info.
+ *
+ * Created by oscars on 23/6/2026.
+ */
+
+package com.adyen.checkout.core.components.internal
+
+import com.adyen.checkout.core.action.internal.ActionComponent
+import com.adyen.checkout.core.components.CheckoutPaymentMethodRoute
+import com.adyen.checkout.core.components.CheckoutSecondaryRoute
+import com.adyen.checkout.core.components.internal.ui.PaymentComponent
+import com.adyen.checkout.core.error.CheckoutError
+import com.adyen.checkout.core.error.CheckoutError.ErrorCode.PAYMENT_METHOD_FAILURE
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+internal class FailureCheckoutFlow(
+    private val errorMessage: String,
+    private val componentRequestDispatcher: ComponentRequestDispatcher,
+) : CheckoutFlow {
+
+    override val paymentComponent: PaymentComponent? = null
+    override val actionComponent: ActionComponent? = null
+    override val paymentMethodNavigation: Flow<CheckoutPaymentMethodRoute> = emptyFlow()
+    override val secondaryNavigation: Flow<CheckoutSecondaryRoute> = emptyFlow()
+
+    init {
+        emitFailure(errorMessage)
+    }
+
+    override fun submit() {
+        emitFailure("Cannot submit: $errorMessage")
+    }
+
+    private fun emitFailure(errorMessage: String) {
+        val error = CheckoutError(
+            code = PAYMENT_METHOD_FAILURE,
+            message = errorMessage,
+        )
+        componentRequestDispatcher.failure(error)
+    }
+
+    override fun requiresUserInteraction(): Boolean {
+        return false
+    }
+}
