b9989f63ba7ee9cf525cf7596d8cc041958f271b
Oscar Spruit <oscar.spruit@adyen.com>
2026-06-26 15:57:53 +0200

    Add preview for StoredCardContent
    COSDK-1120

--- card/src/main/java/com/adyen/checkout/card/internal/ui/view/StoredCardContent.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/view/StoredCardContent.kt
@@ -14,12 +14,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.adyen.checkout.card.internal.ui.state.CardNumberFormat
 import com.adyen.checkout.card.internal.ui.state.StoredCardIntent
 import com.adyen.checkout.card.internal.ui.state.StoredCardViewState
+import com.adyen.checkout.core.common.CardBrand
+import com.adyen.checkout.core.components.internal.ui.state.model.TextInputViewState
 import com.adyen.checkout.ui.internal.element.ComponentScaffold
 import com.adyen.checkout.ui.internal.element.button.PayButton
+import com.adyen.checkout.ui.internal.helper.CheckoutThemeWrapper
+import com.adyen.checkout.ui.internal.helper.ThemePreviewParameterProvider
 import com.adyen.checkout.ui.internal.theme.Dimensions
+import com.adyen.checkout.ui.theme.CheckoutTheme
 import kotlinx.coroutines.flow.StateFlow
 
 @Composable
@@ -65,5 +73,27 @@ private fun StoredCardContent(
                 )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun StoredCardContentPreview(
+    @PreviewParameter(ThemePreviewParameterProvider::class) theme: CheckoutTheme,
+) {
+    CheckoutThemeWrapper(theme) {
+        val viewState = StoredCardViewState(
+            securityCode = TextInputViewState(),
+            brand = CardBrand(""),
+            cardNumberFormat = CardNumberFormat.DEFAULT,
+            isLoading = false,
+        )
+
+        StoredCardContent(
+            viewState = viewState,
+            onIntent = {},
+            onSubmitClick = {},
+            modifier = Modifier,
+        )
     }
 }
