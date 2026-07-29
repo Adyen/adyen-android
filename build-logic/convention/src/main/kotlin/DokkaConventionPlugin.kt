38d3b39c2c3a382b4770599485bbd337b6f66f74
Oscar Spruit <oscar.spruit@adyen.com>
2026-07-29 11:55:56 +0200

    Adapt the Dokka plugin to the version in this project

--- build-logic/convention/src/main/kotlin/DokkaConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/DokkaConventionPlugin.kt
@@ -8,12 +8,11 @@
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.assign
-import org.gradle.kotlin.dsl.configure
-import org.jetbrains.dokka.gradle.DokkaExtension
-import org.jetbrains.dokka.gradle.engine.plugins.DokkaHtmlPluginParameters
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URI
 import java.time.Year
 
 class DokkaConventionPlugin : Plugin<Project> {
@@ -21,30 +20,29 @@ class DokkaConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             apply(plugin = "org.jetbrains.dokka")
-            apply(plugin = "org.jetbrains.dokka-javadoc")
 
             val projectName = name
             val mainSourceDir = file("src/main/java")
 
-            extensions.configure<DokkaExtension> {
+            tasks.withType<DokkaTask>().configureEach {
                 moduleName = projectName
-
-                dokkaPublications.configureEach {
-                    suppressInheritedMembers = true
-                    failOnWarning = true
-                }
+                suppressInheritedMembers = true
+                failOnWarning = true
 
                 dokkaSourceSets.configureEach {
                     sourceLink {
                         localDirectory = mainSourceDir
-                        remoteUrl("https://github.com/Adyen/adyen-android/tree/main/$projectName/src/main/java")
+                        remoteUrl = URI(
+                            "https://github.com/Adyen/adyen-android/tree/main/$projectName/src/main/java",
+                        ).toURL()
                         remoteLineSuffix = "#L"
                     }
                 }
 
-                (pluginsConfiguration as ExtensionAware).extensions.configure<DokkaHtmlPluginParameters>("html") {
-                    footerMessage = "Copyright (c) ${Year.now()} Adyen N.V."
-                }
+                pluginsMapConfiguration.put(
+                    "org.jetbrains.dokka.base.DokkaBase",
+                    """{ "footerMessage": "Copyright (c) ${Year.now()} Adyen N.V." }""",
+                )
             }
         }
     }
