8c5ad89463da0cb044d9dfd5d028be2d34b5231c
Oscar Spruit <oscar.spruit@adyen.com>
2026-08-03 16:12:07 +0200

    Add Kover convention plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     compileOnly(libs.detekt.gradle.plugin)
     compileOnly(libs.dokka.gradle.plugin)
     compileOnly(libs.kotlin.gradle.plugin)
+    compileOnly(libs.kover.gradle.plugin)
     compileOnly(libs.sonarqube.gradle.plugin)
     compileOnly(libs.binary.compatibility.validator.gradle.plugin)
 
@@ -52,6 +53,11 @@ gradlePlugin {
         register("checkoutJacoco") {
             id = libs.plugins.checkout.jacoco.get().pluginId
             implementationClass = "JacocoConventionPlugin"
+        }
+
+        register("checkoutKover") {
+            id = libs.plugins.checkout.kover.get().pluginId
+            implementationClass = "KoverConventionPlugin"
         }
 
         register("checkoutKtlint") {
