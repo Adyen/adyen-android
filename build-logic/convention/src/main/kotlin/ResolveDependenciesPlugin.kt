a42653ddbd889f286ce201c784a67d5db854d9b2
Oscar Spruit <oscar.spruit@adyen.com>
2026-08-03 12:19:11 +0200

    Port improvements to ResolveDependenciesPlugin

--- build-logic/convention/src/main/kotlin/ResolveDependenciesPlugin.kt
+++ build-logic/convention/src/main/kotlin/ResolveDependenciesPlugin.kt
@@ -6,37 +6,42 @@
  * Created by oscars on 3/12/2025.
  */
 
+import com.adyen.checkout.ResolveDependenciesTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.ConfigurationContainer
-import org.gradle.api.logging.Logger
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.register
+import java.util.concurrent.Callable
 
 class ResolveDependenciesPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
-            tasks.register("resolveDependencies") {
-                notCompatibleWithConfigurationCache("Uses project during execution")
-                doNotTrackState("This task must always run to ensure the latest dependencies are resolved")
+            val resolveTask = registerResolveDependenciesTask()
 
-                doLast {
-                    allprojects {
-                        logger.lifecycle("Resolving dependencies for ${project.name}")
-                        project.buildscript.configurations.resolveDependencies(logger)
-                        project.configurations.resolveDependencies(logger)
-                    }
+            if (target == rootProject) {
+                subprojects {
+                    val subprojectResolveTask = registerResolveDependenciesTask()
+                    resolveTask.configure { dependsOn(subprojectResolveTask) }
                 }
             }
         }
     }
 
-    private fun ConfigurationContainer.resolveDependencies(logger: Logger) {
-        this
+    private fun Project.registerResolveDependenciesTask(): TaskProvider<ResolveDependenciesTask> {
+        return tasks.register<ResolveDependenciesTask>("resolveDependencies") {
+            doNotTrackState("This task must always run to ensure the latest dependencies are resolved")
+
+            dependencies.from(Callable { configurations.resolvableArtifactFiles() })
+            dependencies.from(Callable { buildscript.configurations.resolvableArtifactFiles() })
+        }
+    }
+
+    private fun ConfigurationContainer.resolvableArtifactFiles(): List<FileCollection> {
+        return this
             .filter { it.isCanBeResolved }
-            .forEach { config ->
-                config.incoming.artifactView { lenient(true) }.artifacts.failures.forEach {
-                    logger.info(it.message)
-                }
-            }
+            .map { it.incoming.artifactView { lenient(true) }.files }
     }
 }
