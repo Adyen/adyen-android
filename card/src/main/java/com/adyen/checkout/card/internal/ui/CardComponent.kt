4f8255afcf6999959e5750ad93e36860bbb4b1d8
josephj <joseph.jreij@adyen.com>
2026-09-11 15:34:21 +0200

    Read card validity from the form

--- card/src/main/java/com/adyen/checkout/card/internal/ui/CardComponent.kt
+++ card/src/main/java/com/adyen/checkout/card/internal/ui/CardComponent.kt
@@ -33,6 +33,7 @@ import com.adyen.checkout.card.internal.ui.state.CardComponentStateFactory
 import com.adyen.checkout.card.internal.ui.state.CardComponentStatePostProcessor
 import com.adyen.checkout.card.internal.ui.state.CardComponentStateReducer
 import com.adyen.checkout.card.internal.ui.state.CardComponentStateValidator
+import com.adyen.checkout.card.internal.ui.state.CardFormElementId
 import com.adyen.checkout.card.internal.ui.state.CardIntent
 import com.adyen.checkout.card.internal.ui.state.CardPaymentComponentStateFactory
 import com.adyen.checkout.card.internal.ui.state.CardViewStateProducer
@@ -381,7 +382,7 @@ constructor(
             .map { state ->
                 CardBinHelper.getBin(
                     cardNumber = state.cardNumber.text,
-                    isValid = state.cardNumber.isValid,
+                    isValid = state.form.isElementVisibleAndValid(CardFormElementId.CARD_NUMBER),
                 )
             }
             .distinctUntilChanged()
