08eac999f1d26046183d6337251dcce931eb494c
Oscar Spruit <oscar.spruit@adyen.com>
2026-09-17 13:10:41 +0200

    Document the Drop-in configuration.
    COSDK-1398

--- drop-in/src/main/java/com/adyen/checkout/dropin/DropInConfiguration.kt
+++ drop-in/src/main/java/com/adyen/checkout/dropin/DropInConfiguration.kt
@@ -12,12 +12,24 @@ import com.adyen.checkout.core.components.CheckoutConfiguration
 import com.adyen.checkout.core.components.internal.Configuration
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Configuration for Drop-in. Create it by calling [CheckoutConfiguration.dropIn].
+ */
 @Parcelize
 class DropInConfiguration internal constructor(
     val hideStoredPaymentMethods: Boolean?,
     val startWithLastStoredPaymentMethod: Boolean?,
 ) : Configuration
 
+/**
+ * Adds a [DropInConfiguration] to this [CheckoutConfiguration] to configure Drop-in.
+ *
+ * @param hideStoredPaymentMethods Whether the stored payment methods of the shopper should be left off the payment
+ * method list. Defaults to false.
+ * @param startWithLastStoredPaymentMethod Whether Drop-in should open on the stored payment method the shopper used
+ * last, instead of on the payment method list. Only applies if the shopper has a stored payment method, and is not
+ * affected by [hideStoredPaymentMethods]. Defaults to true.
+ */
 fun CheckoutConfiguration.dropIn(
     hideStoredPaymentMethods: Boolean? = null,
     startWithLastStoredPaymentMethod: Boolean? = null,
